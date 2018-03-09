0fd365f38d2d5f91f85806b6e650da9302705afa
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-03-08 16:43:58 -0800

    Dropp errors if queue is full

---
+++ apm-agent-core/src/test/java/co/elastic/apm/report/ApmServerReporterIntegrationTest.java
@@ -0,0 +1,80 @@
+package co.elastic.apm.report;
+
+import co.elastic.apm.impl.error.ErrorCapture;
+import co.elastic.apm.impl.transaction.Transaction;
+import co.elastic.apm.impl.payload.Process;
+import co.elastic.apm.impl.payload.Service;
+import co.elastic.apm.impl.payload.SystemInfo;
+import co.elastic.apm.report.serialize.JacksonPayloadSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import io.undertow.Undertow;
+import okhttp3.OkHttpClient;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+class ApmServerReporterIntegrationTest {
+
+    private static Undertow server;
+    private static int port;
+    private static AtomicInteger receivedHttpRequests = new AtomicInteger();
+    private ApmServerHttpPayloadSender payloadSender;
+    private ReporterConfiguration reporterConfiguration;
+    private ApmServerReporter reporter;
+
+    @BeforeAll
+    static void startServer() {
+        server = Undertow.builder()
+            .addHttpListener(0, "127.0.0.1")
+            .setHandler(exchange -> {
+                receivedHttpRequests.incrementAndGet();
+                exchange.setStatusCode(200).endExchange();
+            }).build();
+        server.start();
+        port = ((InetSocketAddress) server.getListenerInfo().get(0).getAddress()).getPort();
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+    @BeforeEach
+    void setUp() {
+        receivedHttpRequests.set(0);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new AfterburnerModule());
+        reporterConfiguration = spy(new ReporterConfiguration());
+        when(reporterConfiguration.getFlushInterval()).thenReturn(-1);
+        when(reporterConfiguration.getServerUrl()).thenReturn("http://localhost:" + port);
+        payloadSender = new ApmServerHttpPayloadSender(new OkHttpClient(), new JacksonPayloadSerializer(objectMapper), reporterConfiguration);
+        SystemInfo system = new SystemInfo("x64", "localhost", "platform");
+        reporter = new ApmServerReporter(new Service(), new Process(), system, payloadSender, false, reporterConfiguration);
+    }
+
+    @Test
+    void testReportTransaction() throws ExecutionException, InterruptedException {
+        reporter.report(new Transaction());
+        reporter.flush().get();
+        assertThat(reporter.getDropped()).isEqualTo(0);
+        assertThat(receivedHttpRequests.get()).isEqualTo(1);
+    }
+
+    @Test
+    void testReportErrorCapture() throws ExecutionException, InterruptedException {
+        reporter.report(new ErrorCapture());
+        reporter.flush().get();
+        assertThat(reporter.getDropped()).isEqualTo(0);
+        assertThat(receivedHttpRequests.get()).isEqualTo(1);
+    }
+}
--- apm-agent-core/src/test/java/co/elastic/apm/report/ApmServerReporterTest.java
+++ apm-agent-core/src/test/java/co/elastic/apm/report/ApmServerReporterTest.java
@@ -1,80 +1,57 @@
 package co.elastic.apm.report;
 
 import co.elastic.apm.impl.error.ErrorCapture;
-import co.elastic.apm.impl.transaction.Transaction;
 import co.elastic.apm.impl.payload.Process;
 import co.elastic.apm.impl.payload.Service;
 import co.elastic.apm.impl.payload.SystemInfo;
-import co.elastic.apm.report.serialize.JacksonPayloadSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
-import io.undertow.Undertow;
-import okhttp3.OkHttpClient;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import co.elastic.apm.impl.transaction.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.net.InetSocketAddress;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class ApmServerReporterTest {
 
-    private static Undertow server;
-    private static int port;
-    private static AtomicInteger receivedHttpRequests = new AtomicInteger();
-    private ApmServerHttpPayloadSender payloadSender;
-    private ReporterConfiguration reporterConfiguration;
     private ApmServerReporter reporter;
-
-    @BeforeAll
-    static void startServer() {
-        server = Undertow.builder()
-            .addHttpListener(0, "127.0.0.1")
-            .setHandler(exchange -> {
-                receivedHttpRequests.incrementAndGet();
-                exchange.setStatusCode(200).endExchange();
-            }).build();
-        server.start();
-        port = ((InetSocketAddress) server.getListenerInfo().get(0).getAddress()).getPort();
-    }
-
-    @AfterAll
-    static void stopServer() {
-        server.stop();
-    }
+    private PayloadSender payloadSender;
 
     @BeforeEach
     void setUp() {
-        receivedHttpRequests.set(0);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new AfterburnerModule());
-        reporterConfiguration = spy(new ReporterConfiguration());
+        ReporterConfiguration reporterConfiguration = spy(new ReporterConfiguration());
         when(reporterConfiguration.getFlushInterval()).thenReturn(-1);
-        when(reporterConfiguration.getServerUrl()).thenReturn("http://localhost:" + port);
-        payloadSender = new ApmServerHttpPayloadSender(new OkHttpClient(), new JacksonPayloadSerializer(objectMapper), reporterConfiguration);
+        when(reporterConfiguration.getMaxQueueSize()).thenReturn(2);
         SystemInfo system = new SystemInfo("x64", "localhost", "platform");
-        reporter = new ApmServerReporter(new Service(), new Process(), system, payloadSender, false, reporterConfiguration);
+        payloadSender = mock(PayloadSender.class);
+        reporter = new ApmServerReporter(new Service(), new Process(), system, payloadSender, true, reporterConfiguration);
     }
 
     @Test
-    void testReportTransaction() throws ExecutionException, InterruptedException {
-        reporter.report(new Transaction());
-        reporter.flush().get();
-        assertThat(reporter.getDropped()).isEqualTo(0);
-        assertThat(receivedHttpRequests.get()).isEqualTo(1);
+    void testReport_discardTransactions_ifQueueIsFull() {
+        Transaction transaction = mock(Transaction.class);
+        // try to report lots of transactions with a tiny queue should lead to dropped events
+        for (int i = 0; i < 100; i++) {
+            reporter.report(transaction);
+        }
+        assertThat(reporter.getDropped()).isGreaterThan(0);
+        verify(payloadSender, atLeastOnce()).sendPayload(any());
+        verify(transaction, atLeastOnce()).recycle();
     }
 
     @Test
-    void testReportErrorCapture() throws ExecutionException, InterruptedException {
-        reporter.report(new ErrorCapture());
-        reporter.flush().get();
-        assertThat(reporter.getDropped()).isEqualTo(0);
-        assertThat(receivedHttpRequests.get()).isEqualTo(1);
+    void testReport_discardErrors_ifQueueIsFull() {
+        ErrorCapture error = mock(ErrorCapture.class);
+        // try to report lots of errors with a tiny queue should lead to dropped events
+        for (int i = 0; i < 100; i++) {
+            reporter.report(error);
+        }
+        assertThat(reporter.getDropped()).isGreaterThan(0);
+        verify(payloadSender, atLeastOnce()).sendPayload(any());
+        verify(error, atLeastOnce()).recycle();
     }
 }
