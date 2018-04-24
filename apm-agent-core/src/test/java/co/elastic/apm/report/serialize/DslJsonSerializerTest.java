4d32c876719069fb01c0dbd24317dc458179cd42
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-04-24 10:47:33 +0200

    Limit size of string values in JSON
    (closes #23)
    
    Signed-off-by: Felix Barnsteiner <felix.barnsteiner@elastic.co>

--- apm-agent-core/src/test/java/co/elastic/apm/report/serialize/DslJsonSerializerTest.java
+++ apm-agent-core/src/test/java/co/elastic/apm/report/serialize/DslJsonSerializerTest.java
@@ -1,12 +1,16 @@
 package co.elastic.apm.report.serialize;
 
+import com.dslplatform.json.JsonWriter;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 
@@ -30,6 +34,24 @@ class DslJsonSerializerTest {
             softly.assertThat(serializeTags(Map.of("foo*bar*baz", "qux"))).isEqualTo(toJson(Map.of("foo_bar_baz", "qux")));
             softly.assertThat(serializeTags(Map.of("foo\"bar\"baz", "qux"))).isEqualTo(toJson(Map.of("foo_bar_baz", "qux")));
         });
+    }
+
+    @Test
+    void testLimitStringValueLength() throws IOException {
+        StringBuilder longValue = new StringBuilder(DslJsonSerializer.MAX_VALUE_LENGTH + 1);
+        for (int i = 0; i < DslJsonSerializer.MAX_VALUE_LENGTH + 1; i++) {
+            longValue.append('0');
+        }
+
+        serializer.jw.writeByte(JsonWriter.OBJECT_START);
+        serializer.writeField("stringBuilder", longValue);
+        serializer.writeField("string", longValue.toString());
+        serializer.writeLastField("lastString", longValue.toString());
+        serializer.jw.writeByte(JsonWriter.OBJECT_END);
+        final JsonNode jsonNode = objectMapper.readTree(serializer.jw.toString());
+        assertThat(jsonNode.get("stringBuilder").textValue()).hasSize(DslJsonSerializer.MAX_VALUE_LENGTH).endsWith("…");
+        assertThat(jsonNode.get("string").textValue()).hasSize(DslJsonSerializer.MAX_VALUE_LENGTH).endsWith("…");
+        assertThat(jsonNode.get("lastString").textValue()).hasSize(DslJsonSerializer.MAX_VALUE_LENGTH).endsWith("…");
     }
 
     private String toJson(Map<String, String> map) {
