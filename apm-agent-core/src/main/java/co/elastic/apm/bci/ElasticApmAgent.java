0031f63b2b91335415a9f7d12173e83e43ffda37
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-05-08 14:10:03 +0200

    Add javaagent foundation
    Signed-off-by: Felix Barnsteiner <felix.barnsteiner@elastic.co>

---
+++ apm-agent-core/src/main/java/co/elastic/apm/bci/ElasticApmAgent.java
@@ -0,0 +1,116 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 the original author or authors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package co.elastic.apm.bci;
+
+import co.elastic.apm.bci.bytebuddy.ErrorLoggingListener;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
+import net.bytebuddy.dynamic.scaffold.TypeValidation;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.utility.JavaModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+import java.util.ServiceLoader;
+
+import static co.elastic.apm.bci.bytebuddy.ClassLoaderNameMatcher.classLoaderWithName;
+import static co.elastic.apm.bci.bytebuddy.ClassLoaderNameMatcher.isReflectionClassLoader;
+import static net.bytebuddy.asm.Advice.ExceptionHandler.Default.PRINTING;
+import static net.bytebuddy.matcher.ElementMatchers.any;
+
+public class ElasticApmAgent {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticApmAgent.class);
+
+    /**
+     * Allows the installation of this agent via the {@code javaagent} command line argument.
+     *
+     * @param agentArguments  The unused agent arguments.
+     * @param instrumentation The instrumentation instance.
+     */
+    public static void premain(String agentArguments, Instrumentation instrumentation) {
+        initInstrumentation(instrumentation);
+    }
+
+    /**
+     * Allows the installation of this agent via the Attach API.
+     *
+     * @param agentArguments  The unused agent arguments.
+     * @param instrumentation The instrumentation instance.
+     */
+    @SuppressWarnings("unused")
+    public static void agentmain(String agentArguments, Instrumentation instrumentation) {
+        initInstrumentation(instrumentation);
+    }
+
+    static void initInstrumentation(Instrumentation instrumentation) {
+        final ByteBuddy byteBuddy = new ByteBuddy()
+            .with(TypeValidation.of(true)) // TODO make false default to improve performance
+            .with(MethodGraph.Compiler.ForDeclaredMethods.INSTANCE);
+        AgentBuilder agentBuilder = getAgentBuilder(byteBuddy);
+        for (final ElasticApmAdvice advice : ServiceLoader.load(ElasticApmAdvice.class, ElasticApmAdvice.class.getClassLoader())) {
+            logger.debug("Applying advice {}", advice);
+            agentBuilder = agentBuilder
+                .type(new AgentBuilder.RawMatcher() {
+                    @Override
+                    public boolean matches(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, Class<?> classBeingRedefined, ProtectionDomain protectionDomain) {
+                        final boolean typeMatches = advice.getTypeMatcher().matches(typeDescription);
+                        if (typeMatches) {
+                            logger.debug("Type match for advice {}: {} matches {}",
+                                advice.getClass().getSimpleName(), advice.getTypeMatcher(), typeDescription);
+                        }
+                        return typeMatches;
+                    }
+                })
+                .transform(new AgentBuilder.Transformer.ForAdvice()
+                    .advice(new ElementMatcher<MethodDescription>() {
+                        @Override
+                        public boolean matches(MethodDescription target) {
+                            final boolean matches = advice.getMethodMatcher().matches(target);
+                            if (matches) {
+                                logger.debug("Method match for advice {}: {} matches {}",
+                                    advice.getClass().getSimpleName(), advice.getMethodMatcher(), target);
+                            }
+                            return matches;
+                        }
+                    }, advice.getClass().getName())
+                    .include(advice.getClass().getClassLoader())
+                    .withExceptionHandler(PRINTING))
+                .asDecorator();
+        }
+
+        agentBuilder.installOn(instrumentation);
+    }
+
+    private static AgentBuilder getAgentBuilder(ByteBuddy byteBuddy) {
+        return new AgentBuilder.Default(byteBuddy)
+            .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+            .with(new ErrorLoggingListener())
+            .ignore(any(), isReflectionClassLoader())
+            .or(any(), classLoaderWithName("org.codehaus.groovy.runtime.callsite.CallSiteClassLoader"))
+            .disableClassFormatChanges();
+    }
+
+}
---
+++ apm-agent-core/src/main/java/co/elastic/apm/bci/bytebuddy/ClassLoaderNameMatcher.java
@@ -0,0 +1,45 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 the original author or authors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package co.elastic.apm.bci.bytebuddy;
+
+import net.bytebuddy.matcher.ElementMatcher;
+
+public class ClassLoaderNameMatcher extends ElementMatcher.Junction.AbstractBase<ClassLoader> {
+
+	private final String name;
+
+	private ClassLoaderNameMatcher(String name) {
+		this.name = name;
+	}
+
+	public static ElementMatcher.Junction.AbstractBase<ClassLoader> classLoaderWithName(String name) {
+		return new ClassLoaderNameMatcher(name);
+
+	}
+
+	public static ElementMatcher.Junction.AbstractBase<ClassLoader> isReflectionClassLoader() {
+		return new ClassLoaderNameMatcher("sun.reflect.DelegatingClassLoader");
+	}
+
+	@Override
+	public boolean matches(ClassLoader target) {
+		return target != null && name.equals(target.getClass().getName());
+	}
+}
