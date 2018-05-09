d690f60122876933fc9cec8166df5777ce93b9a2
Felix Barnsteiner <felixbarny@users.noreply.github.com>
2018-05-09 11:32:26 +0200

    Add javaagent foundation (#79)

---
+++ apm-agent-core/src/test/java/co/elastic/apm/bci/InstrumentationTest.java
@@ -0,0 +1,65 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 the original author or authors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package co.elastic.apm.bci;
+
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InstrumentationTest {
+
+    @BeforeEach
+    void setUp() {
+        ElasticApmAgent.initInstrumentation(ByteBuddyAgent.install());
+    }
+
+    @Test
+    void testIntercept() {
+        assertThat(interceptMe()).isEqualTo("intercepted");
+    }
+
+    private String interceptMe() {
+        return "";
+    }
+
+    public static class TestAdvice extends ElasticApmAdvice {
+        @Advice.OnMethodExit
+        public static void onMethodExit(@Advice.Return(readOnly = false) String returnValue) {
+            returnValue = "intercepted";
+        }
+
+        @Override
+        public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+            return ElementMatchers.named("co.elastic.apm.bci.InstrumentationTest");
+        }
+
+        @Override
+        public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+            return ElementMatchers.named("interceptMe");
+        }
+    }
+}
