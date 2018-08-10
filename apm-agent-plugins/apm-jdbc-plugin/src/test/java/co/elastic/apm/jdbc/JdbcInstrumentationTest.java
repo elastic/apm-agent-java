5df9e05edb28da3e4fa199570776df69576f7cfd
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-08-10 17:42:56 +0200

    JDBC integration testing with testcontainers
    Tested databases:
    - h2
    - mysql
    - postgresql
    - mariadb
    - mssqlserver
    - oracle
    
    Tested connection pools:
    - tomcat-jdbc
    - HikariCP
    - vibur-dbcp
    - c3p0
    - commons-dbcp
    - commons-dbcp2
    - druid
    
    (closes #121)

--- apm-agent-plugins/apm-jdbc-plugin/src/test/java/co/elastic/apm/jdbc/JdbcInstrumentationTest.java
+++ apm-agent-plugins/apm-jdbc-plugin/src/test/java/co/elastic/apm/jdbc/JdbcInstrumentationTest.java
@@ -19,81 +19,12 @@
  */
 package co.elastic.apm.jdbc;
 
-import co.elastic.apm.AbstractInstrumentationTest;
-import co.elastic.apm.impl.transaction.Db;
-import co.elastic.apm.impl.transaction.Span;
-import co.elastic.apm.impl.transaction.Transaction;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.sql.CallableStatement;
-import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+public class JdbcInstrumentationTest extends AbstractJdbcInstrumentationTest {
 
-class JdbcInstrumentationTest extends AbstractInstrumentationTest {
-
-    private Connection connection;
-    private Transaction transaction;
-
-    @BeforeEach
-    void setUp() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:h2:mem:test", "user", "");
-        connection.createStatement().execute("CREATE TABLE IF NOT EXISTS ELASTIC_APM (FOO INT, BAR VARCHAR(255))");
-        connection.createStatement().execute("INSERT INTO ELASTIC_APM (FOO, BAR) VALUES (1, 'APM')");
-        transaction = tracer.startTransaction().activate();
-        transaction.setName("transaction");
-        transaction.withType("request");
-        transaction.withResult("success");
+    public JdbcInstrumentationTest() throws Exception {
+        super(DriverManager.getConnection("jdbc:h2:mem:test"),  "db.h2.sql");
     }
 
-    @AfterEach
-    void tearDown() throws SQLException {
-        connection.close();
-        transaction.deactivate().end();
-    }
-
-    @Test
-    void testStatement() throws SQLException {
-        final String sql = "SELECT * FROM ELASTIC_APM WHERE FOO=1";
-        ResultSet resultSet = connection.createStatement().executeQuery(sql);
-        assertSpanRecorded(resultSet, sql);
-    }
-
-    @Test
-    void testPreparedStatement() throws SQLException {
-        final String sql = "SELECT * FROM ELASTIC_APM WHERE FOO=$1";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setInt(1, 1);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        assertSpanRecorded(resultSet, sql);
-    }
-
-    @Test
-    void testCallableStatement() throws SQLException {
-        final String sql = "SELECT * FROM ELASTIC_APM WHERE FOO=$1";
-        CallableStatement preparedStatement = connection.prepareCall(sql);
-        preparedStatement.setInt(1, 1);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        assertSpanRecorded(resultSet, sql);
-    }
-
-    private void assertSpanRecorded(ResultSet resultSet, String sql) throws SQLException {
-        assertThat(resultSet.next()).isTrue();
-        assertThat(resultSet.getInt("foo")).isEqualTo(1);
-        assertThat(resultSet.getString("BAR")).isEqualTo("APM");
-        assertThat(reporter.getSpans()).hasSize(1);
-        Span jdbcSpan = reporter.getFirstSpan();
-        assertThat(jdbcSpan.getName().toString()).isEqualTo("SELECT");
-        assertThat(jdbcSpan.getType()).isEqualToIgnoringCase("db.h2.sql");
-        Db db = jdbcSpan.getContext().getDb();
-        assertThat(db.getStatement()).isEqualTo(sql);
-        assertThat(db.getUser()).isEqualToIgnoringCase("user");
-        assertThat(db.getType()).isEqualToIgnoringCase("sql");
-    }
 }
