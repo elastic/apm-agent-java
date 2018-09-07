37a94c5b7b44f95cca9735172fd39fb394ee234b
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-09-07 14:46:18 +0200

    Implement byte unit for api_request_size config

---
+++ apm-agent-core/src/test/java/co/elastic/apm/configuration/converter/ByteValueTest.java
@@ -0,0 +1,50 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 Elastic and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package co.elastic.apm.configuration.converter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+class ByteValueTest {
+
+    @Test
+    void testParseUnitSuccess() {
+        assertSoftly(softly -> {
+            softly.assertThat(ByteValue.of("1b").getBytes()).isEqualTo(1);
+            softly.assertThat(ByteValue.of("1B").getBytes()).isEqualTo(1);
+            softly.assertThat(ByteValue.of("2kb").getBytes()).isEqualTo(2048);
+            softly.assertThat(ByteValue.of("2KB").getBytes()).isEqualTo(2048);
+            softly.assertThat(ByteValue.of("3mb").getBytes()).isEqualTo(3 * 1024 * 1024);
+            softly.assertThat(ByteValue.of("3MB").getBytes()).isEqualTo(3 * 1024 * 1024);
+        });
+    }
+
+    @Test
+    void testParseUnitInvalid() {
+        for (String invalid : List.of("1Kib", "-1b", " 1b", "1 b", "1b ", "1gb")) {
+            assertThatCode(() -> ByteValue.of(invalid)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+}
