fb63f78e28e152ab745cd9c49630e3bef7a2fff7
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-09-11 09:59:26 +0200

    Rename server_url -> server_urls in documentation

--- apm-agent-benchmarks/src/main/java/co/elastic/apm/benchmark/ElasticApmContinuousBenchmark.java
+++ apm-agent-benchmarks/src/main/java/co/elastic/apm/benchmark/ElasticApmContinuousBenchmark.java
@@ -138,7 +138,7 @@ public abstract class ElasticApmContinuousBenchmark extends AbstractBenchmark {
                     .add(CoreConfiguration.SERVICE_NAME, "benchmark")
                     .add(CoreConfiguration.INSTRUMENT, Boolean.toString(apmEnabled))
                     .add(CoreConfiguration.ACTIVE, Boolean.toString(apmEnabled))
-                    .add("server_url", "http://localhost:" + port)
+                    .add("server_urls", "http://localhost:" + port)
                     .add("enable_intake_v2", "false")
                     .add("distributed_tracing", "false"))
                 .optionProviders(ServiceLoader.load(ConfigurationOptionProvider.class))
