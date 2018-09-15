7e6fc75fd60734cc8740f75e4fd87696ce9f395a
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-09-15 18:59:38 +0200

    Enable complex wildcard patterns
    - match case insensitive by default
    - matches are now always allocation free

--- apm-agent-core/src/test/java/co/elastic/apm/matcher/WildcardMatcherTest.java
+++ apm-agent-core/src/test/java/co/elastic/apm/matcher/WildcardMatcherTest.java
@@ -22,7 +22,9 @@ package co.elastic.apm.matcher;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
+import static co.elastic.apm.matcher.WildcardMatcher.indexOfIgnoreCase;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class WildcardMatcherTest {
@@ -53,10 +55,114 @@ class WildcardMatcherTest {
     }
 
     @Test
-    void testInvalidExpressions() {
+    void testCompoundWildcardMatcher() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*foo*");
         assertSoftly(softly -> {
-            softly.assertThatThrownBy(() -> WildcardMatcher.valueOf("/foo/*/baz*")).isInstanceOf(IllegalArgumentException.class);
-            softly.assertThatThrownBy(() -> WildcardMatcher.valueOf("/foo/*/bar/*/baz")).isInstanceOf(IllegalArgumentException.class);
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*foo*");
+            softly.assertThat(matcher.matches("foofoo")).isTrue();
+            softly.assertThat(matcher.matches("foo/bar/foo")).isTrue();
+            softly.assertThat(matcher.matches("/foo/bar/foo/bar")).isTrue();
+            softly.assertThat(matcher.matches("foo")).isFalse();
+        });
+    }
+
+    @Test
+    void testCompoundWildcardMatcher3() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*oo*");
+        assertSoftly(softly -> {
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*oo*");
+            softly.assertThat(matcher.matches("foooo")).isTrue();
+            softly.assertThat(matcher.matches("foofoo")).isTrue();
+            softly.assertThat(matcher.matches("foo/bar/foo")).isTrue();
+            softly.assertThat(matcher.matches("/foo/bar/foo/bar")).isTrue();
+            softly.assertThat(matcher.matches("foo")).isFalse();
+            softly.assertThat(matcher.matches("fooo")).isFalse();
+        });
+    }
+
+    @Test
+    void testCompoundWildcardMatcher2() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*bar*");
+        assertSoftly(softly -> {
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*bar*");
+            softly.assertThat(matcher.matches("foobar")).isTrue();
+            softly.assertThat(matcher.matches("foo/bar/foo/baz")).isTrue();
+            softly.assertThat(matcher.matches("/foo/bar/baz")).isTrue();
+            softly.assertThat(matcher.matches("bar/foo")).isFalse();
+            softly.assertThat(matcher.matches("barfoo")).isFalse();
+        });
+    }
+
+    @Test
+    void testCompoundWildcardMatcher4() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*far*");
+        assertSoftly(softly -> {
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*far*");
+            softly.assertThat(matcher.matches("foofar")).isTrue();
+            softly.assertThat(matcher.matches("foo/far/foo/baz")).isTrue();
+            softly.assertThat(matcher.matches("/foo/far/baz")).isTrue();
+            softly.assertThat(matcher.matches("/far/foo")).isFalse();
+            softly.assertThat(matcher.matches("farfoo")).isFalse();
+        });
+    }
+
+    @Test
+    void testMatchBetween() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*foo*");
+        assertSoftly(softly -> {
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*foo*");
+            softly.assertThat(matcher.matches("foofoo")).isTrue();
+            softly.assertThat(matcher.matches("foofo", "o")).isTrue();
+            softly.assertThat(matcher.matches("foof", "oo")).isTrue();
+            softly.assertThat(matcher.matches("foo", "foo")).isTrue();
+            softly.assertThat(matcher.matches("fo", "ofoo")).isTrue();
+            softly.assertThat(matcher.matches("f", "oofoo")).isTrue();
+            softly.assertThat(matcher.matches("foo/foo/foo/baz")).isTrue();
+            softly.assertThat(matcher.matches("/foo/foo/baz")).isTrue();
+            softly.assertThat(matcher.matches("/foo/foo")).isTrue();
+            softly.assertThat(matcher.matches("foobar")).isFalse();
+        });
+    }
+
+    @Test
+    void testCharAt() {
+        assertSoftly(softly -> {
+            softly.assertThat(WildcardMatcher.SimpleWildcardMatcher.charAt(0, "foo", "bar", "foo".length())).isEqualTo('f');
+            softly.assertThat(WildcardMatcher.SimpleWildcardMatcher.charAt(1, "foo", "bar", "foo".length())).isEqualTo('o');
+            softly.assertThat(WildcardMatcher.SimpleWildcardMatcher.charAt(2, "foo", "bar", "foo".length())).isEqualTo('o');
+            softly.assertThat(WildcardMatcher.SimpleWildcardMatcher.charAt(3, "foo", "bar", "foo".length())).isEqualTo('b');
+            softly.assertThat(WildcardMatcher.SimpleWildcardMatcher.charAt(4, "foo", "bar", "foo".length())).isEqualTo('a');
+            softly.assertThat(WildcardMatcher.SimpleWildcardMatcher.charAt(5, "foo", "bar", "foo".length())).isEqualTo('r');
+        });
+    }
+
+    @Test
+    void testIndexOfIgnoreCase() {
+        assertSoftly(softly -> {
+            softly.assertThat(indexOfIgnoreCase( "foo", "foo", "foo", false, 0, 6)).isEqualTo(0);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "foo", false, 0, 6)).isEqualTo(0);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "oob", false, 0, 6)).isEqualTo(1);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "oba", false, 0, 6)).isEqualTo(2);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "bar", false, 0, 6)).isEqualTo(3);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "o", false, 0, 6)).isEqualTo(1);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "ob", false, 0, 6)).isEqualTo(2);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "ooba", false, 0, 6)).isEqualTo(1);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "oobar", false, 0, 6)).isEqualTo(1);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "fooba", false, 0, 6)).isEqualTo(0);
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "foobar", false, 0, 6)).isEqualTo(0);
+            softly.assertThat(indexOfIgnoreCase( "afoo", "bar", "oba", false, 0, 7)).isEqualTo(3);
+            softly.assertThat(indexOfIgnoreCase( "afoo", "bara", "oba", false, 0, 8)).isEqualTo(3);
+            softly.assertThat(indexOfIgnoreCase( "aafoo", "baraa", "oba", false, 0, 10)).isEqualTo(4);
+
+            softly.assertThat(indexOfIgnoreCase( "foo", "bar", "ara", false, 0, 6)).isEqualTo(-1);
+        });
+    }
+
+    @Test
+    void testComplexExpressions() {
+        assertSoftly(softly -> {
+            softly.assertThat(WildcardMatcher.valueOf("/foo/*/baz*").matches("/foo/a/bar/b/baz")).isTrue();
+            softly.assertThat(WildcardMatcher.valueOf("/foo/*/bar/*/baz").matches("/foo/a/bar/b/baz")).isTrue();
         });
     }
 
@@ -192,9 +298,9 @@ class WildcardMatcherTest {
 
     @Test
     void testMatchesStartsWith_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)foo*");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("foo*");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)foo*");
+            softly.assertThat(matcher.toString()).isEqualTo("foo*");
             softly.assertThat(matcher.matches("foo")).isTrue();
             softly.assertThat(matcher.matches("foobar")).isTrue();
             softly.assertThat(matcher.matches("bar")).isFalse();
@@ -204,9 +310,9 @@ class WildcardMatcherTest {
 
     @Test
     void testInfixEmptyMatcher_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)**");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("**");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)**");
+            softly.assertThat(matcher.toString()).isEqualTo("**");
             softly.assertThat(matcher.matches("")).isTrue();
             softly.assertThat(matcher.matches("foo")).isTrue();
         });
@@ -214,7 +320,7 @@ class WildcardMatcherTest {
 
     @Test
     void testMatchesPartitionedStringStartsWith_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)/foo/bar*");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("/foo/bar*");
         assertSoftly(softly -> {
             softly.assertThat(matcher.matches("/foo/bAR/Baz", "")).isTrue();
             softly.assertThat(matcher.matches("", "/foo/bAR/baz")).isTrue();
@@ -229,9 +335,9 @@ class WildcardMatcherTest {
 
     @Test
     void testMatchesEndsWith_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)*foo");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)*foo");
+            softly.assertThat(matcher.toString()).isEqualTo("*foo");
             softly.assertThat(matcher.matches("fOo")).isTrue();
             softly.assertThat(matcher.matches("foobar")).isFalse();
             softly.assertThat(matcher.matches("bar")).isFalse();
@@ -241,7 +347,7 @@ class WildcardMatcherTest {
 
     @Test
     void testMatchesPartitionedStringEndsWith_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)*/bar/baz");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*/bar/baz");
         assertSoftly(softly -> {
             softly.assertThat(matcher.matches("/foO/BAR/Baz", "")).isTrue();
             softly.assertThat(matcher.matches("", "/foO/Bar/baz")).isTrue();
@@ -255,9 +361,9 @@ class WildcardMatcherTest {
 
     @Test
     void testMatchesEquals_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)foo");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("foo");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)foo");
+            softly.assertThat(matcher.toString()).isEqualTo("foo");
             softly.assertThat(matcher.matches("fOo")).isTrue();
             softly.assertThat(matcher.matches("foOBar")).isFalse();
             softly.assertThat(matcher.matches("BAR")).isFalse();
@@ -267,9 +373,9 @@ class WildcardMatcherTest {
 
     @Test
     void testMatchesInfix_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)*foo*");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)*foo*");
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*");
             softly.assertThat(matcher.matches("FOO")).isTrue();
             softly.assertThat(matcher.matches("foOBar")).isTrue();
             softly.assertThat(matcher.matches("BAR")).isFalse();
@@ -279,41 +385,56 @@ class WildcardMatcherTest {
     }
 
     @Test
-    void testMatchesInfixPartitionedString_allocationFree_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)*foo*");
+    void testMatchesInfix_caseSensitive() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?-i)*foo*");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)*foo*");
+            softly.assertThat(matcher.toString()).isEqualTo("(?-i)*foo*");
+            softly.assertThat(matcher.matches("foo")).isTrue();
+            softly.assertThat(matcher.matches("FOO")).isFalse();
+        });
+    }
+
+    @Test
+    void testMatchesInfixPartitionedString_ignoreCase() {
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("*foo*");
+        assertSoftly(softly -> {
+            softly.assertThat(matcher.toString()).isEqualTo("*foo*");
             // no allocations necessary
             softly.assertThat(matcher.matches("foo", "BAR")).isTrue();
             softly.assertThat(matcher.matches("BAR", "foo")).isTrue();
             softly.assertThat(matcher.matches("baRFoo", "baz")).isTrue();
             softly.assertThat(matcher.matches("bA", "Rfoo")).isTrue();
-        });
-    }
-
-    @Test
-    void testMatchesInfixPartitionedString_notAllocationFree_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)*foo*");
-        assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)*foo*");
-            // requires concatenating the string
             softly.assertThat(matcher.matches("fo", "O")).isTrue();
             softly.assertThat(matcher.matches("barFO", "obaz")).isTrue();
-            softly.assertThat(matcher.matches("bar", "baz")).isFalse();
-        });
+            softly.assertThat(matcher.matches("bar", "baz")).isFalse();});
     }
 
     @Test
     void testMatchesNoWildcard_ignoreCase() {
-        final WildcardMatcher matcher = WildcardMatcher.valueOf("(?i)foo");
+        final WildcardMatcher matcher = WildcardMatcher.valueOf("foo");
         assertSoftly(softly -> {
-            softly.assertThat(matcher.toString()).isEqualTo("(?i)foo");
-            // requires concatenating the string
+            softly.assertThat(matcher.toString()).isEqualTo("foo");
             softly.assertThat(matcher.matches("FO", "O")).isTrue();
             softly.assertThat(matcher.matches("FOO")).isTrue();
             softly.assertThat(matcher.matches("foO", "Bar")).isFalse();
             softly.assertThat(matcher.matches("foobar")).isFalse();
 
+        });
+    }
+
+
+    @Test
+    void testNeedleLongerThanHaystack() {
+        assertSoftly(softly -> {
+            softly.assertThat(WildcardMatcher.valueOf("*foo").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("*foob").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("*fooba").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("*foobar").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("foo*").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("foob*").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("fooba*").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("foobar*").matches("baz")).isFalse();
+            softly.assertThat(WildcardMatcher.valueOf("*foobar*").matches("baz")).isFalse();
         });
     }
 
