3f54a3ef91d0cfad5887689a56bf453e18e8795b
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2018-11-19 12:31:38 +0100

    Make javac happy

--- apm-agent-plugins/apm-jdbc-plugin/src/main/java/co/elastic/apm/jdbc/ConnectionInstrumentation.java
+++ apm-agent-plugins/apm-jdbc-plugin/src/main/java/co/elastic/apm/jdbc/ConnectionInstrumentation.java
@@ -52,7 +52,7 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 public class ConnectionInstrumentation extends ElasticApmInstrumentation {
 
     @VisibleForAdvice
-    public static final Map<Object, String> statementSqlMap = Collections.synchronizedMap(new WeakHashMap<>());
+    public static final Map<Object, String> statementSqlMap = Collections.synchronizedMap(new WeakHashMap<Object, String>());
     static final String JDBC_INSTRUMENTATION_GROUP = "jdbc";
 
     @VisibleForAdvice
