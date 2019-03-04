14e1640e140041d9ff5bcb457e8bd8fe29d490a7
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2019-03-04 10:21:06 +0100

    Add explaining comment

--- apm-agent-core/src/main/java/co/elastic/apm/agent/metrics/builtin/JvmGcMetrics.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/metrics/builtin/JvmGcMetrics.java
@@ -61,6 +61,8 @@ public class JvmGcMetrics implements LifecycleListener {
         try {
             // only refer to hotspot specific class via reflection to avoid linkage errors
             final Class<?> sunBeanClass = Class.forName("com.sun.management.ThreadMXBean");
+            // J9 does contain com.sun.management.ThreadMXBean in classpath
+            // but the actual MBean it uses (com.ibm.lang.management.internal.ExtendedThreadMXBeanImpl) does not implement it
             if (sunBeanClass.isInstance(ManagementFactory.getThreadMXBean())) {
                 // in reference to JMH's GC profiler (gc.alloc.rate)
                 registry.add("jvm.gc.alloc", Collections.<String, String>emptyMap(),
