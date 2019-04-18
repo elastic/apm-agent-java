c4ab0f760d1b8bc5797ae13f8f20f99532917080
Martin Dindoffer <contact@dindoffer.eu>
2019-04-18 15:18:37 +0200

    Exclude $view proxies from JAX-RS instrumentation

--- apm-agent-plugins/apm-jaxrs-plugin/src/test/java/co/elastic/apm/agent/jaxrs/JaxRsTransactionNameInstrumentationTest.java
+++ apm-agent-plugins/apm-jaxrs-plugin/src/test/java/co/elastic/apm/agent/jaxrs/JaxRsTransactionNameInstrumentationTest.java
@@ -104,12 +104,31 @@ public class JaxRsTransactionNameInstrumentationTest extends JerseyTest {
         assertThat(actualTransactions.get(2).getName().toString()).isEqualTo("ResourceWithPathOnAbstract#testMethod");
     }
 
+    @Test
+    public void testProxyClassInstrumentationExclusion() {
+        when(config.getConfig(JaxRsConfiguration.class).isEnableJaxrsAnnotationInheritance()).thenReturn(true);
+        ElasticApmAgent.initInstrumentation(tracer, ByteBuddyAgent.install());
+
+        doRequest("testViewProxy");
+        doRequest("testProxyProxy");
+
+        List<Transaction> actualTransactions = reporter.getTransactions();
+        assertThat(actualTransactions).hasSize(2);
+        assertThat(actualTransactions.get(0).getName().toString()).isEqualTo("unnamed");
+        assertThat(actualTransactions.get(1).getName().toString()).isEqualTo("unnamed");
+    }
+
 
     /**
      * @return configuration for the jersey test server. Includes all resource classes in the co.elastic.apm.agent.jaxrs.resources package.
      */
+    @Override
     protected Application configure() {
-        return new ResourceConfig(ResourceWithPath.class, ResourceWithPathOnInterface.class, ResourceWithPathOnAbstract.class);
+        return new ResourceConfig(ResourceWithPath.class,
+                ResourceWithPathOnInterface.class,
+                ResourceWithPathOnAbstract.class,
+                ProxiedClass$view.class,
+                ProxiedClass$Proxy.class);
     }
 
     /**
@@ -134,7 +153,6 @@ public class JaxRsTransactionNameInstrumentationTest extends JerseyTest {
     @Path("testInterface")
     public interface ResourceInterfaceWithPath extends SuperResourceInterface {
         String testMethod();
-
     }
 
     public interface ResourceInterfaceWithoutPath extends SuperResourceInterface {
@@ -142,13 +160,24 @@ public class JaxRsTransactionNameInstrumentationTest extends JerseyTest {
     }
 
     public abstract static class AbstractResourceClassWithoutPath implements ResourceInterfaceWithoutPath {
-
     }
 
     @Path("testAbstract")
     public abstract static class AbstractResourceClassWithPath implements ResourceInterfaceWithoutPath {
+    }
 
+    @Path("testViewProxy")
+    public static class ProxiedClass$view implements SuperResourceInterface {
+        public String testMethod() {
+            return "ok";
+        }
+    }
 
+    @Path("testProxyProxy")
+    public static class ProxiedClass$Proxy implements SuperResourceInterface {
+        public String testMethod() {
+            return "ok";
+        }
     }
 
     @Path("test")
@@ -156,7 +185,6 @@ public class JaxRsTransactionNameInstrumentationTest extends JerseyTest {
         public String testMethod() {
             return "ok";
         }
-
     }
 
     public static class ResourceWithPathOnAbstract extends AbstractResourceClassWithPath {
