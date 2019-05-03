b5811ca5a7af7c91bd84824bc661c77913fb55b5
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2019-05-03 14:52:11 +0200

    Instrument instead of wrap AsyncHandler
    Associate AsyncHandlers with spans via WeakConcurrentMap

---
+++ apm-agent-plugins/apm-asynchttpclient-plugin/src/main/java/co/elastic/apm/agent/asynchttpclient/AbstractAsyncHttpClientInstrumentation.java
@@ -0,0 +1,180 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 - 2019 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
+package co.elastic.apm.agent.asynchttpclient;
+
+import co.elastic.apm.agent.bci.ElasticApmInstrumentation;
+import co.elastic.apm.agent.bci.VisibleForAdvice;
+import co.elastic.apm.agent.http.client.HttpClientHelper;
+import co.elastic.apm.agent.impl.transaction.Span;
+import co.elastic.apm.agent.impl.transaction.TraceContext;
+import co.elastic.apm.agent.impl.transaction.TraceContextHolder;
+import com.blogspot.mydailyjava.weaklockfree.WeakConcurrentMap;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import org.asynchttpclient.AsyncHandler;
+import org.asynchttpclient.HttpResponseStatus;
+import org.asynchttpclient.Request;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static co.elastic.apm.agent.bci.bytebuddy.CustomElementMatchers.classLoaderCanLoadClass;
+import static net.bytebuddy.matcher.ElementMatchers.hasSuperType;
+import static net.bytebuddy.matcher.ElementMatchers.isBootstrapClassLoader;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+public abstract class AbstractAsyncHttpClientInstrumentation extends ElasticApmInstrumentation {
+
+    @VisibleForAdvice
+    public static final WeakConcurrentMap<AsyncHandler<?>, Span> handlerSpanMap = new WeakConcurrentMap.WithInlinedExpunction<>();
+
+    @Override
+    public Collection<String> getInstrumentationGroupNames() {
+        return Arrays.asList("http-client", "asynchttpclient");
+    }
+
+    @Override
+    public ElementMatcher.Junction<ClassLoader> getClassLoaderMatcher() {
+        return not(isBootstrapClassLoader())
+            .and(classLoaderCanLoadClass("org.asynchttpclient.AsyncHandler"));
+    }
+
+    public static class AsyncHttpClientInstrumentation extends AbstractAsyncHttpClientInstrumentation {
+
+        @Advice.OnMethodEnter(suppress = Throwable.class)
+        private static void onBeforeExecute(@Advice.Argument(value = 0) Request request,
+                                            @Advice.Argument(value = 1) AsyncHandler<?> asyncHandler,
+                                            @Advice.Local("span") Span span) {
+            if (tracer == null || tracer.getActive() == null) {
+                return;
+            }
+
+            final TraceContextHolder<?> parent = tracer.getActive();
+            span = HttpClientHelper.startHttpClientSpan(parent, request.getMethod(), request.getUri().toUrl(), request.getUri().getHost());
+
+            if (span != null) {
+                span.activate();
+                request.getHeaders().add(TraceContext.TRACE_PARENT_HEADER, span.getTraceContext().getOutgoingTraceParentHeader().toString());
+                handlerSpanMap.put(asyncHandler, span);
+            }
+        }
+
+        @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
+        private static void onAfterExecute(@Advice.Local("span") @Nullable Span span,
+                                           @Advice.Argument(value = 1) AsyncHandler<?> asyncHandler,
+                                           @Advice.Thrown @Nullable Throwable t) {
+            if (span != null) {
+                span.deactivate();
+                if (t != null) {
+                    handlerSpanMap.remove(asyncHandler);
+                    span.captureException(t).end();
+                }
+            }
+        }
+
+        @Override
+        public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+            return named("org.asynchttpclient.DefaultAsyncHttpClient");
+        }
+
+        @Override
+        public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+            return named("executeRequest")
+                .and(takesArgument(0, named("org.asynchttpclient.Request")))
+                .and(takesArgument(1, named("org.asynchttpclient.AsyncHandler")));
+        }
+    }
+
+    public abstract static class AbstractAsyncHandlerInstrumentation extends AbstractAsyncHttpClientInstrumentation {
+
+        private final ElementMatcher<? super MethodDescription> methodMatcher;
+
+        protected AbstractAsyncHandlerInstrumentation(ElementMatcher<? super MethodDescription> methodMatcher) {
+            this.methodMatcher = methodMatcher;
+        }
+
+        @Override
+        public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+            return hasSuperType(named("org.asynchttpclient.AsyncHandler"));
+        }
+
+        @Override
+        public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+            return methodMatcher;
+        }
+    }
+
+    public static class AsyncHandlerOnCompletedInstrumentation extends AbstractAsyncHandlerInstrumentation {
+
+        public AsyncHandlerOnCompletedInstrumentation() {
+            super(named("onCompleted").and(takesArguments(0)));
+        }
+
+        @Advice.OnMethodEnter(suppress = Throwable.class)
+        private static void onMethodEnter(@Advice.This AsyncHandler<?> asyncHandler) {
+            final Span span = handlerSpanMap.remove(asyncHandler);
+            if (span != null) {
+                span.end();
+            }
+        }
+    }
+
+    public static class AsyncHandlerOnThrowableInstrumentation extends AbstractAsyncHandlerInstrumentation {
+
+        public AsyncHandlerOnThrowableInstrumentation() {
+            super(named("onThrowable").and(takesArguments(Throwable.class)));
+        }
+
+        @Advice.OnMethodEnter(suppress = Throwable.class)
+        private static void onMethodEnter(@Advice.This AsyncHandler<?> asyncHandler, @Advice.Argument(0) Throwable t) {
+            final Span span = handlerSpanMap.remove(asyncHandler);
+            if (span != null) {
+                span.captureException(t).end();
+            }
+        }
+    }
+
+    public static class AsyncHandlerOnStatusReceivedInstrumentation extends AbstractAsyncHandlerInstrumentation {
+
+        public AsyncHandlerOnStatusReceivedInstrumentation() {
+            super(named("onStatusReceived").and(takesArgument(0, named("org.asynchttpclient.HttpResponseStatus"))));
+        }
+
+        @Advice.OnMethodEnter(suppress = Throwable.class)
+        private static void onMethodEnter(@Advice.This AsyncHandler<?> asyncHandler, @Advice.Argument(0) HttpResponseStatus status) {
+            final Span span = handlerSpanMap.get(asyncHandler);
+            if (span != null) {
+                span.getContext().getHttp().withStatusCode(status.getStatusCode());
+            }
+        }
+    }
+
+}
--- apm-agent-plugins/apm-asynchttpclient-plugin/src/test/java/co/elastic/apm/agent/asynchttpclient/AsyncHttpClientInstrumentationTest.java
+++ apm-agent-plugins/apm-asynchttpclient-plugin/src/test/java/co/elastic/apm/agent/asynchttpclient/AsyncHttpClientInstrumentationTest.java
@@ -4,34 +4,34 @@
  * %%
  * Copyright (C) 2018 - 2019 Elastic and contributors
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  * #L%
  */
 package co.elastic.apm.agent.asynchttpclient;
 
 import co.elastic.apm.agent.httpclient.AbstractHttpClientInstrumentationTest;
-import io.netty.handler.codec.http.HttpHeaders;
-import org.asynchttpclient.*;
-import org.junit.Assert;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Dsl;
+import org.asynchttpclient.RequestBuilder;
+import org.junit.After;
+import org.junit.Before;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 
@@ -39,70 +39,23 @@ public class AsyncHttpClientInstrumentationTest extends AbstractHttpClientInstru
 
     private AsyncHttpClient client;
 
-    @BeforeEach
-    void setUp() {
-        AsyncHttpClientConfig config = Dsl.config()
+    @Before
+    public void setUp() {
+        client = asyncHttpClient(Dsl.config()
             .setFollowRedirect(true)
-            .build();
-        client = asyncHttpClient(config);
+            .build());
     }
 
-    @AfterEach
-    void tearDown() {
-        try {
-            if(client != null) {
-                client.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @After
+    public void tearDown() throws IOException {
+        client.close();
     }
 
     @Override
-    protected void performGet(String path) throws ExecutionException, InterruptedException, TimeoutException { ;
-        Request request = new RequestBuilder()
+    protected void performGet(String path) throws Exception {
+        client.executeRequest(new RequestBuilder()
             .setUrl(path)
-            .build();
-
-        AsyncHandler asyncHandler = new AsyncHandler<Object>() {
-            private final Logger logger = LoggerFactory.getLogger(AsyncHandler.class);
-
-            @Override
-            public State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
-                logger.info("ON_BODY_PART_RECEIVED");
-                return State.CONTINUE;
-            }
-
-            @Override
-            public void onThrowable(Throwable t) { }
-
-            @Override
-            public Object onCompleted() throws Exception {
-                logger.info("ON_COMPLETED");
-                return null;
-            }
-
-            @Override
-            public State onStatusReceived(HttpResponseStatus responseStatus) throws Exception {
-                logger.info("ON_STATUS_RECEIVED: " + responseStatus.getStatusCode());
-                return State.CONTINUE;
-            }
-
-            @Override
-            public State onHeadersReceived(HttpHeaders headers) throws Exception {
-                logger.info("ON_HEADERS_RECEIVED: " + headers.toString());
-                return State.CONTINUE;
-            }
-        };
-
-        asyncHttpClient()
-            .executeRequest(request).get();
+            .build()).get();
     }
 
-    @Test
-    void testGetAdviceClassReturnsAsyncInstrumentationClass() {
-        AsyncHttpClientInstrumentation asyncHttpClientInstrumentation = new AsyncHttpClientInstrumentation();
-
-        Assert.assertEquals(asyncHttpClientInstrumentation.getAdviceClass(), AsyncHttpClientInstrumentation.AsyncHttpClientInstrumentationAdvice.class);
-    }
 }
