8cc9f1633ea087c2fcb3bb1e33c4bf3bee0fcfbd
Felix Barnsteiner <felixbarny@users.noreply.github.com>
2019-05-28 10:03:42 +0200

    Sentence part II - the final endeavour

--- apm-agent-attach/src/main/java/co/elastic/apm/attach/CachedAttachmentProvider.java
+++ apm-agent-attach/src/main/java/co/elastic/apm/attach/CachedAttachmentProvider.java
@@ -30,7 +30,8 @@ import net.bytebuddy.agent.ByteBuddyAgent;
  * Successive attachments would lead to the VirtualMachine class to be loaded from multiple class loaders
  * (see net.bytebuddy.agent.ByteBuddyAgent.AttachmentProvider.ForStandardToolsJarVm#attempt()).
  * That leads to a UnsatisfiedLinkError on Java 7 and 9 because the native library libattach can only be loaded by one class loader.
- * By caching the {@link net.bytebuddy.agent.ByteBuddyAgent.AttachmentProvider.Accessor}, the same
+ * By caching the {@link net.bytebuddy.agent.ByteBuddyAgent.AttachmentProvider.Accessor},
+ * the same VirtualMachine class is reused so that there is no attempt to load the libattach library from another class loader.
  */
 public class CachedAttachmentProvider implements ByteBuddyAgent.AttachmentProvider {
     private final Accessor accessor;
