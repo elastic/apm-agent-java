77eef72d59f694cc9e7e14abba5805a18eef9f57
theobisproject <theobisproject@gmail.com>
2019-07-01 17:44:26 +0200

    Validate more span information

--- apm-agent-plugins/apm-hibernate-search-plugin/apm-hibernate-search-plugin-common/src/test/java/co/elastic/apm/agent/hibernate/search/HibernateSearchAssertionHelper.java
+++ apm-agent-plugins/apm-hibernate-search-plugin/apm-hibernate-search-plugin-common/src/test/java/co/elastic/apm/agent/hibernate/search/HibernateSearchAssertionHelper.java
@@ -11,9 +11,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -25,6 +25,7 @@
 package co.elastic.apm.agent.hibernate.search;
 
 import co.elastic.apm.agent.MockReporter;
+import co.elastic.apm.agent.impl.transaction.Span;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,9 +37,11 @@ public final class HibernateSearchAssertionHelper {
 
     public static void assertApmSpanInformation(final MockReporter reporter, final String expectedQuery) {
         assertEquals(1, reporter.getSpans().size(), "Didn't find 1 span");
-        assertEquals("hibernate-search", reporter.getFirstSpan().getSubtype(),
-            "Subtype of span is not 'hibernate-search'");
-        assertEquals(expectedQuery, reporter.getFirstSpan().getContext().getDb().getStatement(),
-            "Statement is not '" + expectedQuery + "'");
+        final Span span = reporter.getFirstSpan();
+        assertEquals("hibernate-search", span.getSubtype(), "Subtype of span is not 'hibernate-search'");
+        assertEquals(expectedQuery, span.getContext().getDb().getStatement(),"Statement is not '" + expectedQuery + "'");
+        assertEquals("db", span.getType());
+        assertEquals("request", span.getAction());
+        assertEquals("Hibernate Search", span.getName().toString());
     }
 }
