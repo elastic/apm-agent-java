8193cbe46c5dbea6d4791cd3916b5e9bad33524f
kananindzya <kananindzya@gmail.com>
2019-07-31 17:10:57 +0600

    removed unused imports. fixed test

--- apm-agent-plugins/apm-spring-webmvc-plugin/src/test/java/co/elastic/apm/agent/spring/webmvc/ExceptionHandlerInstrumentationWithResponseStatusExceptionTest.java
+++ apm-agent-plugins/apm-spring-webmvc-plugin/src/test/java/co/elastic/apm/agent/spring/webmvc/ExceptionHandlerInstrumentationWithResponseStatusExceptionTest.java
@@ -1,3 +1,27 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 - 2019 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
 package co.elastic.apm.agent.spring.webmvc;
 
 import co.elastic.apm.agent.MockReporter;
@@ -80,7 +104,7 @@ public class ExceptionHandlerInstrumentationWithResponseStatusExceptionTest {
         assertEquals(1, reporter.getTransactions().size());
         assertEquals(0, reporter.getSpans().size());
         assertEquals(1, reporter.getErrors().size());
-        assertEquals("responseStatusException", reporter.getErrors().get(0).getException().getMessage());
+        assertEquals("Response status 409 with reason \"responseStatusException\"; nested exception is java.lang.RuntimeException: runtime exception occured", reporter.getErrors().get(0).getException().getMessage());
         assertEquals("runtime exception occured", reporter.getErrors().get(0).getException().getCause().getMessage());
     }
 }
