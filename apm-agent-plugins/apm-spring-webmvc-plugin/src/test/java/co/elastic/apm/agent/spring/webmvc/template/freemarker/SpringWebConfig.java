1307e1205c11239adeec37d39ede32a010a68125
kananindzya <kananindzya@gmail.com>
2019-09-02 10:09:51 +0600

    added some test with free marker

---
+++ apm-agent-plugins/apm-spring-webmvc-plugin/src/test/java/co/elastic/apm/agent/spring/webmvc/template/freemarker/SpringWebConfig.java
@@ -0,0 +1,29 @@
+package co.elastic.apm.agent.spring.webmvc.template.freemarker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@EnableWebMvc
+@Configuration
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("/static/template/freemarker/");
+        return freeMarkerConfigurer;
+    }
+}
