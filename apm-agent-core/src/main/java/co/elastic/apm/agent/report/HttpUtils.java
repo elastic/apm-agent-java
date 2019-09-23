a4d66fc7c6e1b217f4ef21e8e6a9615388166b8c
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2019-09-23 13:28:09 +0200

    Revert "deleted unecessary calling closeInputStream method from IntakeV2ReportingEventHandler"
    This reverts commit 88fdefe677eae97e464e403f80c44e565e335e4c.

--- apm-agent-core/src/main/java/co/elastic/apm/agent/report/HttpUtils.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/report/HttpUtils.java
@@ -26,7 +26,9 @@ package co.elastic.apm.agent.report;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.stagemonitor.util.IOUtils;
 
+import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -70,5 +72,4 @@ public class HttpUtils {
         bufferedReader.close();
         return bodyString.toString();
     }
-
 }
