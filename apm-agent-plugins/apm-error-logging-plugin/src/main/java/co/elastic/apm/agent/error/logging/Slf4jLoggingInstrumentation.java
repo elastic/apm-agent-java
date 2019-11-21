1b30057552bb2e9ecebe62b5a72a129c458ca818
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2019-11-21 09:48:14 +0200

    Suggested approach for handling nested error invocations

--- apm-agent-plugins/apm-error-logging-plugin/src/main/java/co/elastic/apm/agent/error/logging/Slf4jLoggingInstrumentation.java
+++ apm-agent-plugins/apm-error-logging-plugin/src/main/java/co/elastic/apm/agent/error/logging/Slf4jLoggingInstrumentation.java
@@ -24,44 +24,16 @@
  */
 package co.elastic.apm.agent.error.logging;
 
-import co.elastic.apm.agent.bci.ElasticApmInstrumentation;
-import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.NamedElement;
-import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import static net.bytebuddy.matcher.ElementMatchers.hasSuperType;
-import static net.bytebuddy.matcher.ElementMatchers.nameContains;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.not;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
-public class Slf4jLoggingInstrumentation extends ElasticApmInstrumentation {
-
-    @Override
-    public Class<?> getAdviceClass() {
-        return Slf4jLoggingAdviceService.class;
-    }
-
-    public static class Slf4jLoggingAdviceService {
-
-        @Advice.OnMethodEnter(suppress = Throwable.class)
-        public static void logEnter(@Advice.Argument(1) Throwable exception) {
-            if (tracer == null || tracer.getActive() == null) {
-                return;
-            }
-            tracer.getActive().captureException(exception);
-        }
-    }
-
-    @Override
-    public ElementMatcher<? super NamedElement> getTypeMatcherPreFilter() {
-        return nameContains("Logger");
-    }
+public class Slf4jLoggingInstrumentation extends AbstractLoggingInstrumentation {
 
     @Override
     public ElementMatcher<? super TypeDescription> getTypeMatcher() {
@@ -69,14 +41,9 @@ public class Slf4jLoggingInstrumentation extends ElasticApmInstrumentation {
     }
 
     @Override
-    public ElementMatcher<? super MethodDescription> getMethodMatcher() {
-        return named("error")
-            .and(takesArgument(0, named("java.lang.String"))
-                .and(takesArgument(1, named("java.lang.Throwable"))));
-    }
-
-    @Override
     public Collection<String> getInstrumentationGroupNames() {
-        return Arrays.asList("logging", "slf4j");
+        Collection<String> ret = super.getInstrumentationGroupNames();
+        ret.add("slf4j");
+        return ret;
     }
 }
