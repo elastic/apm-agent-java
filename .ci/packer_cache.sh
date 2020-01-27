97019c88d999fd20e03630acd2d6adf8c02c7ef3
Mike Place <mike.place@elastic.co>
2020-01-27 13:04:17 +0100

    Create Maven cache

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -3,5 +3,7 @@ source /usr/local/bin/bash_standard_lib.sh
 
 JAVA_HOME=$HOME/.java/java10 ./mvnw clean verify --fail-never
 
+cache_maven_deps
+
 (retry 2 docker pull docker.elastic.co/observability-ci/weblogic:12.2.1.3-dev) \
   && docker tag docker.elastic.co/observability-ci/weblogic:12.2.1.3-dev store/oracle/weblogic:12.2.1.3-dev
