0afc44ccb9a54338c664efcb0965aaba88e12715
kananindzya <kananindzya@gmail.com>
2020-01-29 21:10:56 +0600

    miss imports removed

--- apm-agent-core/src/main/java/co/elastic/apm/agent/impl/ElasticApmTracer.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/impl/ElasticApmTracer.java
@@ -11,9 +11,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -62,6 +62,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
+
 
 /**
  * This is the tracer implementation which provides access to lower level agent functionality.
