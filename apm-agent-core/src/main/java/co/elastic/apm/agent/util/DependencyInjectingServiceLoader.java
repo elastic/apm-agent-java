cad7be905b9c4684e516d9575b4c1036bf1cdb49
Sylvain Juge <sylvain.juge@elastic.co>
2020-02-04 09:41:49 +0100

    improve error message to help troubleshooting
    This is quite common when we try to have an instrumentation that is an abstract class, thus making it obvious helps

--- apm-agent-core/src/main/java/co/elastic/apm/agent/util/DependencyInjectingServiceLoader.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/util/DependencyInjectingServiceLoader.java
@@ -11,9 +11,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -114,6 +114,8 @@ public class DependencyInjectingServiceLoader<T> {
             } else {
                 return clazz.cast(implementationClass.getConstructor().newInstance());
             }
+        } catch (InstantiationException e) {
+            throw new ServiceConfigurationError(String.format("unable to instantiate '%s' class, please check descriptor in META-INF and class declaration ", implementation), e);
         } catch (Exception e) {
             throw new ServiceConfigurationError(e.getMessage(), e);
         }
