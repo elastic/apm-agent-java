1bf97cf1ecfaa91932bebedf18776752680b32ff
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2020-02-10 07:03:35 +0200

    Make Process, JMS and Kafka plugins non-incubating

--- apm-agent-plugins/apm-kafka-plugin/apm-kafka-base-plugin/src/main/java/co/elastic/apm/agent/kafka/BaseKafkaInstrumentation.java
+++ apm-agent-plugins/apm-kafka-plugin/apm-kafka-base-plugin/src/main/java/co/elastic/apm/agent/kafka/BaseKafkaInstrumentation.java
@@ -11,9 +11,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -36,8 +36,8 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 import static co.elastic.apm.agent.bci.bytebuddy.CustomElementMatchers.classLoaderCanLoadClass;
 import static net.bytebuddy.matcher.ElementMatchers.isBootstrapClassLoader;
@@ -73,7 +73,7 @@ public abstract class BaseKafkaInstrumentation extends ElasticApmInstrumentation
     @Override
     public Collection<String> getInstrumentationGroupNames() {
         // Incubating until we implement traceparent binary format
-        return Arrays.asList("kafka", "incubating");
+        return Collections.singletonList("kafka");
     }
 
     @Override
