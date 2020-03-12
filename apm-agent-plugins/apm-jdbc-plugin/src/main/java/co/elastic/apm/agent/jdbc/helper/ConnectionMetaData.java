394fda5083a06e00aab275eae658ea7ad79c6cb8
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2020-03-12 10:26:47 +0200

    Enhance JDBC connection string parsing for multiple hosts

--- apm-agent-plugins/apm-jdbc-plugin/src/main/java/co/elastic/apm/agent/jdbc/helper/ConnectionMetaData.java
+++ apm-agent-plugins/apm-jdbc-plugin/src/main/java/co/elastic/apm/agent/jdbc/helper/ConnectionMetaData.java
@@ -28,8 +28,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConnectionMetaData {
 
@@ -83,7 +90,7 @@ public class ConnectionMetaData {
             try {
                 ret = connectionUrlParser.parse(tmpUrl, user);
             } catch (Exception e) {
-                logger.error("Failed to parse connection URL", e);
+                logger.error("Failed to parse connection URL: " + tmpUrl, e);
             }
         } else {
             // Doesn't hurt to try...
@@ -152,10 +159,13 @@ public class ConnectionMetaData {
                 // jdbc:oracle:thin:scott/tiger@localhost:myinstance
                 // jdbc:oracle:oci:scott/tiger/@
                 // jdbc:oracle:thin:@ldap://ldap.acme.com:7777/sales,cn=OracleContext,dc=com
+                // jdbc:oracle:thin:@(DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS=(PROTOCOL=TCP)(HOST=host1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=service_name)))
+                // jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=cluster_alias)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=service_name)))
+                // jdbc:oracle:thin:@(DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=host1)(PORT=1521))(ADDRESS=(PROTOCOL=TCP)(HOST=host2)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=service_name)))
                 int indexOfUserDetailsEnd = connectionUrl.indexOf('@');
                 if (indexOfUserDetailsEnd > 0) {
                     if (connectionUrl.length() > indexOfUserDetailsEnd + 1) {
-                        connectionUrl = connectionUrl.substring(indexOfUserDetailsEnd + 1);
+                        connectionUrl = connectionUrl.substring(indexOfUserDetailsEnd + 1).trim();
                     } else {
                         // jdbc:oracle:oci:scott/tiger/@
                         return new ConnectionMetaData(dbVendor, null, DEFAULT_PORT, user);
@@ -165,36 +175,136 @@ public class ConnectionMetaData {
                 String host = null;
                 int port = DEFAULT_PORT;
 
-                // try looking for a //host:port/instance pattern
-                HostPort hostPort = parseHostPort(connectionUrl);
-                if (hostPort.host != null) {
-                    host = hostPort.host;
-                    if (hostPort.port > 0) {
-                        port = hostPort.port;
+                HostPort hostPort;
+                if (connectionUrl.startsWith("(")) {
+                    // (DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS=(PROTOCOL=TCP)(HOST=host1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=service_name)))
+                    // (DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=host1)(PORT=1521))(ADDRESS=(PROTOCOL=TCP)(HOST=host2)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=service_name)))
+                    try {
+                        hostPort = parseAddressList(connectionUrl);
+                        if (hostPort.host != null) {
+                            host = hostPort.host;
+                            if (hostPort.port > 0) {
+                                port = hostPort.port;
+                            }
+                        }
+                    } catch (Exception e) {
+                        logger.warn("Failed to parse address from this address list: {}", connectionUrl);
+                        port = -1;
                     }
                 } else {
-                    // Thin driver host:port:sid syntax:
-                    // myhost:666:instance
-                    // myhost:instance
-                    // thin:myhost:port:instance
-                    if (connectionUrl.startsWith("thin:")) {
-                        connectionUrl = connectionUrl.substring("thin:".length());
-                    }
+                    // try looking for a //host:port/instance pattern
+                    hostPort = parseHostPort(connectionUrl);
+                    if (hostPort.host != null) {
+                        host = hostPort.host;
+                        if (hostPort.port > 0) {
+                            port = hostPort.port;
+                        }
+                    } else {
+                        // Thin driver host:port:sid syntax:
+                        // myhost:666:instance
+                        // myhost:instance
+                        // thin:myhost:port:instance
+                        if (connectionUrl.startsWith("thin:")) {
+                            connectionUrl = connectionUrl.substring("thin:".length());
+                        }
 
-                    String[] parts = connectionUrl.split(":");
-                    if (parts.length > 0) {
-                        host = parts[0];
-                    }
-                    if (parts.length > 1) {
-                        try {
-                            port = Integer.parseInt(parts[1]);
-                        } catch (NumberFormatException e) {
-                            // apparently not a port...
+                        String[] parts = connectionUrl.split(":");
+                        if (parts.length > 0) {
+                            host = parts[0];
+                        }
+                        if (parts.length > 1) {
+                            try {
+                                port = Integer.parseInt(parts[1]);
+                            } catch (NumberFormatException e) {
+                                // apparently not a port...
+                            }
                         }
                     }
                 }
 
                 return new ConnectionMetaData(dbVendor, host, port, user);
+            }
+
+            private HostPort parseAddressList(String connectionUrl) {
+                TreeNode parsedTree = null;
+                Deque<TreeNode> stack = new ArrayDeque<>();
+                StringBuilder currentValueBuffer = null;
+                for (char c : connectionUrl.toLowerCase().toCharArray()) {
+                    switch (c) {
+                        case '(': {
+                            TreeNode treeNode = new TreeNode();
+                            if (stack.isEmpty()) {
+                                parsedTree = treeNode;
+                            } else {
+                                stack.peek().childNodes.add(treeNode);
+                            }
+                            stack.push(treeNode);
+                            currentValueBuffer = treeNode.name;
+                            break;
+                        }
+                        case ')': {
+                            stack.pop();
+                            // continue to do the same as finding `=`
+                        }
+                        case '=': {
+                            if (stack.isEmpty()) {
+                                currentValueBuffer = null;
+                            } else {
+                                currentValueBuffer = stack.peek().value;
+                            }
+                            break;
+                        }
+                        default: {
+                            if (currentValueBuffer == null) {
+                                logger.warn("Failed to parse Oracle DB address list from: {}", connectionUrl);
+                            } else {
+                                currentValueBuffer.append(c);
+                            }
+                        }
+                    }
+                }
+
+                HostPort ret = null;
+                if (parsedTree == null) {
+                    logger.warn("Failed to parse Oracle DB address list from: {}", connectionUrl);
+                } else {
+                    ret = findAddressInTree(parsedTree);
+                }
+                return (ret != null) ? ret : new HostPort(null, -1);
+            }
+
+            @Nullable
+            HostPort findAddressInTree(TreeNode treeNode) {
+                if (treeNode.name.toString().trim().equals("address")) {
+                    String host = null;
+                    int port = -1;
+                    for (TreeNode childNode : treeNode.childNodes) {
+                        String name = childNode.name.toString().trim();
+                        if (name.equals("host")) {
+                            host = childNode.value.toString().trim();
+                        } else if (name.equals("port")) {
+                            port = Integer.parseInt(childNode.value.toString().trim());
+                        }
+                    }
+                    if (host != null) {
+                        return new HostPort(host, port);
+                    }
+                }
+
+                HostPort ret = null;
+                for (TreeNode childNode : treeNode.childNodes) {
+                    ret = findAddressInTree(childNode);
+                    if (ret != null) {
+                        break;
+                    }
+                }
+                return ret;
+            }
+
+            class TreeNode {
+                StringBuilder name = new StringBuilder();
+                StringBuilder value = new StringBuilder();
+                List<TreeNode> childNodes = new ArrayList<>();
             }
         },
 
@@ -208,7 +318,16 @@ public class ConnectionMetaData {
         MYSQL("mysql") {
             @Override
             ConnectionMetaData parse(String connectionUrl, String user) {
-                return ConnectionUrlParser.defaultParse(connectionUrl, dbVendor, 3306, user);
+                String host = "localhost";
+                int port = 3306;
+                HostPort hostPort = parseMySqlFlavor(connectionUrl);
+                if (hostPort != null) {
+                    host = hostPort.host;
+                    if (hostPort.port > 0) {
+                        port = hostPort.port;
+                    }
+                }
+                return new ConnectionMetaData(dbVendor, host, port, user);
             }
         },
 
@@ -242,17 +361,29 @@ public class ConnectionMetaData {
         },
 
         MARIADB("mariadb") {
+
+            final List<String> SPECIALIZED_PROTOCOL_STRINGS = new ArrayList<>(Arrays.asList(
+                // https://mariadb.com/kb/en/failover-and-high-availability-with-mariadb-connector-j/#configuration
+                "sequential:", "loadbalance:", "failover:", "replication:", "aurora:"
+            ));
+
             @Override
             ConnectionMetaData parse(String connectionUrl, String user) {
                 // just like MySQL
                 String host = "localhost";
                 int port = 3306;
-                HostPort hostPort = parseHostPort(connectionUrl);
-                if (hostPort.host == null) {
-                    // but may also allow a non-proper URL format, like jdbc:mariadb:myhost:666
-                    hostPort = parseAuthority(connectionUrl);
+                // seems to allow a non-proper URL format, like jdbc:mariadb:myhost:666 or jdbc:mariadb:sequential:host1,host2,host3/testdb
+                for (String protocol : SPECIALIZED_PROTOCOL_STRINGS) {
+                    int indexOfProtocol = connectionUrl.indexOf(protocol);
+                    if (indexOfProtocol >= 0) {
+                        connectionUrl = connectionUrl.substring(indexOfProtocol + protocol.length());
+                    }
                 }
-                if (hostPort.host != null) {
+                if (!connectionUrl.contains("//")) {
+                    connectionUrl = "//" + connectionUrl;
+                }
+                HostPort hostPort = parseMySqlFlavor(connectionUrl);
+                if (hostPort != null) {
                     host = hostPort.host;
                     if (hostPort.port > 0) {
                         port = hostPort.port;
@@ -358,7 +489,7 @@ public class ConnectionMetaData {
                 if (indexOfDoubleSlash >= 0 && url.length() > indexOfDoubleSlash + 2) {
                     url = url.substring(indexOfDoubleSlash + 2);
                     if (url.length() == 1) {
-                        // for urls such as: jdbc:mysql:///
+                        // for urls such as: jdbc:hsqldb:///
                         return new HostPort("localhost", -1);
                     }
                     return parseAuthority(url);
@@ -418,6 +549,125 @@ public class ConnectionMetaData {
             }
 
             return new HostPort(host, port);
+        }
+
+        @Nullable static HostPort parseMySqlFlavor(String connectionUrl) {
+            // https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-jdbc-url-format.html
+            // General structure:
+            // protocol//[hosts][/database][?properties]
+
+            // Single host:
+            // jdbc:mysql://host1:33060/sakila
+            // jdbc:mysql://host1:33060/sakila?prop=val
+            // jdbc:mysql://host1:33060?prop=val
+            // jdbc:mysql://address=(host=myhost)(port=1111)(key1=value1)/db
+            // jdbc:mysql://(host=myhost,port=1111,key1=value1)/db
+
+            // Multiple hosts:
+            // jdbc:mysql://myhost1:1111,myhost2:2222/db
+            // jdbc:mysql://address=(host=myhost1)(port=1111)(key1=value1),address=(host=myhost2)(port=2222)(key2=value2)/db
+            // jdbc:mysql://(host=myhost1,port=1111,key1=value1),(host=myhost2,port=2222,key2=value2)/db
+            // jdbc:mysql://myhost1:1111,(host=myhost2,port=2222,key2=value2)/db
+            // jdbc:mysql://sandy:secret@[myhost1:1111,myhost2:2222]/db
+            // jdbc:mysql://sandy:secret@[address=(host=myhost1)(port=1111)(key1=value1),address=(host=myhost2)(port=2222)(key2=value2)]/db
+            // jdbc:mysql://sandy:secret@[myhost1:1111,address=(host=myhost2)(port=2222)(key2=value2)]/db
+
+            HostPort ret = null;
+            connectionUrl = connectionUrl.toLowerCase().trim();
+            final Pattern pattern = Pattern.compile("//([^/?]+)");
+            Matcher matcher = pattern.matcher(connectionUrl);
+            if (matcher.find()) {
+                String hostsPart = matcher.group(1);
+
+                // splitting to hosts, watching out from the "key-value" form: (host=myhost1,port=1111,key1=value1)
+                List<String> hosts = new ArrayList<>();
+                String[] parts = hostsPart.toLowerCase().trim().split(",");
+                StringBuilder sb = new StringBuilder();
+                for (String part : parts) {
+                    if (part.lastIndexOf(')') < part.lastIndexOf('(')) {
+                        // we are in the middle of a "key-value" form, we need to concatenate the next part
+                        sb.append(part).append(',');
+                    } else {
+                        boolean isWithinKeyValuePart = sb.length() > 0;
+                        sb.append(part);
+                        if (isWithinKeyValuePart && !part.contains(")")) {
+                            // key-value part not finished yet
+                            sb.append(',');
+                            continue;
+                        }
+                        hosts.add(sb.toString());
+                        sb.setLength(0);
+                    }
+                }
+
+                String firstHost = hosts.get(0);
+
+                // the "address-equals" form: address=(host=myhost1)(port=1111)(key1=value1)
+                String addressKey = "address=";
+                int indexOfAddress = firstHost.indexOf(addressKey);
+                if (indexOfAddress >= 0) {
+                    String tmp = firstHost.substring(indexOfAddress + addressKey.length());
+                    Matcher hostMatcher = Pattern.compile("\\s*host\\s*=\\s*([^)]+)\\s*").matcher(tmp);
+                    if (hostMatcher.find()) {
+                        String host = hostMatcher.group(1).trim();
+                        int port = -1;
+                        Matcher portMatcher = Pattern.compile("\\s*port\\s*=\\s*([^)]+)\\s*").matcher(tmp);
+                        if (portMatcher.find()) {
+                            port = Integer.parseInt(portMatcher.group(1).trim());
+                        }
+                        return new HostPort(host, port);
+                    } else {
+                        logger.warn("Failed to parse address from a connection URL: {}", connectionUrl);
+                        return null;
+                    }
+                }
+
+                // the "key-value" form: (host=myhost1,port=1111,key1=value1) - address form shouldn't arrive here
+                Matcher keyValueMatcher = Pattern.compile("\\(([^)]+)\\)").matcher(firstHost);
+                if (keyValueMatcher.find()) {
+                    String keyValuePart = keyValueMatcher.group(1).trim();
+                    parts = keyValuePart.split(",");
+                    String host = null;
+                    int port = -1;
+                    for (String part : parts) {
+                        String[] keyValue = part.split("=");
+                        if (keyValue.length == 2) {
+                            if (keyValue[0].trim().equals("host")) {
+                                host = keyValue[1].trim();
+                            } else if (keyValue[0].trim().equals("port")) {
+                                port = Integer.parseInt(keyValue[1].trim());
+                            }
+                        }
+                    }
+                    if (host != null) {
+                        return new HostPort(host, port);
+                    } else {
+                        logger.warn("Failed to parse address from a connection URL: {}", connectionUrl);
+                        return null;
+                    }
+                }
+
+                int indexOfSquareBracket = firstHost.indexOf('[');
+                if (indexOfSquareBracket >= 0) {
+                    if (!firstHost.contains("]") || firstHost.lastIndexOf('[') != indexOfSquareBracket) {
+                        // not IPv6, probably the "sublist" format, trim up to it
+                        firstHost = firstHost.substring(indexOfSquareBracket + 1);
+                    }
+                }
+
+                // trim user part, if set
+                int indexOfUserDetailsEnd = firstHost.indexOf('@');
+                if (indexOfUserDetailsEnd >= 0) {
+                    if (firstHost.length() > indexOfUserDetailsEnd + 1) {
+                        firstHost = firstHost.substring(indexOfUserDetailsEnd + 1).trim();
+                    } else {
+                        return null;
+                    }
+                }
+
+                ret = parseAuthority(firstHost.trim());
+            }
+            return ret;
         }
 
         static class HostPort {
