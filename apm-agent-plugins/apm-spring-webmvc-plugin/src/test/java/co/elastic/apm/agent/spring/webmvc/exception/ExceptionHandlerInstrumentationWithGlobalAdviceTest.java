4f7a6e21061b5bb7e730f2ece78e26ebe21f4178
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2020-03-31 11:00:58 +0300

    Not overriding status code for non-core-Servlet Exceptions (#1103)

--- apm-agent-plugins/apm-spring-webmvc-plugin/src/test/java/co/elastic/apm/agent/spring/webmvc/exception/ExceptionHandlerInstrumentationWithGlobalAdviceTest.java
+++ apm-agent-plugins/apm-spring-webmvc-plugin/src/test/java/co/elastic/apm/agent/spring/webmvc/exception/ExceptionHandlerInstrumentationWithGlobalAdviceTest.java
@@ -11,9 +11,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -24,8 +24,11 @@
  */
 package co.elastic.apm.agent.spring.webmvc.exception;
 
+import co.elastic.apm.agent.configuration.CoreConfiguration;
+import co.elastic.apm.agent.matcher.WildcardMatcher;
 import co.elastic.apm.agent.spring.webmvc.exception.testapp.controller_advice.ControllerAdviceController;
 import co.elastic.apm.agent.spring.webmvc.exception.testapp.controller_advice.ControllerAdviceRuntimeException;
+import co.elastic.apm.agent.spring.webmvc.exception.testapp.controller_advice.ControllerAdviceRuntimeException200;
 import co.elastic.apm.agent.spring.webmvc.exception.testapp.controller_advice.GlobalExceptionHandler;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -33,6 +36,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ContextConfiguration(classes = {
@@ -48,6 +55,29 @@ public class ExceptionHandlerInstrumentationWithGlobalAdviceTest extends Abstrac
         MvcResult result = resultActions.andReturn();
         MockHttpServletResponse response = result.getResponse();
 
-        assertExceptionCapture(ControllerAdviceRuntimeException.class, response, 409, "controller-advice runtime exception occured", "runtime exception occured");
+        assertExceptionCapture(ControllerAdviceRuntimeException.class, response, 409, "controller-advice runtime exception occurred", "runtime exception occurred");
+    }
+
+    @Test
+    public void testExceptionCaptureWithGlobalControllerAdvice_IgnoreExceptions() throws Exception {
+
+        when(config.getConfig(CoreConfiguration.class).getIgnoreExceptions()).thenReturn(
+            List.of(WildcardMatcher.valueOf("co.elastic.apm.agent.spring.webmvc.exception.testapp.controller_advice.ControllerAdviceRuntimeException"))
+        );
+
+        this.mockMvc.perform(get("/controller-advice/throw-exception"));
+
+        assertThat(reporter.getTransactions()).hasSize(1);
+        assertThat(reporter.getErrors()).isEmpty();
+    }
+
+    @Test
+    public void testExceptionCaptureWithGlobalControllerAdvice_StatusCode200() throws Exception {
+
+        ResultActions resultActions = this.mockMvc.perform(get("/controller-advice/throw-exception-sc200"));
+        MvcResult result = resultActions.andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertExceptionCapture(ControllerAdviceRuntimeException200.class, response, 200, "controller-advice runtime exception occurred", "runtime exception occurred");
     }
 }
