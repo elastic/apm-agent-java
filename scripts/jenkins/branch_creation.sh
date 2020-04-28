f5eba5df3d8f80e7ec041e9d9716d7ce5538708a
Mike Place <mike.place@elastic.co>
2020-04-28 11:07:47 +0200

    Simplify tag/version detection

--- scripts/jenkins/branch_creation.sh
+++ scripts/jenkins/branch_creation.sh
@@ -4,6 +4,8 @@ set -xo pipefail
 
 # Usage ./branch_creation 1.0.1
 
+# Requires that $TAG_VER already be present in the env
+
 TMP_CHECKOUT=$(mktemp -d)
 cd $TMP_CHECKOUT
 git clone https://github.com/elastic/apm-agent-java
@@ -15,13 +17,10 @@ VER=$?
 if [ $VER -eq 0 ];
 then
 echo "Found major release"
-NEW_BRANCH_NAME=$(git tag|tail -1|sed s/v//|cut -f1 -d "."|awk '{print $1".x"}')
-git checkout -b $NEW_BRANCH_NAME
+git checkout -b $TAG_DOT_X
 git push
 else
 echo "Found minor release"
-TAG=$(git tag|tail -1)
-TARGET_BRANCH=$(git tag|tail -1|sed s/v//|cut -f1 -d '.'|awk '{print $1".x"}')
-git branch -f $TARGET_BRANCH $TAG
+git branch -f $TAG_DOT_X $TAG_VER
 git push
 fi
--- scripts/jenkins/maven_artifact_url.sh
+++ scripts/jenkins/maven_artifact_url.sh
@@ -3,7 +3,8 @@
 # We are looking for something like this:
 # https://oss.sonatype.org/service/local/repositories/releases/content/co/elastic/apm/apm-agent-java/1.1.12/apm-agent-java-1.1.12.pom
 
+# Requires that $TAG_VER be present in the environment
+
 BASE_URL="https://oss.sonatype.org/service/local/repositories/releases/content/co/elastic/apm/apm-agent-java"
-TAG=$(git tag|tail -1|sed s/v//)
-FULL_URL="$BASE_URL/$TAG/apm-agent-java-$TAG.pom"
+FULL_URL="$BASE_URL/$TAG_VER/apm-agent-java-$TAG_VER.pom"
 echo $FULL_URL
