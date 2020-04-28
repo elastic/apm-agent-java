2dc05690d1eddb85c4b31d33e9250edb2a9b3b58
Mike Place <mike.place@elastic.co>
2020-04-28 18:00:17 +0200

    More changes to git

--- scripts/jenkins/branch_creation.sh
+++ scripts/jenkins/branch_creation.sh
@@ -6,11 +6,6 @@ set -xo pipefail
 
 # Requires that $TAG_VER already be present in the env
 
-TMP_CHECKOUT=$(mktemp -d)
-cd $TMP_CHECKOUT
-git clone https://github.com/elastic/apm-agent-java
-cd apm-agent-java
-
 $(echo ${1}|cut -f2-3 -d '.'|{ read ver; test $ver == '0.0'; })
 VER=$?
 
@@ -18,9 +13,7 @@ if [ $VER -eq 0 ];
 then
 echo "Found major release"
 git checkout -b $TAG_DOT_X
-git push
 else
 echo "Found minor release"
 git branch -f $TAG_DOT_X $TAG_VER
-git push
 fi
