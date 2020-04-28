fdbda0d3f0a1a6ad5777cf65e1656580127648b7
Mike Place <mike.place@elastic.co>
2020-04-28 18:00:17 +0200

    More changes to git

--- scripts/jenkins/update_cloudfoundry.sh
+++ scripts/jenkins/update_cloudfoundry.sh
@@ -4,14 +4,8 @@ set -exo pipefail
 
 # Requires that TAG_VER already be present in the environment
 
-TMP_CHECKOUT=$(mktemp -d)
 BASE_URL=https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent
+CF_FILE=cloudfoundry/index.yml
 
-
-cd $TMP_CHECKOUT
-git clone https://github.com/elastic/apm-agent-java
-cd apm-agent-java
-
-echo "$TAG_VER: $BASE_URL/$TAG/elastic-apm-agent-$TAG_VER.jar"
-
-git push origin master
+echo "$TAG_VER: $BASE_URL/$TAG/elastic-apm-agent-$TAG_VER.jar" >> $CF_FILE
+git commit $CF_FILE -m "Update cloudfoundry for $TAG_VER release"
