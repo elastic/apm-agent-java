b20db8cbb227b41f8371b2b4e808e15e4bc714d7
Sylvain Juge <sylvain.juge@elastic.co>
2020-05-20 17:41:53 +0200

    try disabling change introduced in 1.13

--- apm-agent-core/src/main/java/co/elastic/apm/agent/report/ApmServerClient.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/report/ApmServerClient.java
@@ -81,7 +81,7 @@ public class ApmServerClient {
     }
 
     public ApmServerClient(ReporterConfiguration reporterConfiguration, List<URL> shuffledUrls) {
-        initHttpUrlConnectionClass();
+//        initHttpUrlConnectionClass();
         this.reporterConfiguration = reporterConfiguration;
         this.healthChecker = new ApmServerHealthChecker(this);
         this.reporterConfiguration.getServerUrlsOption().addChangeListener(new ConfigurationOption.ChangeListener<List<URL>>() {
