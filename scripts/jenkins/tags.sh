0ddb5c33621be86ed9219b28b31b907db06274d5
Mike Place <mike.place@elastic.co>
2020-07-23 16:12:51 +0200

    Move tag detection to script and fix sorting error

---
+++ scripts/jenkins/tags.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+case  $1  in
+    bare)
+        git tag | sort -V|tail -1
+        ;;
+    ver)
+        git tag | sort -V |tail -1| sed s/v//
+        ;;
+    dot_x)
+        git tag|sort -V|tail -1 |cut -f1 -d "."|awk '{print $1".x"}'
+        ;;
+esac
