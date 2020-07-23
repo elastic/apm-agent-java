71555f203a4b9bdebe42ae5bb78e2d10a3c90113
cachedout <mike.place@elastic.co>
2020-07-23 17:09:03 +0200

    Update scripts/jenkins/tags.sh
    Co-authored-by: Felix Barnsteiner <felixbarny@users.noreply.github.com>

--- scripts/jenkins/tags.sh
+++ scripts/jenkins/tags.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 case  $1  in
     bare)
-        git tag | sort -V | tail -1 | tr -d '\n'
+        git tag | sort --version-sort | tail -1 | tr -d '\n'
         ;;
     ver)
         git tag | sort -V | tail -1 | sed s/v// | tr -d '\n'
