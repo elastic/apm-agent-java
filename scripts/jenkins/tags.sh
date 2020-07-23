10ffad810c9aa69500d356d2469eab6aecda1ee3
Mike Place <mike.place@elastic.co>
2020-07-23 16:38:27 +0200

    Spaces for readability

--- scripts/jenkins/tags.sh
+++ scripts/jenkins/tags.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 case  $1  in
     bare)
-        git tag | sort -V|tail -1|tr -d '\n'
+        git tag | sort -V | tail -1 | tr -d '\n'
         ;;
     ver)
-        git tag | sort -V |tail -1| sed s/v// | tr -d '\n'
+        git tag | sort -V | tail -1 | sed s/v// | tr -d '\n'
         ;;
     dot_x)
-        git tag|sort -V|tail -1 |cut -f1 -d "."|awk '{print $1".x"}' | tr -d '\n'
+        git tag | sort -V | tail -1 | cut -f1 -d "." | awk '{print $1".x"}' | tr -d '\n'
         ;;
 esac
