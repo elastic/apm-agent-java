29b53d7fabab09d493d32632da98b32324b6271b
kananindzya <kananindzya@gmail.com>
2020-08-02 15:00:26 +0600

    added test for async calls

---
+++ apm-agent-plugins/apm-httpclient11-plugin/src/main/java/co/elastic/apm/agent/httpclient/HttpClientAsyncInstrumentation.java
@@ -0,0 +1,99 @@
+package co.elastic.apm.agent.httpclient;
+
+import co.elastic.apm.agent.bci.VisibleForAdvice;
+import co.elastic.apm.agent.impl.transaction.AbstractSpan;
+import co.elastic.apm.agent.impl.transaction.Span;
+import co.elastic.apm.agent.sdk.advice.AssignTo;
+import co.elastic.apm.agent.sdk.state.GlobalThreadLocal;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import javax.annotation.Nullable;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
+
+public class HttpClientAsyncInstrumentation extends AbstractHttpClientInstrumentation {
+
+    @Override
+    public Class<?> getAdviceClass() {
+        return HttpClient11Advice.class;
+    }
+
+    @VisibleForAdvice
+    public static class HttpClient11Advice {
+
+        @VisibleForAdvice
+        public final static GlobalThreadLocal<Span> spanTls = GlobalThreadLocal.get(HttpClient11Advice.class, "spanTls");
+
+        @Nullable
+        @AssignTo.Argument(value = 0, typing = Assigner.Typing.DYNAMIC)
+        @Advice.OnMethodEnter(suppress = Throwable.class, inline = false)
+        public static Object onBeforeExecute(@Advice.Argument(value = 0) HttpRequest httpRequest) {
+            if (tracer.getActive() == null) {
+                return httpRequest;
+            }
+            final AbstractSpan<?> parent = tracer.getActive();
+            URI uri = httpRequest.uri();
+            Span span = co.elastic.apm.agent.http.client.HttpClientHelper.startHttpClientSpan(parent, httpRequest.method(), uri.toString(), uri.getScheme(),
+                HttpClientHelper.computeHostName(uri.getHost()), uri.getPort());
+            if (span != null) {
+                spanTls.set(span);
+                span.activate();
+                HttpRequest.Builder builder = HttpRequest.newBuilder(httpRequest.uri())
+                    .method(httpRequest.method(), httpRequest.bodyPublisher().orElse(HttpRequest.BodyPublishers.noBody()))
+                    .expectContinue(httpRequest.expectContinue());
+                if (httpRequest.timeout().isPresent()) {
+                    builder = builder.timeout(httpRequest.timeout().get());
+                }
+                if (httpRequest.version().isPresent()) {
+                    builder = builder.version(httpRequest.version().get());
+                }
+                for (String header : httpRequest.headers().map().keySet()) {
+                    builder.header(header, httpRequest.headers().firstValue(header).orElse(null));
+                }
+                span.propagateTraceContext(builder, HttpClientRequestPropertyAccessor.instance());
+                return builder.build();
+            }
+            return httpRequest;
+        }
+
+        @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class, inline = false)
+        public static void onAfterExecute(@Advice.Return @Nullable HttpResponse response,
+                                          @Advice.Thrown @Nullable Throwable t) {
+            final Span span = spanTls.getAndRemove();
+            if (span != null) {
+                try {
+                    if (response != null) {
+                        int statusCode = response.statusCode();
+                        span.getContext().getHttp().withStatusCode(statusCode);
+                    }
+                    span.captureException(t);
+                } finally {
+                    span.deactivate().end();
+                }
+            }
+        }
+    }
+
+    @Override
+    public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+        return named("jdk.internal.net.http.HttpClientImpl");
+    }
+
+    @Override
+    public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+        return named("sendAsync").and(returns(named("java.util.concurrent.CompletableFuture")));
+    }
+
+    @Override
+    public boolean indyPlugin() {
+        return true;
+    }
+}
---
+++ apm-agent-plugins/apm-httpclient11-plugin/src/test/java/co/elastic/apm/agent/httpclient/HttpClientAsyncInstrumentationTest.java
@@ -0,0 +1,38 @@
+package co.elastic.apm.agent.httpclient;
+
+import org.junit.Before;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HttpClientAsyncInstrumentationTest extends AbstractHttpClientInstrumentationTest {
+    private HttpClient client;
+
+    @Before
+    public void setUp() {
+        client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
+    }
+
+    @Override
+    protected void performGet(String path) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(path))
+            .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).get();
+    }
+
+    @Override
+    protected boolean isIpv6Supported() {
+        return true;
+    }
+
+    @Override
+    public void assertCircularRedirect() {
+        assertThat(reporter.getSpans().get(0).getContext().getHttp().getStatusCode()).isEqualTo(303);
+    }
+}
