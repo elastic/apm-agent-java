07ede6a9463fe290434a34618fb4be05906f5a30
kananindzya <kananindzya@gmail.com>
2020-08-12 02:06:09 +0600

    deleted GlobalThreadLocal

--- apm-agent-plugins/apm-httpclient11-plugin/src/main/java/co/elastic/apm/agent/httpclient/HttpClientInstrumentation.java
+++ apm-agent-plugins/apm-httpclient11-plugin/src/main/java/co/elastic/apm/agent/httpclient/HttpClientInstrumentation.java
@@ -4,7 +4,6 @@ import co.elastic.apm.agent.bci.VisibleForAdvice;
 import co.elastic.apm.agent.impl.transaction.AbstractSpan;
 import co.elastic.apm.agent.impl.transaction.Span;
 import co.elastic.apm.agent.sdk.advice.AssignTo;
-import co.elastic.apm.agent.sdk.state.GlobalThreadLocal;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
@@ -29,9 +28,6 @@ public class HttpClientInstrumentation extends AbstractHttpClientInstrumentation
     @VisibleForAdvice
     public static class HttpClient11Advice {
 
-        @VisibleForAdvice
-        public final static GlobalThreadLocal<Span> spanTls = GlobalThreadLocal.get(HttpClient11Advice.class, "spanTls");
-
         @Nullable
         @AssignTo.Argument(value = 0, typing = Assigner.Typing.DYNAMIC)
         @Advice.OnMethodEnter(suppress = Throwable.class, inline = false)
@@ -44,7 +40,6 @@ public class HttpClientInstrumentation extends AbstractHttpClientInstrumentation
             Span span = co.elastic.apm.agent.http.client.HttpClientHelper.startHttpClientSpan(parent, httpRequest.method(), uri.toString(), uri.getScheme(),
                 HttpClientHelper.computeHostName(uri.getHost()), uri.getPort());
             if (span != null) {
-                spanTls.set(span);
                 span.activate();
                 HttpRequest.Builder builder = HttpRequest.newBuilder(httpRequest.uri())
                     .method(httpRequest.method(), httpRequest.bodyPublisher().orElse(HttpRequest.BodyPublishers.noBody()))
@@ -67,7 +62,7 @@ public class HttpClientInstrumentation extends AbstractHttpClientInstrumentation
         @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class, inline = false)
         public static void onAfterExecute(@Advice.Return @Nullable HttpResponse response,
                                           @Advice.Thrown @Nullable Throwable t) {
-            final Span span = spanTls.getAndRemove();
+            final Span span = tracer.getActiveExitSpan();
             if (span != null) {
                 try {
                     if (response != null) {
