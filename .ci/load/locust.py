cd1b20bfeda82d3d7b4d88881d36a4e20f9b447b
Mike Place <mike.place@elastic.co>
2020-10-21 07:45:48 +0200

    Skeleton of perf Jenkinsfile

---
+++ .ci/load/locust.py
@@ -0,0 +1,21 @@
+import time
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index_page(self):
+        self.client.get("/hello")
+        self.client.get("/world")
+
+    @task(3)
+    def view_item(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
+
+    def on_start(self):
+        self.client.post("/login", json={"username":"foo", "password":"bar"})
+
