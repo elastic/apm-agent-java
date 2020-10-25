4ddc982acf5654d98ad2d7d0f7222dda1049e80c
kananindzya <gonindzya@gmail.com>
2020-10-26 00:46:33 +0600

    implement aws metadata fetch

--- apm-agent-core/src/main/java/co/elastic/apm/agent/impl/Cloud.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/impl/Cloud.java
@@ -1,15 +1,66 @@
 package co.elastic.apm.agent.impl;
 
 import co.elastic.apm.agent.impl.payload.CloudProviderInfo;
+import com.dslplatform.json.DslJson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
 
 public class Cloud {
+    private static final Logger logger = LoggerFactory.getLogger(Cloud.class);
 
     @Nullable
     public static CloudProviderInfo getAwsMetadata() {
+        try {
+            String tokenUrl = "http://169.254.169.254/latest/api/token";
+            Map<String, String> headers = Map.of("X-aws-ec2-metadata-token-ttl-seconds", "300");
+            String token = callRequest(tokenUrl, "PUT", headers);
+            String metadataUrl = "http://169.254.169.254/latest/dynamic/instance-identity/document";
+            Map<String, String> documentHeaders = Map.of("X-aws-ec2-metadata-token", token);
+            String metadata = callRequest(metadataUrl, "GET", documentHeaders);
+            AwsMetadata awsMetadata = deserialize(metadata, AwsMetadata.class);
+            return awsMetadata.convert();
+        } catch (Exception e) {
+            logger.warn("Exception during fetching aws metadata", e);
+            return null;
+        }
+    }
 
-        return null;
+    private static <T> T deserialize(String input, Class<T> clazz) throws IOException {
+        DslJson<Object> json = new DslJson<>();
+        byte[] bytes = input.getBytes("UTF-8");
+        return json.deserialize(clazz, bytes, bytes.length);
+    }
+
+    private static String callRequest(@Nonnull String url, @Nonnull String method, @Nonnull Map<String, String> headers) throws IOException {
+        URL myURL = new URL(url);
+        HttpURLConnection urlConnection = (HttpURLConnection) myURL.openConnection();
+        for (String header : headers.keySet()) {
+            urlConnection.setRequestProperty(header, headers.get(header));
+        }
+        urlConnection.setRequestMethod(method);
+        urlConnection.setReadTimeout(3000);
+        urlConnection.setConnectTimeout(3000);
+        String response = null;
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            response = content.toString();
+        } finally {
+            urlConnection.disconnect();
+        }
+        return response;
     }
 
     @Nullable
@@ -22,5 +73,82 @@ public class Cloud {
     public static CloudProviderInfo getAzureMetadata() {
 
         return null;
+    }
+
+    private static class AwsMetadata {
+        private String accountId;
+        private String architecture;
+        private String availabilityZone;
+        private String instanceId;
+        private String instanceType;
+        private String region;
+        private String version;
+
+        public CloudProviderInfo convert() {
+            CloudProviderInfo cloudProviderInfo = new CloudProviderInfo();
+            cloudProviderInfo.setAccount(accountId);
+            cloudProviderInfo.setInstance(instanceId);
+            cloudProviderInfo.setMachine(instanceType);
+            cloudProviderInfo.setAvailabilityZone(availabilityZone);
+            cloudProviderInfo.setRegion(region);
+            cloudProviderInfo.setProvider("aws");
+            return cloudProviderInfo;
+        }
+
+        public String getAccountId() {
+            return accountId;
+        }
+
+        public void setAccountId(String accountId) {
+            this.accountId = accountId;
+        }
+
+        public String getArchitecture() {
+            return architecture;
+        }
+
+        public void setArchitecture(String architecture) {
+            this.architecture = architecture;
+        }
+
+        public String getAvailabilityZone() {
+            return availabilityZone;
+        }
+
+        public void setAvailabilityZone(String availabilityZone) {
+            this.availabilityZone = availabilityZone;
+        }
+
+        public String getInstanceId() {
+            return instanceId;
+        }
+
+        public void setInstanceId(String instanceId) {
+            this.instanceId = instanceId;
+        }
+
+        public String getInstanceType() {
+            return instanceType;
+        }
+
+        public void setInstanceType(String instanceType) {
+            this.instanceType = instanceType;
+        }
+
+        public String getRegion() {
+            return region;
+        }
+
+        public void setRegion(String region) {
+            this.region = region;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
     }
 }
--- apm-agent-core/src/main/java/co/elastic/apm/agent/impl/payload/CloudProviderInfo.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/impl/payload/CloudProviderInfo.java
@@ -10,6 +10,77 @@ import javax.annotation.Nullable;
 public class CloudProviderInfo {
     private static final Logger logger = LoggerFactory.getLogger(CloudProviderInfo.class);
 
+    private String provider;
+
+    private String instance;
+
+    private String availabilityZone;
+
+    private String machine;
+
+    private String region;
+
+    // aws - azure
+    private String account;
+    // gcp - azure
+    private String project;
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getInstance() {
+        return instance;
+    }
+
+    public void setInstance(String instance) {
+        this.instance = instance;
+    }
+
+    public String getAvailabilityZone() {
+        return availabilityZone;
+    }
+
+    public void setAvailabilityZone(String availabilityZone) {
+        this.availabilityZone = availabilityZone;
+    }
+
+    public String getMachine() {
+        return machine;
+    }
+
+    public void setMachine(String machine) {
+        this.machine = machine;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
     @Nullable
     public static CloudProviderInfo create(@Nullable String cloudProviderName) {
         cloudProviderName = cloudProviderName != null ? cloudProviderName.toLowerCase() : null;
