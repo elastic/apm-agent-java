f4ba58000b377fe3f8a7559db03d8892b8199969
SylvainJuge <sylvain.juge@elastic.co>
2020-11-09 14:38:05 +0100

    implement fix for hp-ux version parsing (#1477)

--- apm-agent-core/src/main/java/co/elastic/apm/agent/util/JvmRuntimeInfo.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/util/JvmRuntimeInfo.java
@@ -38,6 +38,7 @@ public class JvmRuntimeInfo {
     private static boolean isHotSpot;
     private static boolean isIbmJ9;
     private static boolean isJ9;
+    private static boolean isHpUx;
 
     static {
         parseVmInfo(System.getProperty("java.version"), System.getProperty("java.vm.name"), System.getProperty("java.vm.version"));
@@ -57,6 +58,16 @@ public class JvmRuntimeInfo {
         javaVmName = vmName;
         javaVmVersion = vmVersion;
 
+        isHotSpot = vmName.contains("HotSpot(TM)") || vmName.contains("OpenJDK");
+        isIbmJ9 = vmName.contains("IBM J9");
+        isJ9 = vmName.contains("J9");
+        isHpUx = version.endsWith("-hp-ux");
+
+        if (isHpUx) {
+            // remove extra hp-ux suffix for parsing
+            version = version.substring(0, version.length() - 6);
+        }
+
         // new scheme introduced in java 9, thus we can use it as a shortcut
         if (version.startsWith("1.")) {
             majorVersion = Character.digit(version.charAt(2), 10);
@@ -71,8 +82,18 @@ public class JvmRuntimeInfo {
 
         int updateIndex = version.lastIndexOf("_");
         if (updateIndex <= 0) {
-            // GA release like '1.8.0'
-            updateVersion = 0;
+
+            if (isHpUx) {
+                try {
+                    updateVersion = Integer.parseInt(version.substring(version.lastIndexOf('.') + 1));
+                } catch (NumberFormatException e) {
+                    updateVersion = -1;
+                }
+            } else {
+                // GA release like '1.8.0'
+                updateVersion = 0;
+            }
+
         } else {
             String updateVersionString;
             int versionSuffixIndex = version.indexOf('-', updateIndex + 1);
@@ -85,14 +106,15 @@ public class JvmRuntimeInfo {
                 updateVersion = Integer.parseInt(updateVersionString);
             } catch (NumberFormatException e) {
                 // in case of unknown format, we just support by default
-                System.err.println("Unsupported format of the java.version system property - " + version);
+
                 updateVersion = -1;
             }
         }
 
-        isHotSpot = vmName.contains("HotSpot(TM)") || vmName.contains("OpenJDK");
-        isIbmJ9 = vmName.contains("IBM J9");
-        isJ9 = vmName.contains("J9");
+        if (updateVersion < 0) {
+            System.err.println("Unsupported format of the java.version system property - " + version);
+        }
+
     }
 
     public static String getJavaVersion() {
@@ -114,6 +136,10 @@ public class JvmRuntimeInfo {
 
     public static boolean isJ9VM() {
         return isJ9;
+    }
+
+    public static boolean isIsHpUx(){
+        return isHpUx;
     }
 
     /**
@@ -144,12 +170,19 @@ public class JvmRuntimeInfo {
             return true;
         }
 
+        // versions prior to that have unreliable invoke dynamic support according to https://groovy-lang.org/indy.html
+        int java7min = 60;
+        int java8min = 40;
+        if (isIsHpUx()) {
+            java7min = 10; // hotspot 7u65
+            java8min = 2; // hotspot 8u45
+        }
+
         switch (majorVersion) {
             case 7:
-                // versions prior to that have unreliable invoke dynamic support according to https://groovy-lang.org/indy.html
-                return updateVersion >= 60;
+                return updateVersion >= java7min;
             case 8:
-                return updateVersion >= 40;
+                return updateVersion >= java8min;
             default:
                 return true;
         }
