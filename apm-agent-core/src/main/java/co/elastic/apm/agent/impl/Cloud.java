5a348e66ad808fd44df8cb0461ba2bd40924ed6f
kananindzya <gonindzya@gmail.com>
2020-11-11 03:10:22 +0600

    minor fixes

--- apm-agent-core/src/main/java/co/elastic/apm/agent/impl/Cloud.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/impl/Cloud.java
@@ -1,3 +1,27 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 - 2020 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
 package co.elastic.apm.agent.impl;
 
 import co.elastic.apm.agent.impl.payload.CloudProviderInfo;
@@ -14,6 +38,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,31 +46,80 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Cloud {
     private static final Logger logger = LoggerFactory.getLogger(Cloud.class);
+
     private final DslJson<Object> dslJson = new DslJson<>(new DslJson.Settings<>());
 
     @Nullable
     public CloudProviderInfo getAwsMetadata() {
         try {
-            String tokenUrl = "http://169.254.169.254/latest/api/token";
-            Map<String, String> headers = Map.of("X-aws-ec2-metadata-token-ttl-seconds", "300");
-            String token = callRequest(tokenUrl, "PUT", headers);
-            String metadataUrl = "http://169.254.169.254/latest/dynamic/instance-identity/document";
-            Map<String, String> documentHeaders = Map.of("X-aws-ec2-metadata-token", token);
-            String metadata = callRequest(metadataUrl, "GET", documentHeaders);
+            String awsTokenUrl = "http://localhost:8080/latest/api/token";
+            Map<String, String> headers = new HashMap<>(1);
+            headers.put("X-aws-ec2-metadata-token-ttl-seconds", "300");
+            String token = callRequest(awsTokenUrl, "PUT", headers);
+            String awsMetadataUrl = "http://localhost:8080/latest/dynamic/instance-identity/document";
+            Map<String, String> documentHeaders = new HashMap<>(1);
+            documentHeaders.put("X-aws-ec2-metadata-token", token);
+            String metadata = callRequest(awsMetadataUrl, "GET", documentHeaders);
             logger.debug("Got aws metadata = {}", metadata);
-            AwsMetadata awsMetadata = null;
-            return awsMetadata.convert();
+            return convertAwsMetadata(metadata);
         } catch (Exception e) {
             logger.debug("Exception during fetching aws metadata", e);
             return null;
         }
     }
 
+    /**
+     * {
+     * "accountId": "946960629917",
+     * "architecture": "x86_64",
+     * "availabilityZone": "us-east-2a",
+     * "billingProducts": null,
+     * "devpayProductCodes": null,
+     * "marketplaceProductCodes": null,
+     * "imageId": "ami-07c1207a9d40bc3bd",
+     * "instanceId": "i-0ae894a7c1c4f2a75",
+     * "instanceType": "t2.medium",
+     * "kernelId": null,
+     * "pendingTime": "2020-06-12T17:46:09Z",
+     * "privateIp": "172.31.0.212",
+     * "ramdiskId": null,
+     * "region": "us-east-2",
+     * "version": "2017-09-30"
+     * }
+     */
+    @Nullable
+    protected CloudProviderInfo convertAwsMetadata(@Nullable String data) throws IOException {
+        if (data == null) {
+            return null;
+        }
+        LinkedHashMap<String, Object> map = deserialize(data);
+
+        Object accountIdValue = map.get("accountId");
+        String accountId = accountIdValue instanceof String ? (String) accountIdValue : null;
+        Object instanceIdValue = map.get("instanceId");
+        String instanceId = instanceIdValue instanceof String ? (String) instanceIdValue : null;
+        Object instanceTypeValue = map.get("instanceType");
+        String instanceType = instanceTypeValue instanceof String ? (String) instanceTypeValue : null;
+        Object availabilityZoneValue = map.get("availabilityZone");
+        String availabilityZone = availabilityZoneValue instanceof String ? (String) availabilityZoneValue : null;
+        Object regionValue = map.get("region");
+        String region = regionValue instanceof String ? (String) regionValue : null;
+
+        CloudProviderInfo cloudProviderInfo = new CloudProviderInfo("aws");
+        cloudProviderInfo.setMachine(new CloudProviderInfo.ProviderMachine(instanceType));
+        cloudProviderInfo.setInstance(new CloudProviderInfo.ProviderInstance(instanceId, null));
+        cloudProviderInfo.setAvailabilityZone(availabilityZone);
+        cloudProviderInfo.setAccount(new CloudProviderInfo.ProviderAccount(accountId));
+        cloudProviderInfo.setRegion(region);
+        return cloudProviderInfo;
+    }
+
     @Nullable
     public CloudProviderInfo getGcpMetadata() {
         try {
             String gcpUrl = "http://metadata.google.internal/computeMetadata/v1/?recursive=true";
-            Map<String, String> headers = Map.of("Metadata-Flavor", "Google");
+            Map<String, String> headers = new HashMap<>(1);
+            headers.put("Metadata-Flavor", "Google");
             String metadata = callRequest(gcpUrl, "GET", headers);
             logger.debug("Got gcp metadata = {}", metadata);
             return convertGcpMetadata(metadata);
@@ -104,18 +178,62 @@ public class Cloud {
     public CloudProviderInfo getAzureMetadata() {
         try {
             String azureUrl = "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15";
-            Map<String, String> headers = Map.of("Metadata", "true");
+            Map<String, String> headers = new HashMap<>(1);
+            headers.put("Metadata", "true");
             String metadata = callRequest(azureUrl, "GET", headers);
             logger.debug("Got azure metadata = {}", metadata);
-            AzureMetadata azureMetadata = null;
-            return azureMetadata.convert();
+            return convertAzureMetadata(metadata);
         } catch (Exception e) {
             logger.debug("Got exception during fetching azure metadata", e);
             return null;
         }
     }
 
-    protected LinkedHashMap deserialize(String input) throws IOException {
+    /**
+     * {
+     * "location": "westus2",
+     * "name": "basepi-test",
+     * "resourceGroupName": "basepi-testing",
+     * "subscriptionId": "7657426d-c4c3-44ac-88a2-3b2cd59e6dba",
+     * "vmId": "e11ebedc-019d-427f-84dd-56cd4388d3a8",
+     * "vmScaleSetName": "",
+     * "vmSize": "Standard_D2s_v3",
+     * "zone": ""
+     * }
+     */
+    @Nullable
+    protected CloudProviderInfo convertAzureMetadata(@Nullable String data) throws IOException {
+        if (data == null) {
+            return null;
+        }
+        LinkedHashMap<String, Object> map = deserialize(data);
+
+        Object subscriptionIdValue = map.get("subscriptionId");
+        String subscriptionId = subscriptionIdValue instanceof String ? (String) subscriptionIdValue : null;
+        Object vmIdValue = map.get("vmId");
+        String vmId = vmIdValue instanceof String ? (String) vmIdValue : null;
+        Object nameValue = map.get("name");
+        String name = nameValue instanceof String ? (String) nameValue : null;
+        Object resourceGroupNameValue = map.get("resourceGroupName");
+        String resourceGroupName = resourceGroupNameValue instanceof String ? (String) resourceGroupNameValue : null;
+        Object zoneValue = map.get("zone");
+        String zone = zoneValue instanceof String ? (String) zoneValue : null;
+        Object vmSizeValue = map.get("vmSize");
+        String vmSize = vmSizeValue instanceof String ? (String) vmSizeValue : null;
+        Object locationValue = map.get("location");
+        String location = locationValue instanceof String ? (String) locationValue : null;
+
+        CloudProviderInfo cloudProviderInfo = new CloudProviderInfo("azure");
+        cloudProviderInfo.setAccount(new CloudProviderInfo.ProviderAccount(subscriptionId));
+        cloudProviderInfo.setInstance(new CloudProviderInfo.ProviderInstance(vmId, name));
+        cloudProviderInfo.setProject(new CloudProviderInfo.ProviderProject(resourceGroupName));
+        cloudProviderInfo.setAvailabilityZone(zone);
+        cloudProviderInfo.setMachine(new CloudProviderInfo.ProviderMachine(vmSize));
+        cloudProviderInfo.setRegion(location);
+        return cloudProviderInfo;
+    }
+
+    private LinkedHashMap deserialize(String input) throws IOException {
         JsonReader<Object> reader = dslJson.newReader(input.getBytes(UTF_8));
         reader.startObject();
         return (LinkedHashMap) ObjectConverter.deserializeObject(reader);
@@ -142,198 +260,5 @@ public class Cloud {
             urlConnection.disconnect();
         }
         return response;
-    }
-
-    /**
-     * {
-     * "accountId": "946960629917",
-     * "architecture": "x86_64",
-     * "availabilityZone": "us-east-2a",
-     * "billingProducts": null,
-     * "devpayProductCodes": null,
-     * "marketplaceProductCodes": null,
-     * "imageId": "ami-07c1207a9d40bc3bd",
-     * "instanceId": "i-0ae894a7c1c4f2a75",
-     * "instanceType": "t2.medium",
-     * "kernelId": null,
-     * "pendingTime": "2020-06-12T17:46:09Z",
-     * "privateIp": "172.31.0.212",
-     * "ramdiskId": null,
-     * "region": "us-east-2",
-     * "version": "2017-09-30"
-     * }
-     */
-    private static class AwsMetadata {
-        private String accountId;
-        private String architecture;
-        private String availabilityZone;
-        private String instanceId;
-        private String instanceType;
-        private String region;
-        private String version;
-
-        public CloudProviderInfo convert() {
-            CloudProviderInfo cloudProviderInfo = new CloudProviderInfo("aws");
-            cloudProviderInfo.setAccount(new CloudProviderInfo.ProviderAccount(accountId));
-            cloudProviderInfo.setInstance(new CloudProviderInfo.ProviderInstance(instanceId, null));
-            cloudProviderInfo.setMachine(new CloudProviderInfo.ProviderMachine(instanceType));
-            cloudProviderInfo.setAvailabilityZone(availabilityZone);
-            cloudProviderInfo.setRegion(region);
-            return cloudProviderInfo;
-        }
-
-        public String getAccountId() {
-            return accountId;
-        }
-
-        public void setAccountId(String accountId) {
-            this.accountId = accountId;
-        }
-
-        public String getArchitecture() {
-            return architecture;
-        }
-
-        public void setArchitecture(String architecture) {
-            this.architecture = architecture;
-        }
-
-        public String getAvailabilityZone() {
-            return availabilityZone;
-        }
-
-        public void setAvailabilityZone(String availabilityZone) {
-            this.availabilityZone = availabilityZone;
-        }
-
-        public String getInstanceId() {
-            return instanceId;
-        }
-
-        public void setInstanceId(String instanceId) {
-            this.instanceId = instanceId;
-        }
-
-        public String getInstanceType() {
-            return instanceType;
-        }
-
-        public void setInstanceType(String instanceType) {
-            this.instanceType = instanceType;
-        }
-
-        public String getRegion() {
-            return region;
-        }
-
-        public void setRegion(String region) {
-            this.region = region;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
-    }
-
-    /**
-     * {
-     * "location": "westus2",
-     * "name": "basepi-test",
-     * "resourceGroupName": "basepi-testing",
-     * "subscriptionId": "7657426d-c4c3-44ac-88a2-3b2cd59e6dba",
-     * "vmId": "e11ebedc-019d-427f-84dd-56cd4388d3a8",
-     * "vmScaleSetName": "",
-     * "vmSize": "Standard_D2s_v3",
-     * "zone": ""
-     * }
-     */
-    private static class AzureMetadata {
-        private String location;
-        private String name;
-        private String resourceGroupName;
-        private String subscriptionId;
-        private String vmId;
-        private String vmScaleSetName;
-        private String vmSize;
-        private String zone;
-
-        public CloudProviderInfo convert() {
-            CloudProviderInfo cloudProviderInfo = new CloudProviderInfo("azure");
-            cloudProviderInfo.setAccount(new CloudProviderInfo.ProviderAccount(subscriptionId));
-            cloudProviderInfo.setInstance(new CloudProviderInfo.ProviderInstance(vmId, name));
-            cloudProviderInfo.setProject(new CloudProviderInfo.ProviderProject(resourceGroupName));
-            cloudProviderInfo.setAvailabilityZone(zone);
-            cloudProviderInfo.setMachine(new CloudProviderInfo.ProviderMachine(vmSize));
-            cloudProviderInfo.setRegion(location);
-            return cloudProviderInfo;
-        }
-
-        public String getLocation() {
-            return location;
-        }
-
-        public void setLocation(String location) {
-            this.location = location;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getResourceGroupName() {
-            return resourceGroupName;
-        }
-
-        public void setResourceGroupName(String resourceGroupName) {
-            this.resourceGroupName = resourceGroupName;
-        }
-
-        public String getSubscriptionId() {
-            return subscriptionId;
-        }
-
-        public void setSubscriptionId(String subscriptionId) {
-            this.subscriptionId = subscriptionId;
-        }
-
-        public String getVmId() {
-            return vmId;
-        }
-
-        public void setVmId(String vmId) {
-            this.vmId = vmId;
-        }
-
-        public String getVmScaleSetName() {
-            return vmScaleSetName;
-        }
-
-        public void setVmScaleSetName(String vmScaleSetName) {
-            this.vmScaleSetName = vmScaleSetName;
-        }
-
-        public String getVmSize() {
-            return vmSize;
-        }
-
-        public void setVmSize(String vmSize) {
-            this.vmSize = vmSize;
-        }
-
-        public String getZone() {
-            return zone;
-        }
-
-        public void setZone(String zone) {
-            this.zone = zone;
-        }
     }
 }
--- apm-agent-core/src/test/java/co/elastic/apm/agent/impl/CloudTest.java
+++ apm-agent-core/src/test/java/co/elastic/apm/agent/impl/CloudTest.java
@@ -1,3 +1,27 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 - 2020 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
 package co.elastic.apm.agent.impl;
 
 import co.elastic.apm.agent.impl.payload.CloudProviderInfo;
@@ -42,5 +66,65 @@ class CloudTest {
         assertThat(gcpMetadata.getProject().getId()).isEqualTo("513326162531");
         assertThat(gcpMetadata.getProject().getName()).isEqualTo("elastic-apm");
         assertThat(gcpMetadata.getMachine().getType()).isEqualTo("projects/513326162531/machineTypes/n1-standard-1");
+    }
+
+    @Test
+    void awsMetadataDeserializeTest() throws IOException {
+        String input = "{\n" +
+            "    \"accountId\": \"946960629917\",\n" +
+            "    \"architecture\": \"x86_64\",\n" +
+            "    \"availabilityZone\": \"us-east-2a\",\n" +
+            "    \"billingProducts\": null,\n" +
+            "    \"devpayProductCodes\": null,\n" +
+            "    \"marketplaceProductCodes\": null,\n" +
+            "    \"imageId\": \"ami-07c1207a9d40bc3bd\",\n" +
+            "    \"instanceId\": \"i-0ae894a7c1c4f2a75\",\n" +
+            "    \"instanceType\": \"t2.medium\",\n" +
+            "    \"kernelId\": null,\n" +
+            "    \"pendingTime\": \"2020-06-12T17:46:09Z\",\n" +
+            "    \"privateIp\": \"172.31.0.212\",\n" +
+            "    \"ramdiskId\": null,\n" +
+            "    \"region\": \"us-east-2\",\n" +
+            "    \"version\": \"2017-09-30\"\n" +
+            "}";
+
+        CloudProviderInfo gcpMetadata = cloud.convertAwsMetadata(input);
+
+        assertThat(gcpMetadata).isNotNull();
+        assertThat(gcpMetadata.getProvider()).isEqualTo("aws");
+        assertThat(gcpMetadata.getAvailabilityZone()).isEqualTo("us-east-2a");
+        assertThat(gcpMetadata.getRegion()).isEqualTo("us-east-2");
+        assertThat(gcpMetadata.getInstance().getId()).isEqualTo("i-0ae894a7c1c4f2a75");
+        assertThat(gcpMetadata.getInstance().getName()).isNull();
+        assertThat(gcpMetadata.getAccount().getId()).isEqualTo("946960629917");
+        assertThat(gcpMetadata.getProject()).isNull();
+        assertThat(gcpMetadata.getMachine().getType()).isEqualTo("t2.medium");
+    }
+
+    @Test
+    void azureMetadataDeserializeTest() throws IOException {
+        String input = "{\n" +
+            "    \"location\": \"westus2\",\n" +
+            "    \"name\": \"basepi-test\",\n" +
+            "    \"resourceGroupName\": \"basepi-testing\",\n" +
+            "    \"subscriptionId\": \"7657426d-c4c3-44ac-88a2-3b2cd59e6dba\",\n" +
+            "    \"vmId\": \"e11ebedc-019d-427f-84dd-56cd4388d3a8\",\n" +
+            "    \"vmScaleSetName\": \"\",\n" +
+            "    \"vmSize\": \"Standard_D2s_v3\",\n" +
+            "    \"zone\": \"\"\n" +
+            "}";
+
+        CloudProviderInfo gcpMetadata = cloud.convertAzureMetadata(input);
+
+        assertThat(gcpMetadata).isNotNull();
+        assertThat(gcpMetadata.getProvider()).isEqualTo("azure");
+        assertThat(gcpMetadata.getAvailabilityZone()).isEqualTo("");
+        assertThat(gcpMetadata.getRegion()).isEqualTo("westus2");
+        assertThat(gcpMetadata.getInstance().getId()).isEqualTo("e11ebedc-019d-427f-84dd-56cd4388d3a8");
+        assertThat(gcpMetadata.getInstance().getName()).isEqualTo("basepi-test");
+        assertThat(gcpMetadata.getAccount().getId()).isEqualTo("7657426d-c4c3-44ac-88a2-3b2cd59e6dba");
+        assertThat(gcpMetadata.getProject().getName()).isEqualTo("basepi-testing");
+        assertThat(gcpMetadata.getProject().getId()).isNull();
+        assertThat(gcpMetadata.getMachine().getType()).isEqualTo("Standard_D2s_v3");
     }
 }
