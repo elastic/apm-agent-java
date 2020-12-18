6123bfca4d0f6733eba318503718e972b1d31e7d
Sylvain Juge <sylvain.juge@elastic.co>
2020-12-18 11:19:05 +0100

    fail load-testing step if >30% of total errors

--- .ci/load/scripts/locustfile.py
+++ .ci/load/scripts/locustfile.py
@@ -1,6 +1,5 @@
 from locust import HttpUser, task, constant_pacing
 
-
 class QuickstartUser(HttpUser):
     wait_time = constant_pacing(1)
 
@@ -16,3 +15,17 @@ class QuickstartUser(HttpUser):
     @task(3)
     def gen_error(self):
         self.client.get("/oups")
+
+@events.quitting.add_listener
+def _(environment, **kw):
+    if environment.stats.total.fail_ratio > 30:
+        logging.error("Test failed due to failure ratio > 30%")
+        environment.process_exit_code = 1
+    elif environment.stats.total.avg_response_time > 200:
+        logging.error("Test failed due to average response time ratio > 200 ms")
+        environment.process_exit_code = 1
+    elif environment.stats.total.get_response_time_percentile(0.95) > 800:
+        logging.error("Test failed due to 95th percentile response time > 800 ms")
+        environment.process_exit_code = 1
+    else:
+        environment.process_exit_code = 0
