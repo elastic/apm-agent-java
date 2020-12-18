096596a0713e8288eaaa0ca95b1b9a58ddee27fb
Sylvain Juge <sylvain.juge@elastic.co>
2020-12-18 14:39:29 +0100

    cleanup when jdk donwload/unpack fails

--- .ci/load/scripts/fetch_sdk.sh
+++ .ci/load/scripts/fetch_sdk.sh
@@ -80,7 +80,8 @@ set -x
 SUB_FOLDER="$(find "${JDK_FOLDER}" -maxdepth 1 -mindepth 1 -type d)"
 if [[ ! -d "${SUB_FOLDER}" ]]
 then
-  echo "JDK sub-folder not found in ${JDK_FOLDER}"
+  echo "JDK sub-folder not found in ${JDK_FOLDER}, cleaning up"
+  rm -rf "${JDK_FOLDER}"
   exit 2
 fi
 
