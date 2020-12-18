cc458dfc059e1599131e783719e4399b8f4ace9e
Sylvain Juge <sylvain.juge@elastic.co>
2020-12-18 16:27:19 +0100

    remove error status on failure rate
    python is not for me

--- .ci/load/scripts/locustfile.py
+++ .ci/load/scripts/locustfile.py
@@ -20,17 +20,3 @@ class QuickstartUser(HttpUser):
                 response.success()
             else:
                 response.failure()
-
-@events.quitting.add_listener
-def _(environment, **kw):
-    if environment.stats.total.fail_ratio > 30:
-        logging.error("Test failed due to failure ratio > 30%")
-        environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 200:
-        logging.error("Test failed due to average response time ratio > 200 ms")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 800:
-        logging.error("Test failed due to 95th percentile response time > 800 ms")
-        environment.process_exit_code = 1
-    else:
-        environment.process_exit_code = 0
