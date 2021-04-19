222cf4ec16ab26ab9fcdf9022e114a0bb3909532
Sylvain Juge <sylvain.juge@elastic.co>
2021-04-19 13:48:58 +0200

    make webflux experimental

--- apm-agent-plugins/apm-spring-webflux/apm-spring-webflux-plugin/src/main/java/co/elastic/apm/agent/springwebflux/WebFluxInstrumentation.java
+++ apm-agent-plugins/apm-spring-webflux/apm-spring-webflux-plugin/src/main/java/co/elastic/apm/agent/springwebflux/WebFluxInstrumentation.java
@@ -38,6 +38,7 @@ import reactor.core.publisher.Operators;
 
 import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.function.BiFunction;
@@ -55,7 +56,7 @@ public abstract class WebFluxInstrumentation extends TracerAwareInstrumentation 
 
     @Override
     public final Collection<String> getInstrumentationGroupNames() {
-        return Collections.singletonList("spring-webflux");
+        return Arrays.asList("spring-webflux", "experimental");
     }
 
     @Nullable
