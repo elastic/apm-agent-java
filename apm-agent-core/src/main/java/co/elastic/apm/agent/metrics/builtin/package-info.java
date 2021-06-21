25899a3181cb5ac30b07617fa0bdd7c42cef0b77
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2021-06-21 19:41:18 +0300

    Remove license header template

--- apm-agent-core/src/main/java/co/elastic/apm/agent/metrics/builtin/package-info.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/metrics/builtin/package-info.java
@@ -1,9 +1,4 @@
-/*-
- * #%L
- * Elastic APM Java agent
- * %%
- * Copyright (C) 2018 - 2020 Elastic and contributors
- * %%
+/*
  * Licensed to Elasticsearch B.V. under one or more contributor
  * license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright
@@ -20,7 +15,6 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * #L%
  */
 /**
  * We can't use micrometer directly as it does not support Java 7 which we want to support with the Java agent
