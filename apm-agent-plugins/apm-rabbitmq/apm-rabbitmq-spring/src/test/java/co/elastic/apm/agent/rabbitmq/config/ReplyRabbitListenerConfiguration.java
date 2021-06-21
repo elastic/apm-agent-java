46d7ab8481710f97b33c0f71338684189ac3509e
kananindzya <gonindzya@gmail.com>
2021-06-21 14:17:28 +0600

    fixed rabbit listener test

--- apm-agent-plugins/apm-rabbitmq/apm-rabbitmq-spring/src/test/java/co/elastic/apm/agent/rabbitmq/config/ReplyRabbitListenerConfiguration.java
+++ apm-agent-plugins/apm-rabbitmq/apm-rabbitmq-spring/src/test/java/co/elastic/apm/agent/rabbitmq/config/ReplyRabbitListenerConfiguration.java
@@ -27,19 +27,16 @@ package co.elastic.apm.agent.rabbitmq.config;
 import co.elastic.apm.agent.rabbitmq.TestConstants;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
-public class RabbitListenerConfiguration extends CommonRabbitListenerConfiguration {
+public class ReplyRabbitListenerConfiguration extends CommonRabbitListenerConfiguration {
 
     @RabbitListener(queues = TestConstants.QUEUE_NAME)
-    public void processMessage(String message) {
+    public String processMessage(String message) {
         testSpan();
+        return "reply";
     }
 
 }
