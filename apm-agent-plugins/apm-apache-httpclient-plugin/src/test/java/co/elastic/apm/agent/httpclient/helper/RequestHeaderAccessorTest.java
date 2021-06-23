cbb7806011b4b0a276c44acf0eaf0f91341c2185
Sylvain Juge <sylvain.juge@elastic.co>
2021-06-23 09:23:42 +0200

    post review changes

--- apm-agent-plugins/apm-apache-httpclient-plugin/src/test/java/co/elastic/apm/agent/httpclient/helper/RequestHeaderAccessorTest.java
+++ apm-agent-plugins/apm-apache-httpclient-plugin/src/test/java/co/elastic/apm/agent/httpclient/helper/RequestHeaderAccessorTest.java
@@ -28,27 +28,21 @@ import co.elastic.apm.agent.impl.transaction.AbstractTextHeaderGetterTest;
 import org.apache.http.HttpRequest;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicHttpRequest;
-import org.junit.jupiter.api.Nested;
 
 import java.util.List;
 import java.util.Map;
 
-class RequestHeaderAccessorTest {
+class RequestHeaderAccessorTest extends AbstractTextHeaderGetterTest<RequestHeaderAccessor, HttpRequest> {
 
-    @Nested
-    class GetterTest extends AbstractTextHeaderGetterTest<RequestHeaderAccessor, HttpRequest> {
-
-        @Override
-        protected RequestHeaderAccessor createTextHeaderGetter() {
-            return RequestHeaderAccessor.INSTANCE;
-        }
-
-        @Override
-        protected HttpRequest createCarrier(Map<String, List<String>> map) {
-            HttpRequest request = new BasicHttpRequest("GET", "http://fake/");
-            map.forEach((k, values) -> values.forEach(v -> request.addHeader(new BasicHeader(k, v))));
-            return request;
-        }
+    @Override
+    protected RequestHeaderAccessor createTextHeaderGetter() {
+        return RequestHeaderAccessor.INSTANCE;
     }
 
+    @Override
+    protected HttpRequest createCarrier(Map<String, List<String>> map) {
+        HttpRequest request = new BasicHttpRequest("GET", "http://fake/");
+        map.forEach((k, values) -> values.forEach(v -> request.addHeader(new BasicHeader(k, v))));
+        return request;
+    }
 }
