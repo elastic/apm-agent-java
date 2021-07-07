b222d37b5def1d95be2b714fb469c3d5269dbe50
Felix Barnsteiner <felixbarny@users.noreply.github.com>
2021-07-07 08:09:48 +0200

    Remove unnecessary import
    Co-authored-by: eyalkoren <41850454+eyalkoren@users.noreply.github.com>

--- apm-agent-core/src/main/java/co/elastic/apm/agent/report/IntakeV2ReportingEventHandler.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/report/IntakeV2ReportingEventHandler.java
@@ -21,7 +21,6 @@ package co.elastic.apm.agent.report;
 import co.elastic.apm.agent.report.processor.ProcessorEventHandler;
 import co.elastic.apm.agent.report.serialize.PayloadSerializer;
 import co.elastic.apm.agent.premain.ThreadUtils;
-import com.lmax.disruptor.Sequence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
