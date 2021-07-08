d85c09d6a9fdf0859cc5eb1ec84165264d1f40ae
Tobias Stadler <ts.stadler@gmx.de>
2021-07-08 09:20:41 +0200

    Added basic support for com.sun.net.httpserver.HttpServer (#1853) (#1854)
    * Added basic support for com.sun.net.httpserver.HttpServer (#1853)
    
    * Added #1854 to changelog
    
    * Fix changelog
    
    * Incorporated review by @SylvainJuge
    
    * fix class not found + handler instr. at runtime
    
    Co-authored-by: Sylvain Juge <sylvain.juge@elastic.co>

---
+++ apm-agent-plugins/apm-jdk-httpserver-plugin/src/main/java/co/elastic/apm/agent/httpserver/JdkHttpServerInstrumentation.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.httpserver;
+
+import co.elastic.apm.agent.sdk.ElasticApmInstrumentation;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public abstract class JdkHttpServerInstrumentation extends ElasticApmInstrumentation {
+
+    @Override
+    public Collection<String> getInstrumentationGroupNames() {
+        return Arrays.asList("jdk-httpserver", "experimental");
+    }
+}
