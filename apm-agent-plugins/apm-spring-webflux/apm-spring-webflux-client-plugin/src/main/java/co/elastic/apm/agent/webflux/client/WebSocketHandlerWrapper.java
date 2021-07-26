3d75f15c0043d30b16244f7e3a34de8c7a6602ad
Abdul Aziz Ali <abdulazizali@acm.org>
2021-07-26 17:30:24 +0800

    Implement Webflux client instrumentation (#1306)

---
+++ apm-agent-plugins/apm-spring-webflux/apm-spring-webflux-client-plugin/src/main/java/co/elastic/apm/agent/webflux/client/WebSocketHandlerWrapper.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.webflux.client;
+
+import co.elastic.apm.agent.impl.Tracer;
+import co.elastic.apm.agent.impl.transaction.AbstractSpan;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+import static co.elastic.apm.agent.webflux.client.WebSocketClientInstrumentation.APM_PARENT_SPAN;
+
+public class WebSocketHandlerWrapper implements WebSocketHandler {
+
+    public static final Logger logger = LoggerFactory.getLogger(WebSocketHandlerWrapper.class);
+    private final Tracer tracer;
+    private final WebSocketHandler actual;
+    private AbstractSpan parentSpan;
+
+    public WebSocketHandlerWrapper(WebSocketHandler actual, AbstractSpan parentSpan, Tracer tracer) {
+        this.actual = actual;
+        this.parentSpan = parentSpan;
+        this.tracer = tracer;
+    }
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        String logPrefix = session.getId();
+
+        if (logPrefix != null && parentSpan != null) {
+            WebfluxClientSubscriber.getLogPrefixMap().put(logPrefix, parentSpan);
+            session.getAttributes().put(APM_PARENT_SPAN, logPrefix);
+
+            return (Mono<Void>)WebfluxClientHelper.wrapSubscriber((Publisher) actual.handle(session), logPrefix, tracer, "WebSocketHandlerHandle-");
+        } else {
+            return actual.handle(session);
+        }
+    }
+
+    public AbstractSpan getParentSpan() {
+        return parentSpan;
+    }
+
+    public void setParentSpan(AbstractSpan parentSpan) {
+        this.parentSpan = parentSpan;
+    }
+
+
+}
---
+++ apm-agent-plugins/apm-spring-webflux/apm-spring-webflux-client-plugin/src/main/java/co/elastic/apm/agent/webflux/client/WebfluxClientHelper.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.webflux.client;
+
+import co.elastic.apm.agent.http.client.HttpClientHelper;
+import co.elastic.apm.agent.impl.Tracer;
+import co.elastic.apm.agent.impl.transaction.AbstractSpan;
+import co.elastic.apm.agent.impl.transaction.Outcome;
+import co.elastic.apm.agent.impl.transaction.Transaction;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpMethod;
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Operators;
+
+import java.net.URI;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class WebfluxClientHelper {
+
+    public static AbstractSpan createHttpSpan(AbstractSpan parentSpan, HttpMethod httpMethod, URI uri) {
+        return HttpClientHelper.startHttpClientSpan(parentSpan, httpMethod.toString(), uri, uri.getHost());
+    }
+
+    public static Transaction getOrCreateTransaction(Tracer tracer, Class clazz) {
+        Transaction t = tracer.currentTransaction();
+        if (t == null) {
+            //TODO: check the object
+            t = tracer.startRootTransaction(clazz.getClassLoader());
+            //FIXME
+            ((Transaction) t)
+                .withName("name")
+                .withType("test")
+                .withResult("success")
+                .withOutcome(Outcome.SUCCESS)
+                .activate();
+        } else {
+            //FIXME: startChildTransaction?
+        }
+        return t;
+    }
+
+    public static <T> Publisher<T> wrapSubscriber(Publisher<T> publisher, final String subscriberKey, final Tracer tracer, final String prependId) {
+
+        Function<? super Publisher<T>, ? extends Publisher<T>> lift = Operators.liftPublisher(
+            new BiFunction<Publisher, CoreSubscriber<? super T>, CoreSubscriber<? super T>>() {
+                @Override
+                public CoreSubscriber<? super T> apply(Publisher publisher, CoreSubscriber<? super T> coreSubscriber) {
+
+                    if (!WebfluxClientSubscriber.getLogPrefixSubscriberMap().containsKey(subscriberKey + prependId)) {
+                        WebfluxClientSubscriber.getLogPrefixSubscriberMap().put(subscriberKey + prependId, true);
+                        return new WebfluxClientSubscriber(coreSubscriber, subscriberKey, tracer, prependId);
+                    } else {
+                        return coreSubscriber;
+                    }
+                }
+            }
+        );
+
+        if (publisher instanceof Mono) {
+            publisher = ((Mono) publisher).transform(lift);
+        } else if (publisher instanceof Flux) {
+            publisher = ((Flux) publisher).transform(lift);
+        }
+
+        return publisher;
+    }
+}
