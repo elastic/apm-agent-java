276fa161384f030e835e9ce515e5b87cb502c982
kananindzya <gonindzya@gmail.com>
2021-07-28 13:19:52 +0600

    added openj9 images

--- integration-tests/application-server-integration-tests/src/test/java/co/elastic/apm/servlet/TomcatIT.java
+++ integration-tests/application-server-integration-tests/src/test/java/co/elastic/apm/servlet/TomcatIT.java
@@ -40,13 +40,15 @@ public class TomcatIT extends AbstractTomcatIT {
     @Parameterized.Parameters(name = "Tomcat {0}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][]{
-            {"7-jre7-slim"},
-            {"8.5.0-jre8"},
-            {"8.5-jre8-slim"},
-            {"9-jre9-slim"},
-            {"9-jre10-slim"},
-            {"9-jre11-slim"},
-            {"9.0.39-jdk14-openjdk-oracle"}
+//            {"7-jre7-slim"},
+//            {"8.5.0-jre8"},
+//            {"8.5-jre8-slim"},
+//            {"9-jre9-slim"},
+//            {"9-jre10-slim"},
+//            {"9-jre11-slim"},
+//            {"9.0.39-jdk14-openjdk-oracle"},
+//            {"jdk8-adoptopenjdk-openj9"},
+            {"jdk11-adoptopenjdk-openj9"}
         });
     }
 
