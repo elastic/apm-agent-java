669bb10dc0d64d0aac06bad36b019378f054e3da
kananindzya <gonindzya@gmail.com>
2021-08-21 11:30:11 +0600

    feature added future to async tests, changed log level

--- apm-agent-plugins/apm-jetty-httpclient-plugin/src/test/java/co/elastic/apm/agent/jettyclient/JettyClientASyncInstrumentationTest.java
+++ apm-agent-plugins/apm-jetty-httpclient-plugin/src/test/java/co/elastic/apm/agent/jettyclient/JettyClientASyncInstrumentationTest.java
@@ -6,6 +6,8 @@ import org.eclipse.jetty.client.api.Response;
 import org.eclipse.jetty.client.api.Result;
 import org.junit.Before;
 
+import java.util.concurrent.CompletableFuture;
+
 
 public class JettyClientASyncInstrumentationTest extends AbstractHttpClientInstrumentationTest {
 
@@ -19,13 +21,16 @@ public class JettyClientASyncInstrumentationTest extends AbstractHttpClientInstr
     @Override
     protected void performGet(String path) throws Exception {
         httpClient.start();
+        final CompletableFuture<Void> future = new CompletableFuture<>();
         httpClient.newRequest(path)
             .send(new Response.CompleteListener() {
                 @Override
                 public void onComplete(Result result) {
                     System.out.println(String.format("Got response with status = %s", result.getResponse().getStatus()));
+                    future.complete(null);
                 }
             });
+        future.get();
         httpClient.stop();
     }
 }
