14e7639eeefb1ae9d5b2b6f93a3e6f875a57aee1
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2021-09-07 12:37:16 +0200

    Remove shading of non-logging-related dependencies
    The log shading config is more peculiar

--- apm-agent-core/src/main/java/co/elastic/apm/agent/bci/IndyPluginClassLoaderFactory.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/bci/IndyPluginClassLoaderFactory.java
@@ -45,7 +45,7 @@ public class IndyPluginClassLoaderFactory {
 
     /**
      * Creates an isolated CL that has two parents: the target class loader and the agent CL.
-     * The agent class loader is currently the bootstrap CL but in the future it will be an isolated CL that is a child of the bootstrap CL.
+     * The agent class loader is an isolated CL that is a child of the bootstrap CL.
      */
     public synchronized static ClassLoader getOrCreatePluginClassLoader(@Nullable ClassLoader targetClassLoader,
                                                                         List<String> classesToInject,
