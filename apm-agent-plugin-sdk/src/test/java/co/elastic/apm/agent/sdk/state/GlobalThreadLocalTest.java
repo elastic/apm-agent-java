122633e70b96a0b1576a6e1944c736978527a234
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2021-09-13 15:22:46 +0300

    Fixing GlobalThreadLocal to use per-thread default values

--- apm-agent-plugin-sdk/src/test/java/co/elastic/apm/agent/sdk/state/GlobalThreadLocalTest.java
+++ apm-agent-plugin-sdk/src/test/java/co/elastic/apm/agent/sdk/state/GlobalThreadLocalTest.java
@@ -20,12 +20,103 @@ package co.elastic.apm.agent.sdk.state;
 
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.Nullable;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GlobalThreadLocalTest {
 
-    GlobalThreadLocal<String> threadLocal = GlobalThreadLocal.get(GlobalThreadLocalTest.class, "test");
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Test
     void setNullValueShouldNotThrow() {
-        threadLocal.set(null);
+        GlobalThreadLocal.get(GlobalThreadLocalTest.class, "setNullValueShouldNotThrow").set(null);
+    }
+
+    @Test
+    void testNonConstantDefaultValue() throws ExecutionException, InterruptedException {
+        final GlobalThreadLocal<Object> threadLocal = GlobalThreadLocal.get(
+            GlobalThreadLocalTest.class,
+            "testNonConstantDefaultValue",
+            new ObjectValueSupplier()
+        );
+        Object mainThreadDefaultValue = threadLocal.get();
+        assertThat(mainThreadDefaultValue).isNotNull();
+        Future<Object> future = executor.submit((Callable<Object>) threadLocal::get);
+        assertThat(future.get()).isNotEqualTo(mainThreadDefaultValue);
+    }
+
+    @Test
+    void testConstantDefaultValue() throws ExecutionException, InterruptedException {
+        final GlobalThreadLocal<Object> threadLocal = GlobalThreadLocal.get(
+            GlobalThreadLocalTest.class,
+            "testConstantDefaultValue",
+            new ConstantValueSupplier()
+        );
+        Object mainThreadDefaultValue = threadLocal.get();
+        assertThat(mainThreadDefaultValue).isNotNull();
+        Future<Object> future = executor.submit((Callable<Object>) threadLocal::get);
+        assertThat(future.get()).isEqualTo(mainThreadDefaultValue);
+    }
+
+    @Test
+    void testNullDefaultValue() {
+        final GlobalThreadLocal<Object> threadLocal = GlobalThreadLocal.get(
+            GlobalThreadLocalTest.class,
+            "testNullDefaultValue",
+            null
+        );
+        assertThat(threadLocal.get()).isNull();
+    }
+
+    @Test
+    void testNonDefaultValue() throws ExecutionException, InterruptedException {
+        final GlobalThreadLocal<Object> threadLocal = GlobalThreadLocal.get(
+            GlobalThreadLocalTest.class,
+            "testNonDefaultValue",
+            null
+        );
+
+        threadLocal.set("main");
+        assertThat(threadLocal.get()).isEqualTo("main");
+
+        Future<Object> future = executor.submit(() -> {
+            threadLocal.set("worker");
+            return threadLocal.get();
+        });
+        assertThat(future.get()).isEqualTo("worker");
+
+        future = executor.submit(threadLocal::getAndRemove);
+        // we can rely on that because we use a single thread executor
+        assertThat(future.get()).isEqualTo("worker");
+        future = executor.submit(threadLocal::getAndRemove);
+        // we can rely on that because we use a single thread executor
+        assertThat(future.get()).isNull();
+
+        assertThat(threadLocal.getAndRemove()).isEqualTo("main");
+        assertThat(threadLocal.get()).isNull();
+    }
+
+    private static class ObjectValueSupplier implements GlobalThreadLocal.DefaultValueSupplier<Object> {
+        @Nullable
+        @Override
+        public Object getDefaultValueForThread() {
+            return new Object();
+        }
+    }
+
+    private static class ConstantValueSupplier implements GlobalThreadLocal.DefaultValueSupplier<Object> {
+        private final Object defaultValue = new Object();
+
+        @Nullable
+        @Override
+        public Object getDefaultValueForThread() {
+            return defaultValue;
+        }
     }
 }
