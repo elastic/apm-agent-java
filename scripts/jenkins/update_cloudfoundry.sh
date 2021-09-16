8d311d000c5f74dda4f75d359c1744c85213e8c3
Victor Martinez <victormartinezrubio@gmail.com>
2021-09-16 14:57:18 +0100

    [release] use labels with docker and ubuntu (#2132)

--- scripts/jenkins/update_cloudfoundry.sh
+++ scripts/jenkins/update_cloudfoundry.sh
@@ -7,5 +7,11 @@ set -exo pipefail
 BASE_URL=https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent
 CF_FILE=cloudfoundry/index.yml
 
+# make script idempotent if release is already in CF descriptor
+set +e
+grep -e "^${TAG_VER}" ${CF_FILE}
+[[ $? == 0 ]] && exit 0
+
+set -e
 echo "${TAG_VER}: ${BASE_URL}/${TAG_VER}/elastic-apm-agent-$TAG_VER.jar" >> ${CF_FILE}
 git commit ${CF_FILE} -m "Update cloudfoundry for ${TAG_VER} release"
