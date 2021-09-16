9c073beafd1d273b2e6f8a5d8de65a61765c9dcf
Sylvain Juge <sylvain.juge@elastic.co>
2021-09-16 14:06:06 +0200

    [release] avoid external script when we can

--- scripts/jenkins/maven_artifact_url.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# We are looking for something like this:
-# https://repo1.maven.org/maven2/co/elastic/apm/apm-agent-java/1.1.12/apm-agent-java-1.1.12.pom
-
-# Requires that $TAG_VER be present in the environment
-
-echo "https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${TAG_VER}/elastic-apm-agent-${TAG_VER}.pom"
