1fcac13259400fa6fa3bf963cdbb729d84c62138
kananindzya <gonindzya@gmail.com>
2021-11-02 11:36:12 +0600

    fixed annotation

--- apm-agent-plugins/apm-rabbitmq/apm-rabbitmq-spring/src/main/java/co/elastic/apm/agent/rabbitmq/SpringAmqpBatchMessageListenerInstrumentation.java
+++ apm-agent-plugins/apm-rabbitmq/apm-rabbitmq-spring/src/main/java/co/elastic/apm/agent/rabbitmq/SpringAmqpBatchMessageListenerInstrumentation.java
@@ -3,8 +3,8 @@ package co.elastic.apm.agent.rabbitmq;
 
 import co.elastic.apm.agent.impl.ElasticApmTracer;
 import co.elastic.apm.agent.impl.GlobalTracer;
-import co.elastic.apm.agent.sdk.advice.AssignTo;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.AssignReturned.ToArguments.ToArgument;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.springframework.amqp.core.Message;
@@ -12,7 +12,9 @@ import org.springframework.amqp.core.Message;
 import javax.annotation.Nullable;
 import java.util.List;
 
-import static net.bytebuddy.matcher.ElementMatchers.*;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 public class SpringAmqpBatchMessageListenerInstrumentation extends SpringBaseInstrumentation {
     @Override
@@ -35,7 +37,7 @@ public class SpringAmqpBatchMessageListenerInstrumentation extends SpringBaseIns
         }
 
         @Nullable
-        @AssignTo.Argument(0)
+        @Advice.AssignReturned.ToArguments(@ToArgument(0))
         @Advice.OnMethodEnter(inline = false)
         public static List<Message> beforeOnBatch(@Advice.Argument(0) @Nullable final List<Message> messageBatch) {
             if (!tracer.isRunning() || tracer.currentTransaction() != null || messageBatch == null) {
