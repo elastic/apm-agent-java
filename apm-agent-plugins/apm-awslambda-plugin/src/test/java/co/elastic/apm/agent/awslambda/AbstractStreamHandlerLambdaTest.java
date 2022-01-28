d8fd523ea8eff6c056c5f38852d28d934465f1c5
Alexander Wert <alexander.wert@elastic.co>
2022-01-28 14:48:54 +0100

    Fixed metadata test for lambda

--- apm-agent-plugins/apm-awslambda-plugin/src/test/java/co/elastic/apm/agent/awslambda/AbstractStreamHandlerLambdaTest.java
+++ apm-agent-plugins/apm-awslambda-plugin/src/test/java/co/elastic/apm/agent/awslambda/AbstractStreamHandlerLambdaTest.java
@@ -45,7 +45,6 @@ public abstract class AbstractStreamHandlerLambdaTest extends AbstractInstrument
         MetaData metaData = tracer.getMetaDataFuture().get(100, TimeUnit.MILLISECONDS);
         assertThat(metaData).isNotNull();
         assertThat(metaData.getService()).isNotNull();
-        assertThat(metaData.getService().getId()).isEqualTo(TestContext.FUNCTION_ARN);
         assertThat(metaData.getService().getRuntime()).isNotNull();
         assertThat(metaData.getService().getRuntime().getName()).startsWith("AWS_Lambda_java");
         assertThat(metaData.getService().getRuntime().getVersion()).isEqualTo(System.getProperty("java.version"));
