31e2608d4cb78cf257975ec3a3b39ca857d7f7b8
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-07 16:54:16 +0000

    remove unrequired entry in the changelog :

--- .ci/create-arn-table.sh
+++ .ci/create-arn-table.sh
@@ -6,14 +6,11 @@ set -o pipefail
 #
 #   - AWS_FOLDER      - that's the location of the publish-layer-version output for each region
 #	- SUFFIX_ARN_FILE - that's the output file.
-#	- RELEASE_VERSION - that's the release version to be published in GitHub.
 #
 
 ARN_FILE=${SUFFIX_ARN_FILE}
 
 {
-    echo "[Release Notes for ${RELEASE_VERSION}](https://www.elastic.co/guide/en/apm/agent/java/current/release-notes-1.x.html#release-notes-${RELEASE_VERSION})"
-	echo ''
 	echo "### ARN"
 	echo ''
 	echo '|Region|ARN|'
