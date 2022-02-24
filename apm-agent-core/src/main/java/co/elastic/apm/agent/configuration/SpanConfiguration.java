a2d66570d40ae8cfe642f532276d3f6c5fe7c457
Tobias Stadler <ts.stadler@gmx.de>
2022-02-24 13:39:00 +0100

    Removed internal tag from span compression related configuration options

--- apm-agent-core/src/main/java/co/elastic/apm/agent/configuration/SpanConfiguration.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/configuration/SpanConfiguration.java
@@ -30,7 +30,7 @@ public class SpanConfiguration extends ConfigurationOptionProvider {
     private final ConfigurationOption<Boolean> spanCompressionEnabled = ConfigurationOption.booleanOption()
         .key("span_compression_enabled")
         .configurationCategory(HUGE_TRACES_CATEGORY)
-        .tags("added[1.30.0]", "internal")
+        .tags("added[1.30.0]")
         .description("Setting this option to true will enable span compression feature.\n" +
             "Span compression reduces the collection, processing, and storage overhead, and removes clutter from the UI. " +
             "The tradeoff is that some information such as DB statements of all the compressed spans will not be collected.")
@@ -40,7 +40,7 @@ public class SpanConfiguration extends ConfigurationOptionProvider {
     private final ConfigurationOption<TimeDuration> spanCompressionExactMatchMaxDuration = TimeDurationValueConverter.durationOption("ms")
         .key("span_compression_exact_match_max_duration")
         .configurationCategory(HUGE_TRACES_CATEGORY)
-        .tags("added[1.30.0]", "internal")
+        .tags("added[1.30.0]")
         .description("Consecutive spans that are exact match and that are under this threshold will be compressed into a single composite span. " +
             "This option does not apply to composite spans. This reduces the collection, processing, and storage overhead, and removes clutter from the UI. " +
             "The tradeoff is that the DB statements of all the compressed spans will not be collected.")
@@ -50,7 +50,7 @@ public class SpanConfiguration extends ConfigurationOptionProvider {
     private final ConfigurationOption<TimeDuration> spanCompressionSameKindMaxDuration = TimeDurationValueConverter.durationOption("ms")
         .key("span_compression_same_kind_max_duration")
         .configurationCategory(HUGE_TRACES_CATEGORY)
-        .tags("added[1.30.0]", "internal")
+        .tags("added[1.30.0]")
         .description("Consecutive spans to the same destination that are under this threshold will be compressed into a single composite span. " +
             "This option does not apply to composite spans. This reduces the collection, processing, and storage overhead, and removes clutter from the UI. " +
             "The tradeoff is that the DB statements of all the compressed spans will not be collected.")
