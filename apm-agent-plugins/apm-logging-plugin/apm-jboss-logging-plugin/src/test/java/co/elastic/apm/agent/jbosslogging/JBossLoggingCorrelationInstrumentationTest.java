9d4bcae8d0d04ed8c293ae14e3970f88a1f8aee7
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2022-02-28 10:42:27 +0200

    Refactoring apm-logging-plugin

---
+++ apm-agent-plugins/apm-logging-plugin/apm-jboss-logging-plugin/src/test/java/co/elastic/apm/agent/jbosslogging/JBossLoggingCorrelationInstrumentationTest.java
@@ -0,0 +1,151 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.jbosslogging;
+
+import co.elastic.apm.agent.AbstractInstrumentationTest;
+import co.elastic.apm.agent.impl.TextHeaderMapAccessor;
+import co.elastic.apm.agent.impl.error.ErrorCapture;
+import co.elastic.apm.agent.impl.transaction.TraceContext;
+import co.elastic.apm.agent.impl.transaction.Transaction;
+import co.elastic.apm.agent.logging.AbstractLogCorrelationHelper;
+import org.jboss.logging.Logger;
+import org.jboss.logging.MDC;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import static co.elastic.apm.agent.impl.transaction.TraceContext.W3C_TRACE_PARENT_TEXTUAL_HEADER_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JBossLoggingCorrelationInstrumentationTest extends AbstractInstrumentationTest {
+
+    public static final String LOGGER_NAME = "Test-Logger";
+
+    private static final LoggingCorrelationVerifier loggingCorrelationVerifier = new LoggingCorrelationVerifier();
+    private static Logger logger;
+
+    private Transaction transaction;
+
+    @BeforeAll
+    static void initializeLogger() {
+        System.setProperty("org.jboss.logging.provider", "jdk");
+        logger = Logger.getLogger(LOGGER_NAME);
+
+        // Getting the underlying JULI logger in order to set a handler
+        java.util.logging.Logger juliLogger = java.util.logging.Logger.getLogger(LOGGER_NAME);
+        assertThat(juliLogger).isNotNull();
+        juliLogger.addHandler(new LoggingCorrelationVerifier());
+    }
+
+    @BeforeEach
+    public void setup() {
+        transaction = Objects.requireNonNull(tracer.startRootTransaction(null)).activate();
+        loggingCorrelationVerifier.reset();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        transaction.deactivate().end();
+    }
+
+    @Test
+    public void testSimple() {
+        assertThat(loggingCorrelationVerifier.isVerified()).isFalse();
+        // TraceContext#toString() returns the text representation of the traceparent header
+        logger.info(transaction.getTraceContext());
+        assertThat(loggingCorrelationVerifier.isVerified()).isTrue();
+    }
+
+    // todo: enable once support for JULI error logging is added
+    @Test
+    @Disabled
+    public void testErrorLogging() {
+        assertThat(loggingCorrelationVerifier.isVerified()).isFalse();
+        // TraceContext#toString() returns the text representation of the traceparent header
+        logger.error(transaction.getTraceContext());
+        assertThat(loggingCorrelationVerifier.isVerified()).isTrue();
+    }
+
+    private static class LoggingCorrelationVerifier extends Handler {
+        private boolean verified;
+
+        public void reset() {
+            verified = false;
+        }
+
+        public boolean isVerified() {
+            return verified;
+        }
+
+        public void setVerified() {
+            verified = true;
+        }
+
+        @Override
+        public void publish(LogRecord record) {
+            try {
+                Object traceId = MDC.get(AbstractLogCorrelationHelper.TRACE_ID_MDC_KEY);
+                System.out.println("traceId = " + traceId);
+                assertThat(traceId).isNotNull();
+                Object transactionId = MDC.get(AbstractLogCorrelationHelper.TRANSACTION_ID_MDC_KEY);
+                System.out.println("transactionId = " + transactionId);
+                assertThat(transactionId).isNotNull();
+                Object errorId = MDC.get(AbstractLogCorrelationHelper.ERROR_ID_MDC_KEY);
+                System.out.println("errorId = " + transactionId);
+                boolean shouldContainErrorId = record.getLevel().getName().equals("ERROR");
+                String traceParent = record.getMessage();
+                assertThat(traceParent).isNotNull();
+                Map<String, String> textHeaderMap = Map.of(W3C_TRACE_PARENT_TEXTUAL_HEADER_NAME, traceParent);
+                TraceContext childTraceContext = TraceContext.with64BitId(tracer);
+                TraceContext.<Map<String, String>>getFromTraceContextTextHeaders().asChildOf(childTraceContext, textHeaderMap, TextHeaderMapAccessor.INSTANCE);
+                System.out.println("childTraceContext = " + childTraceContext);
+                assertThat(childTraceContext.getTraceId().toString()).isEqualTo(traceId.toString());
+                assertThat(childTraceContext.getParentId().toString()).isEqualTo(transactionId.toString());
+                if (shouldContainErrorId) {
+                    assertThat(errorId).isNotNull();
+                    ErrorCapture activeError = ErrorCapture.getActive();
+                    assertThat(activeError).isNotNull();
+                    assertThat(activeError.getTraceContext().getId().toString()).isEqualTo(errorId.toString());
+                } else {
+                    assertThat(errorId).isNull();
+                }
+                loggingCorrelationVerifier.setVerified();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+
+        @Override
+        public void flush() {
+
+        }
+
+        @Override
+        public void close() throws SecurityException {
+
+        }
+    }
+}
---
+++ apm-agent-plugins/apm-logging-plugin/apm-logback-plugin/apm-logback-plugin-impl/src/main/java/co/elastic/apm/agent/logback/LogbackLogShadingInstrumentation.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.logback;
+
+import co.elastic.apm.agent.logging.AbstractLogIntegrationInstrumentation;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import java.util.Collection;
+
+import static co.elastic.apm.agent.bci.bytebuddy.CustomElementMatchers.classLoaderCanLoadClass;
+import static net.bytebuddy.matcher.ElementMatchers.isBootstrapClassLoader;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+import static net.bytebuddy.matcher.ElementMatchers.takesGenericArgument;
+
+public abstract class LogbackLogShadingInstrumentation extends AbstractLogIntegrationInstrumentation {
+
+    @Override
+    public Collection<String> getInstrumentationGroupNames() {
+        Collection<String> ret = super.getInstrumentationGroupNames();
+        ret.add("logback-ecs");
+        return ret;
+    }
+
+    @Override
+    public ElementMatcher.Junction<ClassLoader> getClassLoaderMatcher() {
+        return not(isBootstrapClassLoader()).and(classLoaderCanLoadClass("ch.qos.logback.core.OutputStreamAppender"));
+    }
+
+    @Override
+    public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+        return named("ch.qos.logback.core.OutputStreamAppender");
+    }
+
+    public static class ShadingInstrumentation extends LogbackLogShadingInstrumentation {
+
+        /**
+         * Instrumenting {@link ch.qos.logback.core.OutputStreamAppender#append(Object)}
+         */
+        @Override
+        public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+            return named("append").and(takesGenericArgument(0, TypeDescription.Generic.Builder.typeVariable("E").build()));
+        }
+
+        @Override
+        public String getAdviceClassName() {
+            return "co.elastic.apm.agent.logback.LogbackAppenderAppendAdvice";
+        }
+
+    }
+
+    public static class StopAppenderInstrumentation extends LogbackLogShadingInstrumentation {
+
+        /**
+         * Instrumenting {@link ch.qos.logback.core.OutputStreamAppender#stop}
+         */
+        @Override
+        public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+            return named("stop").and(takesArguments(0));
+        }
+
+        @Override
+        public String getAdviceClassName() {
+            return "co.elastic.apm.agent.logback.LogbackAppenderStopAdvice";
+        }
+
+    }
+}
