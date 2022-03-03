e1935ebc4da40cbc4ee4d3b51a671a78b48b3a77
Tobias Stadler <ts.stadler@gmx.de>
2022-03-03 17:23:29 +0100

    Added (initial) support for compressing spans (#2477)
    * Added (initial) support for compressing spans
    
    * Added #2477 to the changelog
    
    * Added test case for context propagation
    
    * It is not necessary to activate the transaction
    
    * Report "compressed spans" as dropped
    
    * Added assertions for span counts to compression tests
    
    * Move compression configuration to new class as discussed with @jackshirazi
    
    * Clear bufferedSpan when the transaction/span is recycled
    
    * Prevent NullPointerException
    
    * Cache "SpanConfiguration" when the transaction starts to provide consistent span compression
    
    * Review by @jackshirazi
    
    * Review by @jackshirazi
    
    * removed the synchronized locked
    
    * Review by @jackshirazi
    
    * A sibling span might have been started before the composite span
    
    * Fix NPE in `switch(composite.getCompressionStrategy())`
    
    * Added tests for parallel span compression

---
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/impl/transaction/Composite.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.impl.transaction;
+
+import co.elastic.apm.agent.objectpool.Recyclable;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Composite implements Recyclable {
+
+    private final AtomicInteger count = new AtomicInteger(0);
+
+    private final AtomicLong sum = new AtomicLong(0L);
+
+    private final AtomicReference<String> compressionStrategy = new AtomicReference<>(null);
+
+    public boolean init(long sum, String compressionStrategy) {
+        if (!this.count.compareAndSet(0, 1)) {
+            return false;
+        }
+        this.sum.set(sum);
+        this.compressionStrategy.set(compressionStrategy);
+        return true;
+    }
+
+    public int getCount() {
+        return count.get();
+    }
+
+    public void increaseCount() {
+        count.incrementAndGet();
+    }
+
+    public long getSum() {
+        return sum.get();
+    }
+
+    public double getSumMs() {
+        return sum.get() / 1000.0;
+    }
+
+    public void increaseSum(long delta) {
+        this.sum.addAndGet(delta);
+    }
+
+    public String getCompressionStrategy() {
+        return compressionStrategy.get();
+    }
+
+    @Override
+    public void resetState() {
+        this.count.set(0);
+        this.sum.set(0L);
+        this.compressionStrategy.set(null);
+    }
+}
