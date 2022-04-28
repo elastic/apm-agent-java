25fe8d4c71aa7a065168c79f0633bf6b660c8833
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2022-04-28 18:20:22 +0300

    Vertx avoid wrapping (#2564)

--- apm-agent-plugins/apm-vertx/apm-vertx3-plugin/src/main/java/co/elastic/apm/agent/vertx/v3/web/HttpServerRequestImplEndInstrumentation.java
+++ apm-agent-plugins/apm-vertx/apm-vertx3-plugin/src/main/java/co/elastic/apm/agent/vertx/v3/web/HttpServerRequestImplEndInstrumentation.java
@@ -16,47 +16,63 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package co.elastic.apm.agent.vertx.v3.web.http1;
+package co.elastic.apm.agent.vertx.v3.web;
 
-import co.elastic.apm.agent.vertx.v3.web.WebHelper;
-import co.elastic.apm.agent.vertx.v3.web.WebInstrumentation;
+import co.elastic.apm.agent.impl.transaction.Transaction;
+import co.elastic.apm.agent.sdk.logging.Logger;
+import co.elastic.apm.agent.sdk.logging.LoggerFactory;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.http.impl.HttpServerRequestImpl;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
 import net.bytebuddy.matcher.ElementMatcher;
 
+import javax.annotation.Nullable;
+
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.takesNoArguments;
 
 /**
- * Instruments {@link io.vertx.core.http.impl.HttpServerRequestImpl#doEnd()} to remove the context from the context map again.
+ * Instruments {@link HttpServerRequestImpl#handleEnd()} and {@link io.vertx.core.http.impl.Http2ServerRequestImpl#handleEnd} to finalize
+ * the transaction and remove the mapping.
  */
 @SuppressWarnings("JavadocReference")
 public class HttpServerRequestImplEndInstrumentation extends WebInstrumentation {
 
     @Override
     public ElementMatcher<? super TypeDescription> getTypeMatcher() {
-        return named("io.vertx.core.http.impl.HttpServerRequestImpl");
+        return named("io.vertx.core.http.impl.HttpServerRequestImpl")
+            .or(named("io.vertx.core.http.impl.Http2ServerRequestImpl"));
     }
 
     @Override
     public ElementMatcher<? super MethodDescription> getMethodMatcher() {
-        return named("doEnd").or(named("onEnd")).and(takesNoArguments());
+        return named("handleEnd");
     }
 
     @Override
     public String getAdviceClassName() {
-        return "co.elastic.apm.agent.vertx.v3.web.http1.HttpServerRequestImplEndInstrumentation$HttpRequestEndAdvice";
+        return "co.elastic.apm.agent.vertx.v3.web.HttpServerRequestImplEndInstrumentation$HttpRequestEndAdvice";
     }
 
     public static class HttpRequestEndAdvice {
 
+        private static final Logger log = LoggerFactory.getLogger(HttpRequestEndAdvice.class);
+
         private static final WebHelper helper = WebHelper.getInstance();
 
         @Advice.OnMethodExit(suppress = Throwable.class, inline = false)
-        public static void exit(@Advice.This HttpServerRequestImpl request) {
-            helper.removeTransactionFromContext(request);
+        public static void exit(@Advice.This HttpServerRequest request,
+                                @Advice.FieldValue(value = "response", typing = Assigner.Typing.DYNAMIC) @Nullable HttpServerResponse response) {
+            Transaction transaction = helper.removeTransactionMapping(request);
+            if (transaction != null) {
+                helper.finalizeTransaction(response, transaction);
+                log.debug("VERTX-DEBUG: ended Vert.x transaction {} with details from this response: {}", transaction, response);
+            } else {
+                log.debug("VERTX-DEBUG: could not find a transaction for the following Vert.x request: {}", request);
+            }
         }
     }
 }
