dbec817e7df14cc6b380e28224fdea5d7b0ed192
Alexander Wert <alexander.wert@elastic.co>
2022-05-03 08:18:44 +0200

    fixed Java 7 compatibility problems

--- apm-agent-plugins/apm-aws-sdk/apm-aws-sdk-common/src/main/java/co/elastic/apm/agent/awssdk/common/AbstractS3InstrumentationHelper.java
+++ apm-agent-plugins/apm-aws-sdk/apm-aws-sdk-common/src/main/java/co/elastic/apm/agent/awssdk/common/AbstractS3InstrumentationHelper.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.apm.agent.awssdk.common;
 
 import co.elastic.apm.agent.impl.ElasticApmTracer;
@@ -6,7 +24,6 @@ import co.elastic.apm.agent.impl.transaction.Span;
 
 import javax.annotation.Nullable;
 import java.net.URI;
-import java.util.Objects;
 
 public abstract class AbstractS3InstrumentationHelper<R, C> {
     private static final String S3_TYPE = "s3";
@@ -42,10 +59,11 @@ public abstract class AbstractS3InstrumentationHelper<R, C> {
         }
         span.withName("S3", AbstractSpan.PRIO_DEFAULT - 1);
 
+
         span.getContext()
             .getDestination()
             .getService()
-            .withResource(Objects.requireNonNullElse(bucketName, S3_TYPE));
+            .withResource(bucketName != null ? bucketName : S3_TYPE);
 
         span.getContext().getDestination()
             .withAddress(httpURI.getHost())
