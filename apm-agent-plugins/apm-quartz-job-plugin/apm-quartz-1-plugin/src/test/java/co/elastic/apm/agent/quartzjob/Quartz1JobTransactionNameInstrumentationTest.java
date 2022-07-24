b9f69cd5ec681e84650c84c3f0b60693cc1950ef
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2022-07-24 15:59:39 +0300

    Increase transaction count timeout

--- apm-agent-plugins/apm-quartz-job-plugin/apm-quartz-1-plugin/src/test/java/co/elastic/apm/agent/quartzjob/Quartz1JobTransactionNameInstrumentationTest.java
+++ apm-agent-plugins/apm-quartz-job-plugin/apm-quartz-1-plugin/src/test/java/co/elastic/apm/agent/quartzjob/Quartz1JobTransactionNameInstrumentationTest.java
@@ -21,10 +21,12 @@ package co.elastic.apm.agent.quartzjob;
 import co.elastic.apm.agent.impl.ElasticApmTracer;
 import co.elastic.apm.agent.impl.transaction.Outcome;
 import co.elastic.apm.agent.impl.transaction.Transaction;
+import org.junit.jupiter.api.Test;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.SchedulerException;
 import org.quartz.SimpleTrigger;
 
 import javax.annotation.Nullable;
@@ -34,9 +36,17 @@ import static org.awaitility.Awaitility.await;
 
 class Quartz1JobTransactionNameInstrumentationTest extends AbstractJobTransactionNameInstrumentationTest {
 
+    @Test
+    void test100Times() throws SchedulerException {
+        for (int i = 0; i < 100; i++) {
+            super.testJobWithResult();
+            reporter.reset();
+        }
+    }
+
     @Override
     public Transaction verifyTransactionFromJobDetails(JobDetail job, Outcome expectedOutcome) {
-        reporter.awaitTransactionCount(1);
+        reporter.awaitTransactionCount(1, 2000);
 
         Transaction transaction = reporter.getFirstTransaction();
         await().untilAsserted(() -> assertThat(reporter.getTransactions().size()).isEqualTo(1));
