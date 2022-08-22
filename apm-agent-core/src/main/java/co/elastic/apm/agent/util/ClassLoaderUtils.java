230a9007ddf4e1c29b46129627053a16344bab94
Sylvain Juge <sylvain.juge@elastic.co>
2022-08-22 13:52:26 +0200

    remove unused code

--- apm-agent-core/src/main/java/co/elastic/apm/agent/util/ClassLoaderUtils.java
+++ apm-agent-core/src/main/java/co/elastic/apm/agent/util/ClassLoaderUtils.java
@@ -25,27 +25,6 @@ import javax.annotation.Nullable;
 
 public class ClassLoaderUtils {
 
-    /**
-     * Checks whether the provided {@link ClassLoader} may be unloaded like a web application class loader, for example.
-     * <p>
-     * If the class loader can't be unloaded, it is safe to use {@link ThreadLocal}s and to reuse the {@code WeakConcurrentMap.LookupKey}.
-     * Otherwise, the use of {@link ThreadLocal}s may lead to class loader leaks as it prevents the class loader this class
-     * is loaded by to unload.
-     * </p>
-     *
-     * @param classLoader The class loader to check.
-     * @return {@code true} if the provided class loader can be unloaded.
-     */
-    public static boolean isPersistentClassLoader(@Nullable ClassLoader classLoader) {
-        try {
-            return classLoader == null // bootstrap class loader
-                || classLoader == ClassLoader.getSystemClassLoader()
-                || classLoader == ClassLoader.getSystemClassLoader().getParent(); // ext/platfrom class loader;
-        } catch (Throwable ignored) {
-            return false;
-        }
-    }
-
     public static boolean isAgentClassLoader(@Nullable ClassLoader classLoader) {
         return (classLoader != null && classLoader.getClass().getName().startsWith("co.elastic.apm")) ||
             // This one also covers unit tests, where the app class loader loads the agent
