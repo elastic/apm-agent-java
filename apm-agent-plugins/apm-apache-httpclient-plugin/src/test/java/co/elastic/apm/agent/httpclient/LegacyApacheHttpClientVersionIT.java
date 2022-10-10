457d2896d044f23380f67b905a48a391c2aa9073
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2022-10-10 17:32:25 +0200

    Add test case for alternative code path in legacy Apache HttpClient instrumentation

--- apm-agent-plugins/apm-apache-httpclient-plugin/src/test/java/co/elastic/apm/agent/httpclient/LegacyApacheHttpClientVersionIT.java
+++ apm-agent-plugins/apm-apache-httpclient-plugin/src/test/java/co/elastic/apm/agent/httpclient/LegacyApacheHttpClientVersionIT.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.apm.agent.httpclient;
 
 import co.elastic.apm.agent.TestClassWithDependencyRunner;
@@ -15,16 +33,16 @@ public class LegacyApacheHttpClientVersionIT {
 
 
     public LegacyApacheHttpClientVersionIT(List<String> dependencies) throws Exception {
-        this.runner = new TestClassWithDependencyRunner(dependencies, "co.elastic.apm.agent.httpclient.LegacyApacheHttpClientInstrumentationTest");
+        this.runner = new TestClassWithDependencyRunner(dependencies, LegacyApacheHttpClientBasicHttpRequestInstrumentationTest.class);
     }
 
     @Parameterized.Parameters(name = "{0}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][]{
-            {List.of("org.apache.httpcomponents:httpcore:4.0")},
-            {List.of("org.apache.httpcomponents:httpcore:4.0.1")},
-            {List.of("org.apache.httpcomponents:httpcore:4.1.4")},
-            {List.of("org.apache.httpcomponents:httpcore:4.2.5")}
+            {List.of("org.apache.httpcomponents:httpclient:4.0")},
+            {List.of("org.apache.httpcomponents:httpclient:4.0.1")},
+            {List.of("org.apache.httpcomponents:httpclient:4.1.3")},
+            {List.of("org.apache.httpcomponents:httpclient:4.2.5")}
         });
     }
 
