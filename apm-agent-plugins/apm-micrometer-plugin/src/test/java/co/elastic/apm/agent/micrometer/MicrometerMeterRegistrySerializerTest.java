cf1b9147b87aa1d673ca5c95d52b8f92373ba1c6
Jack Shirazi <jack.shirazi@elastic.co>
2022-12-01 10:13:55 +0000

    refactor for cleaner implementation

--- apm-agent-plugins/apm-micrometer-plugin/src/test/java/co/elastic/apm/agent/micrometer/MicrometerMeterRegistrySerializerTest.java
+++ apm-agent-plugins/apm-micrometer-plugin/src/test/java/co/elastic/apm/agent/micrometer/MicrometerMeterRegistrySerializerTest.java
@@ -120,46 +120,26 @@ public class MicrometerMeterRegistrySerializerTest {
             testMeter1.meter().getId(), testMeter1.meter(),
             testMeter2.meter().getId(), testMeter2.meter()
         ), 0);
-        assertThat(serialized.size()).isEqualTo(2);
-        JsonNode jsonNode1 = readJsonString(serialized.get(0).toString());
-        JsonNode jsonNode2 = readJsonString(serialized.get(1).toString());
-        if (testMeter1.containsValues(jsonNode1)) {
-            testMeter1.checkSerialization(jsonNode1);
-            testMeter2.checkSerialization(jsonNode2);
-        } else {
-            testMeter1.checkSerialization(jsonNode2);
-            testMeter2.checkSerialization(jsonNode1);
-        }
-    }
-
-    static void checkPathHasValue(JsonNode jsonNode, String[] path, int value) {
-        jsonNode = getPathNode(jsonNode, path);
-        assertThat(jsonNode.asInt()).isEqualTo(value);
-    }
-
-    static JsonNode getPathNode(JsonNode jsonNode, String[] path) {
-        assertThat(jsonNode).isNotNull();
-        for (String element: path) {
-            jsonNode = jsonNode.get(element);
-            assertThat(jsonNode).isNotNull();
-        }
-        return jsonNode;
-    }
-
-    static boolean containsPathNode(JsonNode jsonNode, String[] path) {
-        if (jsonNode == null) {
-            return false;
-        }
-        for (String element: path) {
-            jsonNode = jsonNode.get(element);
-            if (jsonNode == null) {
-                return false;
+        assertThat(serialized.size()).isLessThan(3);
+        assertThat(serialized.size()).isGreaterThan(0);
+        if (serialized.size() == 1) {
+            JsonNode jsonNode = readJsonString(serialized.get(0).toString());
+            testMeter1.checkSerialization(jsonNode);
+            testMeter2.checkSerialization(jsonNode);
+        } else if(serialized.size() == 2) {
+            JsonNode jsonNode1 = readJsonString(serialized.get(0).toString());
+            JsonNode jsonNode2 = readJsonString(serialized.get(1).toString());
+            if (testMeter1.containsValues(jsonNode1)) {
+                testMeter1.checkSerialization(jsonNode1);
+                testMeter2.checkSerialization(jsonNode2);
+            } else {
+                testMeter1.checkSerialization(jsonNode2);
+                testMeter2.checkSerialization(jsonNode1);
             }
         }
-        return true;
     }
 
-    static JsonNode readJsonString(String jsonString) {
+    private static JsonNode readJsonString(String jsonString) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode json = objectMapper.readTree(jsonString);
@@ -174,66 +154,98 @@ public class MicrometerMeterRegistrySerializerTest {
         }
     }
 
-    interface TestMeter {
-        void addToMeterRegistry(MeterRegistry registry);
-        void populateValues();
-        void checkSerialization(JsonNode jsonNode);
-        Meter meter();
-        boolean containsValues(JsonNode jsonNode);
-    }
+    static abstract class TestMeter {
+        abstract void addToMeterRegistry(MeterRegistry registry);
+        abstract void populateValues();
+        abstract void checkSerialization(JsonNode jsonNode);
+        abstract String meternameExtension();
 
-    static class TestCounter implements TestMeter {
-        static String[] path = new String[]{"metricset", "samples", "counter", "value"};
+        protected Meter meter;
 
-        Counter counter;
-        int value = 42;
-
-        @Override
         public Meter meter() {
-            return counter;
+            return meter;
         }
 
-        @Override
+        public String metername() {
+            return "meter"+getClass().getSimpleName();
+        }
+
+        public String[] path() {
+            return path(meternameExtension());
+        }
+
+        public String[] path(String extension) {
+            return new String[]{"metricset", "samples", metername()+extension, "value"};
+        }
+
         public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path);
+            return containsPathNode(jsonNode, path());
+        }
+
+        private static boolean containsPathNode(JsonNode jsonNode, String[] path) {
+            if (jsonNode == null) {
+                return false;
+            }
+            for (String element: path) {
+                jsonNode = jsonNode.get(element);
+                if (jsonNode == null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        protected static void checkPathHasValue(JsonNode jsonNode, String[] path, int value) {
+            jsonNode = getPathNode(jsonNode, path);
+            assertThat(jsonNode.asInt()).isEqualTo(value);
+        }
+
+        protected static JsonNode getPathNode(JsonNode jsonNode, String[] path) {
+            assertThat(jsonNode).isNotNull();
+            for (String element: path) {
+                jsonNode = jsonNode.get(element);
+                assertThat(jsonNode).isNotNull();
+            }
+            return jsonNode;
+        }
+
+    }
+
+    static class TestCounter extends TestMeter {
+
+        @Override
+        String meternameExtension() {
+            return "";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            counter = Counter.builder("counter").tags("tag", "value").register(registry);
+            meter = Counter.builder(metername()).register(registry);
         }
 
         @Override
         public void populateValues() {
-            counter.increment(value);
+            ((Counter) meter).increment(42);
         }
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path, value);
+            checkPathHasValue(jsonNode, path(), 42);
         }
 
     }
 
-    static class TestFunctionCounter implements TestMeter {
-        static String[] path = new String[]{"metricset", "samples", "functioncounter", "value"};
-
-        FunctionCounter functioncounter;
+    static class TestFunctionCounter extends TestMeter {
         long count;
 
         @Override
-        public Meter meter() {
-            return functioncounter;
-        }
-
-        @Override
-        public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path);
+        String meternameExtension() {
+            return "";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            functioncounter = FunctionCounter.builder("functioncounter", this, (o)->count).register(registry);
+            meter = FunctionCounter.builder(metername(), this, (o)->count).register(registry);
         }
 
         @Override
@@ -243,15 +255,12 @@ public class MicrometerMeterRegistrySerializerTest {
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path, (int) count);
+            checkPathHasValue(jsonNode, path(), 48);
         }
 
     }
 
-    static class TestSummary implements TestMeter {
-        static String[] path1 = new String[]{"metricset", "samples", "summary_example.count", "value"};
-
-        DistributionSummary summary;
+    static class TestSummary extends TestMeter {
         int sum = 0;
         int count = 0;
         int under5Count = 0;
@@ -260,19 +269,14 @@ public class MicrometerMeterRegistrySerializerTest {
         int[] values = new int[]{22, 55, 66, 98};
 
         @Override
-        public Meter meter() {
-            return summary;
-        }
-
-        @Override
-        public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path1);
+        String meternameExtension() {
+            return ".count";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            summary = DistributionSummary
-                .builder("summary_example")
+            meter = DistributionSummary
+                .builder(metername())
                 .distributionStatisticBufferLength(20)
                 .serviceLevelObjectives(5,50,95)
                 .publishPercentileHistogram()
@@ -282,7 +286,7 @@ public class MicrometerMeterRegistrySerializerTest {
         @Override
         public void populateValues() {
             for (int val : values) {
-                summary.record(val);
+                ((DistributionSummary) meter).record(val);
                 count++;
                 sum += val;
                 if (val < 5) {
@@ -299,15 +303,18 @@ public class MicrometerMeterRegistrySerializerTest {
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path1, count);
-            checkPathHasValue(jsonNode, new String[]{"metricset", "samples", "summary_example.sum", "value"}, sum);
-            JsonNode histoNode1 = getPathNode(jsonNode, new String[]{"metricset", "samples", "summary_example.histogram", "values"});
+            checkPathHasValue(jsonNode, path(), count);
+            checkPathHasValue(jsonNode, path(".sum"), sum);
+            String[] path = path(".histogram");
+            path[3] = "values";
+            JsonNode histoNode1 = getPathNode(jsonNode, path);
             assertThat(histoNode1.isArray()).isTrue();
             assertThat(histoNode1.size()).isEqualTo(3); //the 3 bucket boundaries of the SLOs 5,50,95
             assertThat(histoNode1.get(0).asDouble()).isEqualTo(5.0);
             assertThat(histoNode1.get(1).asDouble()).isEqualTo(50.0);
             assertThat(histoNode1.get(2).asDouble()).isEqualTo(95.0);
-            JsonNode histoNode2 = getPathNode(jsonNode, new String[]{"metricset", "samples", "summary_example.histogram", "counts"});
+            path[3] = "counts";
+            JsonNode histoNode2 = getPathNode(jsonNode, path);
             assertThat(histoNode2.isArray()).isTrue();
             assertThat(histoNode2.size()).isEqualTo(3); //the 3 counts of samples under boundaries of the SLOs 5,50,95
             assertThat(histoNode2.get(0).asInt()).isEqualTo(under5Count);
@@ -316,25 +323,17 @@ public class MicrometerMeterRegistrySerializerTest {
         }
     }
 
-    static class TestGauge implements TestMeter {
-        static String[] path = new String[]{"metricset", "samples", "gaugetest", "value"};
-
-        Gauge gauge;
+    static class TestGauge extends TestMeter {
         List<String> list = new ArrayList<>(4);
 
         @Override
-        public Meter meter() {
-            return gauge;
-        }
-
-        @Override
-        public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path);
+        String meternameExtension() {
+            return "";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            gauge = Gauge.builder(path[2], list, List::size).register(registry);
+            meter = Gauge.builder(metername(), list, List::size).register(registry);
         }
 
         @Override
@@ -345,64 +344,47 @@ public class MicrometerMeterRegistrySerializerTest {
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path, 2);
+            checkPathHasValue(jsonNode, path(), 2);
         }
 
     }
 
-    static class TestTimer implements TestMeter {
-        static String[] path = new String[]{"metricset", "samples", "timertest.sum.us", "value"};
-
-        Timer timer;
+    static class TestTimer extends TestMeter {
 
         @Override
-        public Meter meter() {
-            return timer;
-        }
-
-        @Override
-        public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path);
+        String meternameExtension() {
+            return ".sum.us";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            timer = Timer.builder("timertest").register(registry);
+            meter = Timer.builder(metername()).register(registry);
         }
 
         @Override
         public void populateValues() {
-            timer.record(430, TimeUnit.MILLISECONDS);
+            ((Timer) meter).record(430, TimeUnit.MILLISECONDS);
         }
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path, 430000);
+            checkPathHasValue(jsonNode, path(), 430000);
         }
 
     }
 
-    static class TestFunctionTimer implements TestMeter {
-        static String[] path1 = new String[]{"metricset", "samples", "functiontimertest.sum.us", "value"};
-        static String[] path2 = new String[]{"metricset", "samples", "functiontimertest.count", "value"};
-
-        FunctionTimer functiontimer;
+    static class TestFunctionTimer extends TestMeter {
         double functionDoubleValue;
         long functionLongValue;
 
         @Override
-        public Meter meter() {
-            return functiontimer;
-        }
-
-        @Override
-        public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path1);
+        String meternameExtension() {
+            return ".sum.us";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            functiontimer = FunctionTimer.builder("functiontimertest", this, (o)->functionLongValue, (o)->functionDoubleValue, TimeUnit.MILLISECONDS).register(registry);
+            meter = FunctionTimer.builder(metername(), this, (o)->functionLongValue, (o)->functionDoubleValue, TimeUnit.MILLISECONDS).register(registry);
         }
 
         @Override
@@ -413,41 +395,32 @@ public class MicrometerMeterRegistrySerializerTest {
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path1, 440000);
-            checkPathHasValue(jsonNode, path2, 2);
+            checkPathHasValue(jsonNode, path(), 440000);
+            checkPathHasValue(jsonNode, path(".count"), 2);
         }
 
     }
 
-    static class TestLongTaskTimer implements TestMeter {
-        static String[] path = new String[]{"metricset", "samples", "longtasktimertest.count", "value"};
-
-        LongTaskTimer longtasktimer;
-
+    static class TestLongTaskTimer extends TestMeter {
         @Override
-        public Meter meter() {
-            return longtasktimer;
-        }
-
-        @Override
-        public boolean containsValues(JsonNode jsonNode) {
-            return containsPathNode(jsonNode, path);
+        String meternameExtension() {
+            return ".count";
         }
 
         @Override
         public void addToMeterRegistry(MeterRegistry registry) {
-            longtasktimer = LongTaskTimer.builder("longtasktimertest").register(registry);
+            meter = LongTaskTimer.builder(metername()).register(registry);
         }
 
         @Override
         public void populateValues() {
-            longtasktimer.start();
-            longtasktimer.start();
+            ((LongTaskTimer) meter).start();
+            ((LongTaskTimer) meter).start();
         }
 
         @Override
         public void checkSerialization(JsonNode jsonNode) {
-            checkPathHasValue(jsonNode, path, 2);
+            checkPathHasValue(jsonNode, path(), 2);
         }
 
     }
