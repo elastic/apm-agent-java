ecb97d1dc21060b630a80e2399a252294555249a
Jonas Kunz <jonas.kunz@elastic.co>
2022-12-05 15:05:16 +0100

    Memoize transaction_max_spans in ElasticApmTracer constructor to prevent mockito races

--- apm-agent-core/src/test/java/co/elastic/apm/agent/impl/ElasticApmTracerTest.java
+++ apm-agent-core/src/test/java/co/elastic/apm/agent/impl/ElasticApmTracerTest.java
@@ -337,44 +337,52 @@ class ElasticApmTracerTest {
     @Test
     void testActivationStackOverflow() {
         doReturn(2).when(tracerImpl.getConfig(CoreConfiguration.class)).getTransactionMaxSpans();
-        Transaction transaction = startTestRootTransaction();
-        assertThat(tracerImpl.getActive()).isNull();
+
+        ElasticApmTracer tracer = new ElasticApmTracerBuilder()
+            .configurationRegistry(config)
+            .withApmServerClient(mock(ApmServerClient.class))
+            .reporter(reporter)
+            .withObjectPoolFactory(objectPoolFactory)
+            .buildAndStart();
+
+        Transaction transaction = tracer.startRootTransaction(getClass().getClassLoader());
+        assertThat(tracer.getActive()).isNull();
         try (Scope scope = transaction.activateInScope()) {
-            assertThat(tracerImpl.getActive()).isEqualTo(transaction);
+            assertThat(tracer.getActive()).isEqualTo(transaction);
             Span child1 = transaction.createSpan();
             try (Scope childScope = child1.activateInScope()) {
-                assertThat(tracerImpl.getActive()).isEqualTo(child1);
+                assertThat(tracer.getActive()).isEqualTo(child1);
                 Span grandchild1 = child1.createSpan();
                 try (Scope grandchildScope = grandchild1.activateInScope()) {
                     // latter activation should not be applied due to activation stack overflow
-                    assertThat(tracerImpl.getActive()).isEqualTo(child1);
+                    assertThat(tracer.getActive()).isEqualTo(child1);
                     Span ggc = grandchild1.createSpan();
                     try (Scope ggcScope = ggc.activateInScope()) {
-                        assertThat(tracerImpl.getActive()).isEqualTo(child1);
+                        assertThat(tracer.getActive()).isEqualTo(child1);
                         ggc.end();
                     }
                     grandchild1.end();
                 }
-                assertThat(tracerImpl.getActive()).isEqualTo(child1);
+                assertThat(tracer.getActive()).isEqualTo(child1);
                 child1.end();
             }
-            assertThat(tracerImpl.getActive()).isEqualTo(transaction);
+            assertThat(tracer.getActive()).isEqualTo(transaction);
             Span child2 = transaction.createSpan();
             try (Scope childScope = child2.activateInScope()) {
-                assertThat(tracerImpl.getActive()).isEqualTo(child2);
+                assertThat(tracer.getActive()).isEqualTo(child2);
                 Span grandchild2 = child2.createSpan();
                 try (Scope grandchildScope = grandchild2.activateInScope()) {
                     // latter activation should not be applied due to activation stack overflow
-                    assertThat(tracerImpl.getActive()).isEqualTo(child2);
+                    assertThat(tracer.getActive()).isEqualTo(child2);
                     grandchild2.end();
                 }
-                assertThat(tracerImpl.getActive()).isEqualTo(child2);
+                assertThat(tracer.getActive()).isEqualTo(child2);
                 child2.end();
             }
-            assertThat(tracerImpl.getActive()).isEqualTo(transaction);
+            assertThat(tracer.getActive()).isEqualTo(transaction);
             transaction.end();
         }
-        assertThat(tracerImpl.getActive()).isNull();
+        assertThat(tracer.getActive()).isNull();
         assertThat(reporter.getTransactions()).hasSize(1);
         assertThat(reporter.getSpans()).hasSize(2);
     }
