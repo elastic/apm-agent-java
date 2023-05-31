ffb0d164091bfff04b8755da2625264bc8712fd7
Jan Calanog <jan.calanog@elastic.co>
2023-05-31 23:12:16 +0200

    Add buildkite trigger action and complete lambda publish steps

---
+++ .ci/release.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+##  This script runs the release given the different environment variables
+##    dry_run
+##
+##  It relies on the .buildkite/hooks/pre-command so the Vault and other tooling
+##  are prepared automatically by buildkite.
+##
+
+set -eo pipefail
+
+# Make sure we delete this folder before leaving even in case of failure
+clean_up () {
+  ARG=$?
+  export VAULT_TOKEN=$PREVIOUS_VAULT_TOKEN
+  echo "--- Deleting tmp workspace"
+  rm -rf $TMP_WORKSPACE
+  exit $ARG
+}
+trap clean_up EXIT
+
+echo "--- Debug JDK installation :coffee:"
+echo $JAVA_HOME
+echo $PATH
+java -version
+
+set +x
+echo "--- Deploy the release :package:"
+if [[ "$dry_run" == "true" ]] ; then
+  ./mvnw clean install -DskipTests=true -Dmaven.javadoc.skip=true | tee release.txt
+else
+  echo "hello" | tee release.txt
+  # ./mvnw -V -s .ci/settings.xml -Pgpg clean deploy -DskipTests --batch-mode | tee release.txt
+fi
