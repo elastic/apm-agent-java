94f6e844cbc273808107bee7395c440d77139b99
Jan Calanog <jan.calanog@elastic.co>
2023-06-20 10:07:12 +0200

    Fix lambda layer publish script (#3185)
    * Fix lambda layer publish script
    
    * fix comments
    
    * Remove deletion of previous layers

--- .ci/create-arn-table.sh
+++ .ci/create-arn-table.sh
@@ -4,11 +4,10 @@ set -o pipefail
 #
 # Create the AWS ARN table given the below environment variables:
 #
-#   - AWS_FOLDER      - that's the location of the publish-layer-version output for each region
-#	- SUFFIX_ARN_FILE - that's the output file.
-#
+#  AWS_FOLDER - that's the location of the publish-layer-version output for each region
 
-ARN_FILE=${SUFFIX_ARN_FILE}
+AWS_FOLDER=${AWS_FOLDER?:No aws folder provided}
+ARN_FILE=".ci/.arn-file.md"
 
 {
 	echo "### ARNs of the APM Java Agent's AWS Lambda Layer"
@@ -19,7 +18,7 @@ ARN_FILE=${SUFFIX_ARN_FILE}
 
 for f in $(ls "${AWS_FOLDER}"); do
 	LAYER_VERSION_ARN=$(grep '"LayerVersionArn"' "$AWS_FOLDER/${f}" | cut -d":" -f2- | sed 's/ //g' | sed 's/"//g' | cut -d"," -f1)
-	echo "INFO: create-arn-table ARN(${LAYER_VERSION_ARN}):region(${f}))"
+	echo "INFO: create-arn-table ARN(${LAYER_VERSION_ARN}):region(${f})"
 	echo "|${f}|${LAYER_VERSION_ARN}|" >> "${ARN_FILE}"
 done
 
