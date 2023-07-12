c46b17b9fe3d419a60b9c1371adcad10e3d6c82b
jackshirazi <jack.shirazi@elastic.co>
2023-07-12 15:24:28 +0100

    Jakarta jms (#3198)
    * add jakarta to Messaging config doc
    
    * rename all classes importing javax.jms from package *.jms.* to *.jms.javax.*
    
    * add jakarta.jms to package info
    
    * add jakarta.jms dependency
    
    * add jakarta.jms instrumentation
    
    * refactor text package names needed to get instrumentation working and tests passing
    
    * refactor to combine common code into base classes
    
    * fix a few broken things
    
    * fix Object/String break
    
    * fix remaining test failures
    
    * fix minor things while doing diff
    
    * refactor to split javax and jakarta instrumentations
    
    * fix javadoc break
    
    * add change log entry
    
    * minor whitespace difference
    
    * fixing merge conflicts
    
    ---------
    
    Co-authored-by: Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>

---
+++ apm-agent-plugins/apm-jms-plugin/apm-jms-jakarta/src/test/java/co/elastic/apm/agent/jms/jakarta/JmsMessageListenerTest.java
@@ -0,0 +1,167 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.jms.jakarta;
+
+import co.elastic.apm.agent.MockTracer;
+import co.elastic.apm.agent.bci.ElasticApmAgent;
+import co.elastic.apm.agent.impl.ElasticApmTracer;
+import co.elastic.apm.agent.impl.Tracer;
+import co.elastic.apm.agent.impl.transaction.Transaction;
+import co.elastic.apm.agent.jms.jakarta.test.TestMessageConsumer;
+import co.elastic.apm.agent.jms.jakarta.test.TestMessageListener;
+import co.elastic.apm.agent.jms.jakarta.test.TestMsgHandler;
+import co.elastic.apm.agent.tracer.GlobalTracer;
+import co.elastic.apm.agent.tracer.configuration.MessagingConfiguration;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.stagemonitor.configuration.ConfigurationRegistry;
+
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static co.elastic.apm.agent.testutils.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+// does not inherit from AbstractInstrumentationTest because the type pre-filter is applied early and can't be changed
+// at runtime, thus an isolated agent setup is required
+public class JmsMessageListenerTest {
+
+    private ElasticApmTracer tracer;
+    private ConfigurationRegistry config;
+
+    @BeforeEach
+    public void before(){
+        MockTracer.MockInstrumentationSetup mockInstrumentationSetup = MockTracer.createMockInstrumentationSetup();
+        tracer = mockInstrumentationSetup.getTracer();
+        config = mockInstrumentationSetup.getConfig();
+        assertThat(tracer.isRunning()).isTrue();
+    }
+
+    @AfterEach
+    public void after() {
+        tracer.stop();
+        ElasticApmAgent.reset();
+    }
+
+    private void startAgent(){
+        ElasticApmAgent.initInstrumentation(tracer, ByteBuddyAgent.install());
+    }
+
+    @Test
+    public void testJmsMessageListenerPackage_defaultConfig() throws Exception {
+        startAgent();
+
+        // default configuration
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.MATCHING_NAME_CONVENTION);
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.INNER_CLASS);
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.LAMBDA);
+        testJmsMessageListenerPackage(false, JmsMessageListenerVariant.NOT_MATCHING_NAME_CONVENTION);
+    }
+
+    @Test
+    public void testJmsMessageListenerPackage_customValue() throws Exception {
+        doReturn(Arrays.asList("co.elastic.apm.agent.jms.jakarta.test")).when(config.getConfig(MessagingConfiguration.class)).getJmsListenerPackages();
+
+        startAgent();
+
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.MATCHING_NAME_CONVENTION);
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.INNER_CLASS);
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.LAMBDA);
+        testJmsMessageListenerPackage(true, JmsMessageListenerVariant.NOT_MATCHING_NAME_CONVENTION);
+    }
+
+    private enum JmsMessageListenerVariant {
+        MATCHING_NAME_CONVENTION,
+        INNER_CLASS,
+        LAMBDA,
+        NOT_MATCHING_NAME_CONVENTION
+    }
+
+    private void testJmsMessageListenerPackage(boolean expectIncluded, JmsMessageListenerVariant variant) throws Exception {
+
+        assertThat(tracer.currentTransaction())
+            .describedAs("no active transaction expected before onMessage")
+            .isNull();
+
+        Message message = mock(Message.class);
+
+        AtomicReference<Transaction> transaction = new AtomicReference<>();
+
+        switch (variant){
+            case MATCHING_NAME_CONVENTION:
+                // name fits the naming convention
+                new TestMessageListener(transaction).onMessage(message);
+                break;
+            case NOT_MATCHING_NAME_CONVENTION:
+                new TestMsgHandler(transaction).onMessage(message);
+                break;
+            case INNER_CLASS:
+                new InnerClassMsgHandler(transaction).onMessage(message);
+                break;
+            case LAMBDA:
+                // lambda is instrumented through wrapping on the message consumer
+                MessageListener listener = msg -> {
+                    transaction.set(GlobalTracer.get().require(Tracer.class).currentTransaction());
+                };
+                TestMessageConsumer consumer = new TestMessageConsumer();
+                // listener should be wrapped on setter entry
+                consumer.setMessageListener(listener);
+                // thus the wrapped listener is returned on getter
+                consumer.getMessageListener().onMessage(message);
+                break;
+            default:
+                throw new IllegalArgumentException("unknown case " + variant);
+        }
+
+        assertThat(tracer.currentTransaction())
+            .describedAs("no active transaction expected after onMessage")
+            .isNull();
+
+        if (expectIncluded) {
+            assertThat(transaction.get())
+                .describedAs("transaction should be created for scenario " + variant)
+                .isNotNull();
+        } else {
+            assertThat(transaction.get())
+                .describedAs("transaction should not be created for scenario " + variant)
+                .isNull();
+        }
+    }
+
+    // will be instrumented by default as it's an inner class
+    private static class InnerClassMsgHandler implements MessageListener {
+
+        private final AtomicReference<Transaction> transaction;
+
+        public InnerClassMsgHandler(AtomicReference<Transaction> transaction) {
+            this.transaction = transaction;
+        }
+
+        @Override
+        public void onMessage(Message message) {
+            transaction.set(GlobalTracer.get().require(Tracer.class).currentTransaction());
+        }
+    }
+
+}
---
+++ apm-agent-plugins/apm-jms-plugin/apm-jms-javax/src/main/java/co/elastic/apm/agent/jms/javax/JmsInstrumentationHelper.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.agent.jms.javax;
+
+import co.elastic.apm.agent.sdk.logging.Logger;
+import co.elastic.apm.agent.sdk.logging.LoggerFactory;
+import co.elastic.apm.agent.tracer.AbstractSpan;
+import co.elastic.apm.agent.tracer.GlobalTracer;
+import co.elastic.apm.agent.tracer.Tracer;
+import co.elastic.apm.agent.tracer.dispatch.TextHeaderGetter;
+import co.elastic.apm.agent.tracer.dispatch.TextHeaderSetter;
+
+import javax.annotation.Nullable;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.TemporaryQueue;
+import javax.jms.TemporaryTopic;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import java.util.Enumeration;
+
+public class JmsInstrumentationHelper extends co.elastic.apm.agent.jms.JmsInstrumentationHelper<Destination, Message, MessageListener, JMSException> {
+
+    private static final Logger logger = LoggerFactory.getLogger(JmsInstrumentationHelper.class);
+
+    private static final JmsInstrumentationHelper INSTANCE = new JmsInstrumentationHelper(GlobalTracer.get());
+
+    public static JmsInstrumentationHelper get() {
+        return INSTANCE;
+    }
+
+    private JmsInstrumentationHelper(Tracer tracer) {
+        super(tracer);
+    }
+
+    public static class MessageListenerWrapper implements MessageListener {
+
+        private final MessageListener delegate;
+
+        MessageListenerWrapper(MessageListener delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public void onMessage(Message message) {
+            delegate.onMessage(message);
+        }
+    }
+
+    @Override
+    public MessageListener newMessageListener(MessageListener listener) {
+        return new MessageListenerWrapper(listener);
+    }
+
+    @Nullable
+    public String extractDestinationName(@Nullable Message message, Destination destination) {
+        String destinationName = null;
+        if (message != null) {
+            destinationName = JmsMessagePropertyAccessor.instance().getFirstHeader(JMS_DESTINATION_NAME_PROPERTY, message);
+        }
+        if (destinationName == null) {
+            try {
+                if (destination instanceof Queue) {
+                    destinationName = ((Queue) destination).getQueueName();
+                } else if (destination instanceof Topic) {
+                    destinationName = ((Topic) destination).getTopicName();
+                }
+            } catch (JMSException e) {
+                logger.error("Failed to obtain destination name", e);
+            }
+        }
+        return destinationName;
+    }
+
+    @Override
+    public boolean isTempDestination(Destination destination, @Nullable String extractedDestinationName) {
+        return destination instanceof TemporaryQueue
+            || destination instanceof TemporaryTopic
+            || (extractedDestinationName != null && extractedDestinationName.startsWith(TIBCO_TMP_QUEUE_PREFIX));
+    }
+
+    @Override
+    public TextHeaderGetter<Message> propertyAccessorGetter() {
+        return JmsMessagePropertyAccessor.instance();
+    }
+
+    @Override
+    public TextHeaderSetter<Message> propertyAccessorSetter() {
+        return JmsMessagePropertyAccessor.instance();
+    }
+
+    @Override
+    public void addDestinationDetails(Destination destination,
+                                      String destinationName,
+                                      AbstractSpan<?> span) {
+
+        String prefix = null;
+        if (destination instanceof Queue) {
+            prefix = "queue ";
+        } else if (destination instanceof Topic) {
+            prefix = "topic ";
+        }
+
+        if (prefix != null) {
+            span.appendToName(prefix).appendToName(destinationName)
+                .getContext().getMessage().withQueue(destinationName);
+        }
+    }
+
+    @Override
+    public long getJMSTimestamp(Message message) throws JMSException {
+        return message.getJMSTimestamp();
+    }
+
+    @Override
+    public boolean isTextMessage(Message message) {
+        return message instanceof TextMessage;
+    }
+
+    @Override
+    public String getText(Message message) throws JMSException {
+        return ((TextMessage) message).getText();
+    }
+
+    @Override
+    public String getJMSMessageID(Message message) throws JMSException {
+        return message.getJMSMessageID();
+    }
+
+    @Override
+    public long getJMSExpiration(Message message) throws JMSException {
+        return message.getJMSExpiration();
+    }
+
+    @Override
+    public Enumeration getPropertyNames(Message message) throws JMSException {
+        return message.getPropertyNames();
+    }
+
+    @Override
+    public Object getObjectProperty(Message message, String propertyName) throws JMSException {
+        return message.getObjectProperty(propertyName);
+    }
+
+    @Override
+    public void setStringProperty(Message message, String propertyName, String value) throws JMSException {
+        message.setStringProperty(propertyName, value);
+    }
+
+    @Override
+    protected Destination getJMSDestination(Message message) throws JMSException {
+        return message.getJMSDestination();
+    }
+
+}
