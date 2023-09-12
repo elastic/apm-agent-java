f96ec8c96333e20febe51ea226d8d13802f09ad6
Jonas Kunz <jonas.kunz@elastic.co>
2023-09-12 11:05:34 +0200

    Remove IntelliJ debugging agent detection (#3315)

--- apm-agent-plugins/apm-java-concurrent-plugin/src/main/java/co/elastic/apm/agent/concurrent/ForkJoinTaskInstrumentation.java
+++ apm-agent-plugins/apm-java-concurrent-plugin/src/main/java/co/elastic/apm/agent/concurrent/ForkJoinTaskInstrumentation.java
@@ -42,17 +42,6 @@ public class ForkJoinTaskInstrumentation extends ElasticApmInstrumentation {
 
     private static final Tracer tracer = GlobalTracer.get();
 
-    static {
-        if (Boolean.parseBoolean(System.getProperty("intellij.debug.agent"))) {
-            // IntelliJ debugger also instrument some java.util.concurrent classes and changes the class structure.
-            // However, the changes are not re-applied when re-transforming already loaded classes, which makes our
-            // agent unable to see those structural changes and try to load classes with their original bytecode
-            //
-            // Go to the following to enable/disable: File | Settings | Build, Execution, Deployment | Debugger | Async Stack Traces
-            throw new IllegalStateException("IntelliJ debug agent detected, disable it to prevent unexpected instrumentation errors. See https://github.com/elastic/apm-agent-java/issues/1673");
-        }
-    }
-
     @Override
     public ElementMatcher<? super TypeDescription> getTypeMatcher() {
         return is(ForkJoinTask.class);
