6628442a3a69fae2d54782f54c4ddb86587f8256
Jan Calanog <jan.calanog@elastic.co>
2023-09-18 23:17:27 +0200

    fix

--- .buildkite/scripts/build-agent.sh
+++ .buildkite/scripts/build-agent.sh
@@ -11,7 +11,3 @@ cd "${AGENT_BASE_DIR}"
 git checkout "origin/${APM_VERSION}"
 ./mvnw --batch-mode clean package -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.sources.skip=true
 cp -v "$(find ./elastic-apm-agent/target -name '*.jar' | grep -v sources | grep -v original | grep -v javadoc)" "${current_working_dir}/${agent_jar_name}"
-cd -
-if [[ -n ${BUILDKITE} ]]; then
-  buildkite-agent artifact upload "${agent_jar_name}"
-fi
