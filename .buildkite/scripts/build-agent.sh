973b2a0fdd40e1d5f16b7780db78bcc8b07f4cd5
Jan Calanog <jan.calanog@elastic.co>
2023-09-18 23:07:39 +0200

    test

--- .buildkite/scripts/build-agent.sh
+++ .buildkite/scripts/build-agent.sh
@@ -4,13 +4,14 @@
 #
 set -euo pipefail
 
-agent_jar_path="$(pwd)/elastic-apm-agent.jar"
+current_working_dir="$(pwd)"
+agent_jar_name="elastic-apm-agent.jar"
 git clone "https://github.com/elastic/${REPO}.git" "${AGENT_BASE_DIR}"
 cd "${AGENT_BASE_DIR}"
 git checkout "origin/${APM_VERSION}"
 ./mvnw --batch-mode clean package -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.sources.skip=true
-cp -v "$(find ./elastic-apm-agent/target -name '*.jar' | grep -v sources | grep -v original | grep -v javadoc)" "${agent_jar_path}"
+cp -v "$(find ./elastic-apm-agent/target -name '*.jar' | grep -v sources | grep -v original | grep -v javadoc)" "${current_working_dir}/${agent_jar_name}"
 
 if [[ -n ${BUILDKITE} ]]; then
-  buildkite-agent artifact upload "${agent_jar_path}"
+  buildkite-agent artifact upload "${agent_jar_name}"
 fi
