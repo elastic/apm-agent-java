3fb8aaae1e749b61213f7a15b18419f6d780a1d9
Jan Calanog <jan.calanog@elastic.co>
2023-09-18 17:24:56 +0200

    ok

--- .buildkite/scripts/setup-load-testing-vault-secrets.sh
+++ .buildkite/scripts/setup-load-testing-vault-secrets.sh
@@ -24,5 +24,8 @@ echo "--- Prepare keys context :key:"
 VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID_SECRET" secret_id="$VAULT_SECRET_ID_SECRET")
 export VAULT_TOKEN
 
+APP_TOKEN_TYPE=$(vault read -field=user secret/apm-team/ci/bandstand)
+export APP_TOKEN_TYPE
+export APP_TOKEN_TYPE_SECRET="${APP_TOKEN_TYPE}" # This will mask the value in the logs
 APP_TOKEN=$(vault read -field=password secret/apm-team/ci/bandstand)
 export APP_TOKEN
