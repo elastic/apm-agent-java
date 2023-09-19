404c3bcee61578e1b586839cb207310ac85ef2d4
Jan Calanog <jan.calanog@elastic.co>
2023-09-19 09:52:34 +0200

    fix

--- .buildkite/scripts/build-test-application.sh
+++ .buildkite/scripts/build-test-application.sh
@@ -6,7 +6,7 @@ current_working_dir="$(pwd)"
 app_branch="main"
 binary_ext="jar"
 
-build_jdk_path=$(.ci/load/scripts/fetch_jdk.sh "$JVM_VERSION")
+build_jdk_path=$(.ci/load/scripts/fetch_sdk.sh "$JVM_VERSION")
 major_jdk_version=$(jq -r '.version' "/tmp/${JVM_VERSION}/jdk.json")
 
 if [[ ${major_jdk_version} = 7* ]]; then
