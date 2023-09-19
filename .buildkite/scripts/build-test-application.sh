ddf232368ff8000705d815a0a026ee4b59e189c5
Jan Calanog <jan.calanog@elastic.co>
2023-09-19 10:00:52 +0200

    test

--- .buildkite/scripts/build-test-application.sh
+++ .buildkite/scripts/build-test-application.sh
@@ -6,7 +6,7 @@ current_working_dir="$(pwd)"
 app_branch="main"
 binary_ext="jar"
 
-build_jdk_path=$(.ci/load/scripts/fetch_sdk.sh "$JVM_VERSION")
+build_jdk_path=$(.ci/load/scripts/fetch_sdk.sh "${JVM_VERSION}")
 major_jdk_version=$(jq -r '.version' "/tmp/${JVM_VERSION}/jdk.json")
 
 if [[ ${major_jdk_version} = 7* ]]; then
@@ -18,6 +18,8 @@ fi
 
 git clone --depth 1 --branch "${app_branch}" "https://github.com/spring-projects/${APP}" "${APP_BASE_DIR}"
 cd "${APP_BASE_DIR}"
+
+echo JAVA_HOME="${build_jdk_path}" java -version
 
 JAVA_HOME="${build_jdk_path}" ./mvnw --batch-mode clean package -DskipTests=true -Dcheckstyle.skip
 cp -v target/*."${binary_ext}" "${current_working_dir}"/app."${binary_ext}"
