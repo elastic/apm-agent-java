38b42a4e145f4b27d10307b2a97e2bd4a2c75d67
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-10-18 23:02:52 +0200

    improve: use gh and clone main with shallow cloning

--- .buildkite/hooks/prepare-benchmark.sh
+++ .buildkite/hooks/prepare-benchmark.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "--- Prepare github secrets :vault:"
 GITHUB_SECRET=$(vault kv get -field token "kv/ci-shared/observability-ci/github-apmmachine")
-export GITHUB_SECRET
+GH_TOKEN=$GITHUB_SECRET
+export GITHUB_SECRET GH_TOKEN
 GITHUB_USERNAME=apmmachine
 export GITHUB_USERNAME
+
+echo "--- Install gh :github:"
+GH_URL=https://github.com/cli/cli/releases/download/v2.37.0/gh_2.37.0_linux_amd64.tar.gz
+GH_HOME=$(pwd)/.gh
+curl -L --output /tmp/gh.tar.gz "$GH_URL"
+mkdir -p "$GH_HOME"
+tar --extract --file /tmp/gh.tar.gz --directory "$GH_HOME" --strip-components 1
+
+PATH=$GH_HOME/bin:$PATH
+export PATH
+
+echo "--- Configure gh :github:"
+echo "$GH_TOKEN" | gh auth login --with-token
+gh auth setup-git
--- .buildkite/scripts/opentelemetry-benchmark.sh
+++ .buildkite/scripts/opentelemetry-benchmark.sh
@@ -2,17 +2,14 @@
 set -eo pipefail
 
 echo "--- Build opentelemetry-java-instrumentation"
-git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
+git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git --depth 1 --branch main
 cd opentelemetry-java-instrumentation/
 ./gradlew assemble
 
 echo "--- Download the latest elastic-agent.zip"
 cd benchmark-overhead
-ELASTIC_SNAPSHOT_URL=$(curl -s -u $GITHUB_USERNAME:$GITHUB_SECRET "https://api.github.com/repos/elastic/apm-agent-java/actions/workflows/49838992/runs?branch=main" | jq -c '.workflow_runs[] | {conclusion, updated_at, display_title, url}' | grep -v null  | grep -v pending | grep -v cancelled | grep success | head -1 | awk -F'":"' '{print $5}' | tr -d '"}')
-ELASTIC_SNAPSHOT_ARTIFACTS=$(curl -s -u $GITHUB_USERNAME:$GITHUB_SECRET "$ELASTIC_SNAPSHOT_URL" | grep artifacts_url | awk -F'":' '{print $2}' | tr -d '"} ,')
-ELASTIC_SNAPSHOT_ZIPFILE=$(curl -s -u $GITHUB_USERNAME:$GITHUB_SECRET "$ELASTIC_SNAPSHOT_ARTIFACTS" | jq -c ".artifacts[] | {name,archive_download_url}" | grep '"elastic-apm-agent"' | awk -F'":' '{print $3}' | tr -d '"}')
-curl -s --output "elastic-agent.zip" -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_SECRET" -H "X-GitHub-Api-Version: 2022-11-28" -u $GITHUB_USERNAME:$GITHUB_SECRET "$ELASTIC_SNAPSHOT_ZIPFILE"
-unzip elastic-agent.zip
+run_id=$(gh run list --branch main --status success --workflow main.yml -L 1 --json databaseId --jq '.[].databaseId')
+gh run download "$run_id" -n elastic-apm-agent
 ELASTIC_SNAPSHOT_JAR=$(ls -1 elastic-apm-agent-*.jar)
 ELASTIC_SNAPSHOT_ENTRY="new Agent(\\\"elastic-snapshot\\\",\\\"latest available snapshot version from elastic main\\\",\\\"file://$PWD/$ELASTIC_SNAPSHOT_JAR\\\")"
 ELASTIC_LATEST_VERSION=$(curl -s https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/ | perl -ne 's/<.*?>//g; if(s/^([\d\.]+).*$/$1/){print}' | sort -V | tail -1)
