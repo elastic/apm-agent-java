218b8630e2d4152cc8945fdb3d8e7b09a1d7d98f
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-10-18 22:01:23 +0200

    no retry

--- .buildkite/hooks/prepare-benchmark.sh
+++ .buildkite/hooks/prepare-benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GITHUB_SECRET=$(retry 5 vault kv get -field token "kv/ci-shared/observability-ci/github-apmmachine")
+GITHUB_SECRET=$(vault kv get -field token "kv/ci-shared/observability-ci/github-apmmachine")
 export GITHUB_SECRET
 GITHUB_USERNAME=apmmachine
 export GITHUB_USERNAME
