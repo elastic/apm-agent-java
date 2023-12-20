233a6cbeaba804c041800c89c514f0e9f6b1e033
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-12-20 12:14:45 +0100

    use full docker name

--- .ci/scripts/jboss-upload.sh
+++ .ci/scripts/jboss-upload.sh
@@ -4,9 +4,9 @@
 set -eo pipefail
 
 ## This script is called manually when a new Docker image is added.
-while read -r i
-do
-  docker pull registry.access.redhat.com/jboss-eap-7/$i --platform linux/amd64
-  docker tag registry.access.redhat.com/jboss-eap-7/$i docker.elastic.co/observability-ci/$i
-  docker push docker.elastic.co/observability-ci/$i
+while read -r i ; do
+  name="${i%%*/}"
+  docker pull --platform linux/amd64 $i
+  docker tag $i docker.elastic.co/observability-ci/$name
+  docker push docker.elastic.co/observability-ci/$name
 done < .ci/scripts/jboss-docker-images.txt
