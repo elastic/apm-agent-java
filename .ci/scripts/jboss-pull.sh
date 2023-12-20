e0458d32b17dae542172d8762377499c86e3175a
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-12-20 19:12:00 +0100

    ci: support retry

--- .ci/scripts/jboss-pull.sh
+++ .ci/scripts/jboss-pull.sh
@@ -3,6 +3,24 @@
 # Bash strict mode
 set -eo pipefail
 
+# Retry the given command for the given times.
+retry() {
+  local retries=$1
+  shift
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** count))
+    count=$((count + 1))
+    if [ $count -lt "$retries" ]; then
+      sleep $wait
+    else
+      return $exit
+    fi
+  done
+  return 0
+}
+
 ## This script is used by the CI to be able to re-tag the docker images
 ## to use the official docker namespace.
 ## Or by an Elastic employee to configure the docker images as needed.
@@ -10,7 +28,7 @@ while read -r i ; do
   [[ -z $i ]] && continue
   name="${i##*/}"
   echo "::group::$name"
-  docker pull docker.elastic.co/observability-ci/$name --platform linux/amd64
+  retry 3 docker pull --platform linux/amd64 docker.elastic.co/observability-ci/$name
   docker tag docker.elastic.co/observability-ci/$name $i
   echo "::endgroup::"
 done < .ci/scripts/jboss-docker-images.txt
