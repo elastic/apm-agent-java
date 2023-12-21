8d43455233ea87fbdf3b545d3a0dd3e73aa25560
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2023-12-21 14:07:55 +0100

    fix pebkc compile error

--- apm-agent-core/src/test/java/co/elastic/apm/agent/impl/ElasticApmTracerTest.java
+++ apm-agent-core/src/test/java/co/elastic/apm/agent/impl/ElasticApmTracerTest.java
@@ -48,6 +48,7 @@ import org.stagemonitor.configuration.ConfigurationRegistry;
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
