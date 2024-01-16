e5ac455cce055c58b3337126bdc5a800efbebfee
Victor Martinez <victormartinezrubio@gmail.com>
2024-01-16 09:25:46 +0100

    use vault kv get

--- .buildkite/hooks/prepare-release.sh
+++ .buildkite/hooks/prepare-release.sh
@@ -14,16 +14,16 @@ chmod -R 700 $TMP_WORKSPACE
 echo "--- Prepare keys context :key:"
 # Nexus credentials
 NEXUS_SECRET=kv/ci-shared/release-eng/team-release-secrets/apm/maven_central
-SERVER_USERNAME=$(vault read -field username $NEXUS_SECRET)
+SERVER_USERNAME=$(vault kv get --field="username" $NEXUS_SECRET)
 export SERVER_USERNAME
-SERVER_PASSWORD=$(vault read -field password $NEXUS_SECRET)
+SERVER_PASSWORD=$(vault kv get --field="password" $NEXUS_SECRET)
 export SERVER_PASSWORD
 
 # Signing keys
 GPG_SECRET=kv/data/ci-shared/release-eng/team-release-secrets/apm/gpg
-vault read -field="keyring" $GPG_SECRET | base64 -d > $KEY_FILE
+vault kv get --field="keyring" $GPG_SECRET | base64 -d > $KEY_FILE
 ## NOTE: passphase is the name of the field.
-KEYPASS_SECRET=$(vault read -field="passphase" $GPG_SECRET)
+KEYPASS_SECRET=$(vault kv get --field="passphase" $GPG_SECRET)
 export KEYPASS_SECRET
 KEY_ID=$(vault kv get --field="key_id" $GPG_SECRET)
 KEY_ID_SECRET=${KEY_ID: -8}
