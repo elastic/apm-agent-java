ba639d7ac96a1c7695a34b846f1099ae4ffde1c1
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-04-22 21:46:38 +0200

    ci: faster artifact sharing between the two CIs

--- .ci/snapshot.sh
+++ .ci/snapshot.sh
@@ -34,3 +34,6 @@ fi
 
 echo "--- Deploy the snapshot :package: [./mvnw $GOAL)] $DRY_RUN_MSG"
 ./mvnw -V -s .ci/settings.xml -Pgpg clean $GOAL -DskipTests --batch-mode | tee snapshot.txt
+
+echo "--- Archive the target folder with jar files"
+find . -type d -name target -exec find {} -name '*.jar' -print0 \; | xargs -0 tar -cvf dist.tar
