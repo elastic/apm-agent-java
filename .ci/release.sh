ce87909b089a09acaa879bfc8fe73eae3ad890ca
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-04-24 18:25:58 +0200

    use script

--- .ci/release.sh
+++ .ci/release.sh
@@ -34,4 +34,7 @@ echo "--- Deploy the release :package: [./mvnw $GOAL)] $DRY_RUN_MSG"
 ./mvnw -V -s .ci/settings.xml -Pgpg clean $GOAL -DskipTests --batch-mode | tee release.txt
 
 echo "--- Archive the target folder with jar files"
-find . -type d -name target -exec find {} -name '*.jar' -print0 \; | xargs -0 tar -cvf "${TARBALL_FILE:-dist.tar}"
+echo 'gather artifacts'
+.ci/published-artifacts-list.sh | tee artifacts.list
+echo 'create tarbal'
+tar -cvf "${TARBALL_FILE:-artifacts.tar}" -T artifacts.list
