f3b28350fad9cdd41e51c6be599dab52152bade0
Jonas Kunz <jonas.kunz@elastic.co>
2024-08-20 13:45:21 +0200

    Add support for apache http client v5 body capturing (#3767)

--- apm-agent-plugins/apm-apache-httpclient/apm-apache-httpclient4-plugin/src/main/java/co/elastic/apm/agent/httpclient/v4/ApacheHttpEntityGetContentInstrumentation.java
+++ apm-agent-plugins/apm-apache-httpclient/apm-apache-httpclient4-plugin/src/main/java/co/elastic/apm/agent/httpclient/v4/ApacheHttpEntityGetContentInstrumentation.java
@@ -18,71 +18,31 @@
  */
 package co.elastic.apm.agent.httpclient.v4;
 
-import co.elastic.apm.agent.httpclient.RequestBodyRecordingInputStream;
-import co.elastic.apm.agent.httpclient.RequestBodyRecordingOutputStream;
-import co.elastic.apm.agent.httpclient.v4.helper.RequestBodyCaptureRegistry;
-import co.elastic.apm.agent.sdk.logging.Logger;
-import co.elastic.apm.agent.sdk.logging.LoggerFactory;
-import co.elastic.apm.agent.tracer.Span;
+import co.elastic.apm.agent.httpclient.common.AbstractApacheHttpRequestBodyCaptureAdvice;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.NamedElement;
 import net.bytebuddy.description.method.MethodDescription;
-import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.apache.http.HttpEntity;
 
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
 
-import static co.elastic.apm.agent.sdk.bytebuddy.CustomElementMatchers.classLoaderCanLoadClass;
-import static net.bytebuddy.matcher.ElementMatchers.hasSuperType;
-import static net.bytebuddy.matcher.ElementMatchers.isBootstrapClassLoader;
-import static net.bytebuddy.matcher.ElementMatchers.nameContains;
-import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.not;
-import static net.bytebuddy.matcher.ElementMatchers.returns;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
-public class ApacheHttpEntityGetContentInstrumentation extends BaseApacheHttpClientInstrumentation {
+public class ApacheHttpEntityGetContentInstrumentation extends BaseApacheHttpEntityInstrumentation {
 
-    public static class ApacheHttpEntityGetContentAdvice {
-
-        private static final Logger logger = LoggerFactory.getLogger(ApacheHttpEntityGetContentAdvice.class);
+    public static class ApacheHttpEntityGetContentAdvice extends AbstractApacheHttpRequestBodyCaptureAdvice {
 
         @Advice.OnMethodExit(suppress = Throwable.class, inline = false)
         @Advice.AssignReturned.ToReturned
-        public static InputStream onExit(@Advice.This HttpEntity thiz, @Advice.Return InputStream content) throws URISyntaxException {
-            Span<?> clientSpan = RequestBodyCaptureRegistry.removeSpanFor(thiz);
-            if (clientSpan != null) {
-                logger.debug("Wrapping input stream for request body capture for HttpEntity {} ({}) for span {}", thiz.getClass().getName(), System.identityHashCode(thiz), clientSpan);
-                return new RequestBodyRecordingInputStream(content, clientSpan);
-            }
-            return content;
+        public static InputStream onExit(@Advice.This HttpEntity thiz, @Advice.Return InputStream content) {
+            return maybeCaptureRequestBodyInputStream(thiz, content);
         }
     }
 
     @Override
     public String getAdviceClassName() {
         return "co.elastic.apm.agent.httpclient.v4.ApacheHttpEntityGetContentInstrumentation$ApacheHttpEntityGetContentAdvice";
-    }
-
-    @Override
-    public ElementMatcher.Junction<ClassLoader> getClassLoaderMatcher() {
-        return not(isBootstrapClassLoader())
-            .and(classLoaderCanLoadClass("org.apache.http.HttpEntity"));
-    }
-
-    @Override
-    public ElementMatcher<? super NamedElement> getTypeMatcherPreFilter() {
-        return nameStartsWith("org.apache.http").and(nameContains("Entity"));
-    }
-
-    @Override
-    public ElementMatcher<? super TypeDescription> getTypeMatcher() {
-        return hasSuperType(named("org.apache.http.HttpEntity"));
     }
 
     @Override
